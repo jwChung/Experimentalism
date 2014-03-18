b94cdedca88a0ad1b5d0875205fb41d820c9a4c9
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-18 23:48:21 +0900

    fix(AssemblyInfo): fixed assembly titles with Experiment.

--- src/Experiment/Properties/AssemblyInfo.cs
+++ src/Experiment/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Experimental")]
+[assembly: AssemblyTitle("Experiment")]
 [assembly: AssemblyDescription("")]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("e27d6f4e-54f1-4b67-95e5-92c353423b03")]
--- test/ExperimentUnitTest/Properties/AssemblyInfo.cs
+++ test/ExperimentUnitTest/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("ExperimentalUnitTest")]
+[assembly: AssemblyTitle("ExperimentUnitTest")]
 [assembly: AssemblyDescription("")]
 [assembly: Guid("0405debd-b86d-403f-88b1-faa1f9b1bd33")]
