91c4a7cbcf78b7d208537bff3cbef57760845b57
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-18 16:27:16 +0900

    chore(#1): resolve some code analysis warnings.

--- src/Experimental/TheoremAttribute.cs
+++ src/Experimental/TheoremAttribute.cs
@@ -1,11 +1,13 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace Jwc.Experimental
 {
     /// <summary>
     /// 테스트 메소드를 지칭하는 어트리뷰트로써, test runner에 의해 실행된다.
     /// </summary>
-    public class TheoremAttribute : FactAttribute
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class TheoremAttribute : FactAttribute
     {
     }
 }
