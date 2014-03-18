2fcbc370274ad8824252828140398473deb9ee62
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-18 15:45:34 +0900

    feat(assembly-level): restrict Experimental's references
    The product project is allowed to add only specified references.

---
+++ test/ExperimentalUnitTest/AssemblyLevelTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace Jwc.Experimental
+{
+    public class AssemblyLevelTest
+    {
+        [Fact]
+        public void SutOnlyReferencesSpecifiedAssemblies()
+        {
+            var sut = Assembly.LoadFrom("Jwc.Experimental.dll");
+            Assert.NotNull(sut);
+            var specifiedAssemblies = new []
+            {
+                "mscorlib"
+            };
+
+            var actual = sut.GetReferencedAssemblies().Select(an => an.Name).ToArray();
+
+            Assert.Equal(specifiedAssemblies.Length, actual.Length);
+            Assert.False(specifiedAssemblies.Except(actual).Any(), "Empty");
+        } 
+    }
+}
