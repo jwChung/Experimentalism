792d59e6a6f3edf2a2bae75d22ab30ac88599074
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-18 21:37:01 +0900

    chore(release): increment minor version
    to indicate that TheoremAttribute is test attribute and supports
    parameterized test.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -11,8 +11,14 @@ using System.Runtime.InteropServices;
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-pre01")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
 
 /*
- * Version 0.1.0-pre01
+ * Version 0.1.0
+ * 
+ * - TheoremAttribute is test attribute specified on method to indicate
+ *   test-case which is the same with FactAttribute of xunit.
+ * 
+ * - TheoremAttribute supports parameterized test, which is the same with
+ *   TheoryAttribute of xunit.
  */
