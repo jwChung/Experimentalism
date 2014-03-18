523f1ecbc2dc397a38d586a5b81c28cf8c872e25
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-19 06:31:59 +0900

    chore(run.cmd): move run.cmd out from build directory.

--- build/Run.cmd
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-CD build
-IF "%VS120COMNTOOLS%"=="" (
-    SET VsVersionProperty=""
-) ELSE (
-    SET VsVersionProperty="/p:VisualStudioVersion=12.0"
-)
-
-"%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild" Experimentalism.proj /v:minimal /maxcpucount /nodeReuse:false %VsVersionProperty% %*
