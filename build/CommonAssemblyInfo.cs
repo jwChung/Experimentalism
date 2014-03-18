8f98e4a993c72a1ec3b7f9388fbd6abf497f19e0
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-18 14:48:59 +0900

    chore: add CommonAssemblyInfo.

---
+++ build/CommonAssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Jin-Wook Chung")]
+[assembly: AssemblyCopyright("Copyright (c) 2014, Jin-Wook Chung")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion("0.0.1-pre01")]
+
+/*
+ */
