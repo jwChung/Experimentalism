9a0d727445fc1708ca405301523df02c3cd9f9da
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-18 16:20:53 +0900

    test(#1): address scenario test for #1.

---
+++ test/ExperimentalUnitTest/Scenario.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Jwc.Experimental
+{
+    public class Scenario
+    {
+        [Theorem]
+        public void TheoremActAsFactAttribute()
+        {
+            Assert.True(true, "Excuted");
+        }
+    }
+}
