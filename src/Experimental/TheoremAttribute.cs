30390d1e8b0f4a82f60bcab4c570e8363b9b3305
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-18 16:16:02 +0900

    docs(#1): slightly improve the XML doucment of TheoremAttribute.

--- src/Experimental/TheoremAttribute.cs
+++ src/Experimental/TheoremAttribute.cs
@@ -3,7 +3,7 @@
 namespace Jwc.Experimental
 {
     /// <summary>
-    /// 테스트 메소드를 가리키기 위한 어트리뷰트이며, test runner에 의해 실행된다.
+    /// 테스트 메소드를 지칭하는 어트리뷰트로써, test runner에 의해 실행된다.
     /// </summary>
     public class TheoremAttribute : FactAttribute
     {
