186255add4a1f9cd9ffd7108a9a291f51f4b618f
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-18 21:53:41 +0900

    docs(Theorem): improve XML document to clarify.

--- src/Experimental/TheoremAttribute.cs
+++ src/Experimental/TheoremAttribute.cs
@@ -7,7 +7,9 @@ using Xunit.Sdk;
 namespace Jwc.Experimental
 {
     /// <summary>
-    /// 테스트 메소드를 지칭하는 어트리뷰트로써, test runner에 의해 실행된다.
+    /// 이 attribute는 method위에 선언되어 해당 method가 test-case라는 것을
+    /// 지칭하게 되며, non-parameterized test 뿐 아니라 parameterized test에도
+    /// 사용될 수 있다.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
     public sealed class TheoremAttribute : FactAttribute
