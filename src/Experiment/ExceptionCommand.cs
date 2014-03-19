9ac0f7fddcb11a8a2332f843c516d44c036506ad
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-19 21:43:31 +0900

    docs(ExceptionCommand): add XML documents.

--- src/Experiment/ExceptionCommand.cs
+++ src/Experiment/ExceptionCommand.cs
@@ -4,7 +4,7 @@ using Xunit.Sdk;
 namespace Jwc.Experiment
 {
     /// <summary>
-    /// 
+    /// Exception 테스트 케이스를 나타냄.
     /// </summary>
     public class ExceptionCommand : TestCommand
     {
@@ -18,9 +18,10 @@ namespace Jwc.Experiment
         }
 
         /// <summary>
+        /// 테스트 메소드 실행.
         /// </summary>
-        /// <param name="testClass"></param>
-        /// <returns></returns>
+        /// <param name="testClass">테스트 메소드의 owner.</param>
+        /// <returns>테스트 결과.</returns>
         public override MethodResult Execute(object testClass)
         {
             throw new System.NotImplementedException();
