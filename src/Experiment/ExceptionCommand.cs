e475cefce7fdd55c3b78ffe0c2f80cb3acc022df
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-19 21:27:40 +0900

    feat(ExceptionCommand): introduce ExceptionCommand.

---
+++ src/Experiment/ExceptionCommand.cs
@@ -0,0 +1,28 @@
+﻿using Xunit.Sdk;
+
+namespace Jwc.Experiment
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ExceptionCommand : TestCommand
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExceptionCommand"/> class.
+        /// </summary>
+        /// <param name="method">The method under test.</param>
+        public ExceptionCommand(IMethodInfo method)
+            : base(method, "dummy", 1000)
+        {
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="testClass"></param>
+        /// <returns></returns>
+        public override MethodResult Execute(object testClass)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/ExperimentUnitTest/ExceptionCommandTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Jwc.Experiment
+{
+    public class ExceptionCommandTest
+    {
+        [Fact]
+        public void SutIsTestCommand()
+        {
+            var sut = new ExceptionCommand(Reflector.Wrap((MethodInfo)MethodBase.GetCurrentMethod()));
+            Assert.IsAssignableFrom<TestCommand>(sut);
+        }
+    }
+}
