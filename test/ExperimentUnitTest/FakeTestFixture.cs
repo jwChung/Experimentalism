f735ec42d74b9c401adbb2a8e963876946071e12
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-19 11:39:56 +0900

    feat: create FakeTestFixture.

---
+++ test/ExperimentUnitTest/FakeTestFixture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Jwc.Experiment
+{
+    public class FakeTestFixture : ITestFixture
+    {
+        public Func<object, object> OnCreate
+        {
+            get;
+            set;
+        }
+
+        public object Create(object request)
+        {
+            return OnCreate(request);
+        }
+    }
+}
