94f5818b0e7bf0790a727661ed37d78458305854
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-19 12:43:54 +0900

    feat: introduce NotSupportedFixture.

---
+++ test/ExperimentUnitTest/NotSupportedFixtureTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Jwc.Experiment
+{
+    public class NotSupportedFixtureTest
+    {
+        [Fact]
+        public void SutIsTestFixture()
+        {
+            var sut = new NotSupportedFixture();
+            Assert.IsAssignableFrom<ITestFixture>(sut);
+        }
+    }
+}
