a94f2959cf3e47bf4fa6d1c23a02d55b1c297c1b
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-20 13:00:40 +0900

    chore(assemblyinfo): define AssemblyDescription.

--- src/Experiment/Properties/AssemblyInfo.cs
+++ src/Experiment/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Experiment")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Experiment is to support parameterized tests with auto data.")]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("e27d6f4e-54f1-4b67-95e5-92c353423b03")]
