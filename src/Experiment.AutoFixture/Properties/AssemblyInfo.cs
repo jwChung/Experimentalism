5ba284f2a185f681315ee64d803c01998b6fdbd1
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-21 14:54:08 +0900

    build: rectify nuget package name in description.

--- src/Experiment.AutoFixture/Properties/AssemblyInfo.cs
+++ src/Experiment.AutoFixture/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Experiment.AutoFixture")]
-[assembly: AssemblyDescription("Experiment supports parameterized tests with auto data using AutoFixture library.")]
+[assembly: AssemblyDescription("Experiment.AutoFixture supports parameterized tests with auto data using AutoFixture library.")]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("de75befd-1c15-4d5a-98f1-9eb745e07e71")]
