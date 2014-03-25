bbae930a81d0305a0a710b645c9ea6a750c1775d
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-25 23:20:53 +0900

    build(DEPLOYMENT): increment patch version,
    to indicate that renaming default to naive is a fix rather than
    breaking change.
    
    Closes https://github.com/jwChung/Experimentalism/issues/24

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,19 +10,22 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.8.0")]
-[assembly: AssemblyInformationalVersion("0.8.0")]
+[assembly: AssemblyVersion("0.8.1")]
+[assembly: AssemblyInformationalVersion("0.8.1")]
 
 /*
- * Version 0.8.0
+ * Version 0.8.1
  * 
- * - Implemented the feature of First class tests, which is
- *   varataion of parameterized tests.
- *   
- *   Closes https://github.com/jwChung/Experimentalism/pull/19
- *   
- *   Related to pull requests:
- *   https://github.com/jwChung/Experimentalism/pull/20
- *   https://github.com/jwChung/Experimentalism/pull/21
- *   https://github.com/jwChung/Experimentalism/pull/22
+ * Rename naive to default:
+ * - NaiveTheoremAttribute -> DefaultTheoremAttribute
+ * - NaiveFirstClassTheoremAttribute - NaiveFirstClassTheoremAttribute
+ * 
+ * This renaming is breaking change but as the current major version is
+ * zero(unstable), the change is acceptable according to Semantic Versioning.
+ * 
+ * Issue
+ * - https://github.com/jwChung/Experimentalism/issues/24
+ * 
+ * Pull requests
+ * - https://github.com/jwChung/Experimentalism/pull/25
  */
