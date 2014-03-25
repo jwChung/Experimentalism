c7fdfe8579f49adddc236c41e01151851ac6e5ed
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-25 09:46:45 +0900

    dev: introduce ITestCase and FakeTestCase.

---
+++ test/ExperimentUnitTest/FakeTestCase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit.Sdk;
+
+namespace Jwc.Experiment
+{
+    public class FakeTestCase : ITestCase
+    {
+        public Func<IMethodInfo, ITestCommand> OnConvertToTestCommand
+        {
+            get;
+            set;
+        }
+
+        public ITestCommand ConvertToTestCommand(IMethodInfo method)
+        {
+            return OnConvertToTestCommand(method);
+        }
+    }
+}
