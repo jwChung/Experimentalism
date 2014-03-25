03115e8f3ed2d57eef2d745ded047e8f725faa3e
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-25 19:52:37 +0900

    dev: add some scenario tests for first class tests.

--- test/ExperimentUnitTest/Scenario.cs
+++ test/ExperimentUnitTest/Scenario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Xunit;
 using Xunit.Extensions;
@@ -21,6 +22,48 @@ namespace Jwc.Experiment
         {
             Assert.Equal("expected", arg1);
             Assert.Equal(1234, arg2);
+        }
+
+        [NaiveFirstClassTheorem]
+        public IEnumerable<ITestCase> NaiveFirstClassTheoremSupportsYieldReturnedTestCases()
+        {
+            yield return TestCase.New(() => Assert.Equal(3, 2 + 1));
+
+            yield return TestCase.New(
+                3, 7, 10,
+                (x, y, z) => Assert.Equal(z, x + y));
+        }
+
+        [NaiveFirstClassTheorem]
+        public ITestCase[] NaiveFirstClassTheoremSupportsArrayTestCases()
+        {
+            var testCases = new[]
+            {
+                new { X = 1, Y = 2, Z = 3 },
+                new { X = 3, Y = 7, Z = 10 },
+                new { X = 100, Y = 23, Z = 123 }
+            };
+
+            return testCases.Select(tc =>
+                TestCase.New(
+                    tc,
+                    ptc => Assert.Equal(ptc.Z, ptc.X + ptc.Y)))
+                .ToArray();
+        }
+
+        [NaiveFirstClassTheorem]
+        public IEnumerable<ITestCase> NaiveFirstClassTheoremSupportsTestMethodCases()
+        {
+            var testCases = new[]
+            {
+                new { X = "expected", Y = 1234 },
+                new { X = "expected", Y = 1234 }
+            };
+
+            return testCases.Select(tc =>
+                TestCase.New(
+                    new Scenario(), tc,
+                    (ps, ptc) => ps.NaiveTheoremSupportsParameterizedTest(ptc.X, ptc.Y)));
         }
 
         private class ParameterizedTestDataAttribute : DataAttribute
