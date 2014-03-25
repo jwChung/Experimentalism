6e2d2418474bad1c0217ca14e56a218cfb371859
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-25 10:34:36 +0900

    dev: implement EnumerateTestCommands,
    of NaiveFirstClassTheoremAttribute.

--- src/Experiment/NaiveFirstClassTheoremAttribute.cs
+++ src/Experiment/NaiveFirstClassTheoremAttribute.cs
@@ -1,4 +1,8 @@
-﻿using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Sdk;
 
 namespace Jwc.Experiment
 {
@@ -8,5 +12,29 @@ namespace Jwc.Experiment
     /// </summary>
     public class NaiveFirstClassTheoremAttribute : FactAttribute
     {
+        /// <summary>
+        /// Enumerates the test commands represented by this test method.
+        /// Derived classes should override this method to return instances of
+        /// <see cref="ITestCommand" />, one per execution of a test method.
+        /// </summary>
+        /// <param name="method">The test method</param>
+        /// <returns>
+        /// The test commands which will execute the test runs for the given method
+        /// </returns>
+        protected override IEnumerable<ITestCommand> EnumerateTestCommands(IMethodInfo method)
+        {
+            var methodInfo = method.MethodInfo;
+            var declaringObject = IsStatic(methodInfo.DeclaringType) 
+                ? null
+                : Activator.CreateInstance(methodInfo.DeclaringType);
+            
+            var testCases = (IEnumerable<ITestCase>)methodInfo.Invoke(declaringObject, null);
+            return testCases.Select(tc => tc.ConvertToTestCommand(method));
+        }
+
+        private static bool IsStatic(Type type)
+        {
+            return type.IsAbstract && type.IsSealed;
+        }
     }
 }
--- test/ExperimentUnitTest/NaiveFirstClassTheoremAttributeTest.cs
+++ test/ExperimentUnitTest/NaiveFirstClassTheoremAttributeTest.cs
@@ -1,4 +1,8 @@
-﻿using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Sdk;
 
 namespace Jwc.Experiment
 {
@@ -9,6 +13,54 @@ namespace Jwc.Experiment
         {
             var sut = new NaiveFirstClassTheoremAttribute();
             Assert.IsAssignableFrom<FactAttribute>(sut);
+        }
+
+        [Fact]
+        public void CreateTestCommandsReturnsCorrectCommands()
+        {
+            var sut = new NaiveFirstClassTheoremAttribute();
+            const string methodName = "TestCasesTest";
+            var method = Reflector.Wrap(GetType().GetMethod(methodName));
+
+            var actual = sut.CreateTestCommands(method).ToArray();
+
+            Assert.Equal(3, actual.Length);
+            Array.ForEach(actual, c =>
+            {
+                var command = Assert.IsType<FactCommand>(c);
+                Assert.Equal(methodName, command.MethodName);
+            });
+        }
+
+        [Fact]
+        public void CreateTestCommandsFromStaticReturnsCorrectCommands()
+        {
+            var sut = new NaiveFirstClassTheoremAttribute();
+            const string methodName = "StaticTestCasesTest";
+            var method = Reflector.Wrap(GetType().GetMethod(methodName));
+
+            var actual = sut.CreateTestCommands(method).ToArray();
+
+            Assert.Equal(3, actual.Length);
+            Array.ForEach(actual, c =>
+            {
+                var command = Assert.IsType<FactCommand>(c);
+                Assert.Equal(methodName, command.MethodName);
+            });
+        }
+
+        public IEnumerable<ITestCase> TestCasesTest()
+        {
+            yield return new FakeTestCase { OnConvertToTestCommand = m => new FactCommand(m) };
+            yield return new FakeTestCase { OnConvertToTestCommand = m => new FactCommand(m) };
+            yield return new FakeTestCase { OnConvertToTestCommand = m => new FactCommand(m) };
+        }
+
+        public static IEnumerable<ITestCase> StaticTestCasesTest()
+        {
+            yield return new FakeTestCase { OnConvertToTestCommand = m => new FactCommand(m) };
+            yield return new FakeTestCase { OnConvertToTestCommand = m => new FactCommand(m) };
+            yield return new FakeTestCase { OnConvertToTestCommand = m => new FactCommand(m) };
         }
     }
 }
