7e8ea9917878e423652acb7e36d0656f88d21e9a
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-26 14:32:43 +0900

    bulid(DEPLOYMENT): increment patch version,
    to indicate that well-formatted name of first class test is a fix.
    
    Closes https://github.com/jwChung/Experimentalism/issues/26

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,22 +10,18 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.8.1")]
-[assembly: AssemblyInformationalVersion("0.8.1")]
+[assembly: AssemblyVersion("0.8.2")]
+[assembly: AssemblyInformationalVersion("0.8.2")]
 
 /*
- * Version 0.8.1
+ * Version 0.8.2
  * 
- * Rename naive to default:
- * - NaiveTheoremAttribute -> DefaultTheoremAttribute
- * - NaiveFirstClassTheoremAttribute - NaiveFirstClassTheoremAttribute
- * 
- * This renaming is breaking change but as the current major version is
- * zero(unstable), the change is acceptable according to Semantic Versioning.
+ * Implemented FistClassCommand to display well-formatted name
+ * of first class test.
  * 
  * Issue
- * - https://github.com/jwChung/Experimentalism/issues/24
+ * - https://github.com/jwChung/Experimentalism/issues/26
  * 
  * Pull requests
- * - https://github.com/jwChung/Experimentalism/pull/25
+ * - https://github.com/jwChung/Experimentalism/pull/27
  */
