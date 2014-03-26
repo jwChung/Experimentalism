0eae26fc58b256c87f2df23c1477080eb7766598
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-26 13:01:21 +0900

    dev: introduce the new FirstClassCommand.

---
+++ src/Experiment/FirstClassCommand.cs
@@ -0,0 +1,18 @@
+﻿using Xunit.Sdk;
+
+namespace Jwc.Experiment
+{
+    /// <summary>
+    /// Represents a test command for first class tests.
+    /// </summary>
+    public class FirstClassCommand : FactCommand
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FirstClassCommand"/> class.
+        /// </summary>
+        /// <param name="method">The test method.</param>
+        public FirstClassCommand(IMethodInfo method) : base(method)
+        {
+        }
+    }
+}
---
+++ test/ExperimentUnitTest/FirstClassCommandTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Jwc.Experiment
+{
+    public class FirstClassCommandTest
+    {
+        [Fact]
+        public void SutIsFactCommand()
+        {
+            var method = Reflector.Wrap((MethodInfo)MethodBase.GetCurrentMethod());
+            var sut = new FirstClassCommand(method);
+            Assert.IsAssignableFrom<FactCommand>(sut);
+        }
+    }
+}
