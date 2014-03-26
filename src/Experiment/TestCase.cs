d95b295c8ab43edd03008c7151f2be5c2d313b8a
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-26 21:51:49 +0900

    docs: clarify exception message.

--- src/Experiment/TestCase.cs
+++ src/Experiment/TestCase.cs
@@ -27,7 +27,7 @@ namespace Jwc.Experiment
             {
                 throw new ArgumentException(
                     "The supplied delegate should be static, but isn't because of using objects " +
-                    "from outer scope, which results in problems from Shared Fixture " +
+                    "from outer scope, which can result in problems from Shared Fixture " +
                     "- Erratic Tests, probably complicated than Minimal Fixture and leading to Fragile Fixture. " +
                     "(http://xunitpatterns.com/Shared%20Fixture.html)",
                     "delegate");
