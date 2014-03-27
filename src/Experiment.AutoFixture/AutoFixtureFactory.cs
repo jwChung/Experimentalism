8fe68d761df0b7d817da19c37413cb68b054d1c9
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-27 23:30:51 +0900

    dev: introduce AutoFixtureFactory.

---
+++ src/Experiment.AutoFixture/AutoFixtureFactory.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Ploeh.AutoFixture;
+
+namespace Jwc.Experiment
+{
+    /// <summary>
+    /// Represents a fixture factory to create an instance of
+    /// <see cref="Fixture"/>.
+    /// </summary>
+    public class AutoFixtureFactory : ITestFixtureFactory
+    {
+        /// <summary>
+        /// Creates an instance of <see cref="Fixture" />.
+        /// </summary>
+        /// <param name="testMethod">The test method</param>
+        /// <returns>
+        /// The created fixture.
+        /// </returns>
+        public ITestFixture Create(MethodInfo testMethod)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/Experiment.AutoFixtureUnitTest/AutoFixtureFactoryTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Jwc.Experiment
+{
+    public class AutoFixtureFactoryTest
+    {
+        [Fact]
+        public void SutIsTestFixtureFactory()
+        {
+            var sut = new AutoFixtureFactory();
+            Assert.IsAssignableFrom<ITestFixtureFactory>(sut);
+        }
+    }
+}
