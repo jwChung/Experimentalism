5b3ce108395b2170165a32ae28651a93d3cf284b
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-27 20:38:34 +0900

    dev: introduce TypeFixtureFactory.

---
+++ src/Experiment/TypeFixtureFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace Jwc.Experiment
+{
+    /// <summary>
+    /// Represents a test factory to create an instance base on its type.
+    /// </summary>
+    public class TypeFixtureFactory : ITestFixtureFactory
+    {
+        /// <summary>
+        /// Creates an instance of <see cref="ITestFixture" />. This uses
+        /// a default constructor of supplied test fixture.
+        /// </summary>
+        /// <param name="method">
+        /// The method to be called when a test is executed.
+        /// </param>
+        /// <returns>
+        /// The result instance.
+        /// </returns>
+        public ITestFixture Create(MethodInfo method)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/ExperimentUnitTest/TypeFixtureFactoryTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Jwc.Experiment
+{
+    public class TypeFixtureFactoryTest
+    {
+        [Fact]
+        public void SutIsTestFixtureFactory()
+        {
+            var sut = new TypeFixtureFactory();
+            Assert.IsAssignableFrom<ITestFixtureFactory>(sut);
+        } 
+    }
+}
