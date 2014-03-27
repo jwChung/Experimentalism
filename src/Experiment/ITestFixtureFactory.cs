41b5e205819efed3016171e989023572909eac2c
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-27 20:19:43 +0900

    dev: introduced ITestFixtureFactory.

---
+++ src/Experiment/ITestFixtureFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+namespace Jwc.Experiment
+{
+    /// <summary>
+    /// Represents a facotry to create an instance of
+    /// the <see cref="ITestFixture"/> type.
+    /// </summary>
+    public interface ITestFixtureFactory
+    {
+        /// <summary>
+        /// Creates an instance of <see cref="ITestFixture"/>.
+        /// </summary>
+        /// <param name="method">
+        /// The method to be called when a test is executed.
+        /// </param>
+        /// <returns>
+        /// The result instance.
+        /// </returns>
+        ITestFixture Create(MethodInfo method);
+    }
+}
