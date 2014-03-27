99f1f64f1d6510e4983a5bc6478788b27dad6959
Jin-Wook Chung <jwChung@hotmail.com>
2014-03-27 20:23:18 +0900

    dev: introduce NotSupportedFixtureFactory.

---
+++ src/Experiment/NotSupportedFixtureFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace Jwc.Experiment
+{
+    /// <summary>
+    /// Represents a default factory to create an instance of
+    /// <see cref="NotSupportedFixture"/>.
+    /// </summary>
+    public class NotSupportedFixtureFactory : ITestFixtureFactory
+    {
+        /// <summary>
+        /// Creates an instance of <see cref="NotSupportedFixture" />.
+        /// </summary>
+        /// <param name="method">
+        /// The method to be called when a test is executed.
+        /// </param>
+        /// <returns>
+        /// The result instance.
+        /// </returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public ITestFixture Create(MethodInfo method)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/ExperimentUnitTest/NotSupportedFixtureFactoryTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Jwc.Experiment
+{
+    public class NotSupportedFixtureFactoryTest
+    {
+        [Fact]
+        public void SutIsTestFixtureFactory()
+        {
+            var sut = new NotSupportedFixtureFactory();
+            Assert.IsAssignableFrom<ITestFixtureFactory>(sut);
+        }
+    }
+}
