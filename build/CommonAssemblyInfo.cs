4031e2abd4ab0ac88c4e15812a751fa503279e38
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-03 08:28:43 +0900

    build(DEPLOYMENT): increment patch version,
    to indicate that making ShouldCreateInstance false is a fix.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,18 +10,12 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.8.14")]
-[assembly: AssemblyInformationalVersion("0.8.14")]
+[assembly: AssemblyVersion("0.8.15")]
+[assembly: AssemblyInformationalVersion("0.8.15")]
 
 /*
- * Version 0.8.14
+ * Version 0.8.15
  * 
- * Change the base class of the FirstClassCommand class to the TestCommand
- * class. 
- * 
- * BREAKING CHANGE
- *   before:
- *   public class FirstClassCommand : FactCommand { ... }
- *   after:
- *   public class FirstClassCommand : TestCommand { ... }
+ * Make ShouldCreateInstance of FirstClassCommand false
+ * not to create a test case instance for first class test.
  */
