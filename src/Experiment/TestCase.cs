a112e5a7cdc77f6358b1992da6bebcadb7b327fd
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-03 01:02:34 +0900

    dev: introduce the new TestCase class.

---
+++ src/Experiment/TestCase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xunit.Sdk;
+
+namespace Jwc.Experiment
+{
+    /// <summary>
+    /// Represents a weakly-typed test case that can be turned into an
+    /// xUnit.net ITestCommand when returned from a test method adorned with
+    /// the <see cref="DefaultFirstClassTheoremAttribute" />.
+    /// </summary>
+    public class TestCase : ITestCase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestCase"/> class.
+        /// </summary>
+        /// <param name="action">The test action.</param>
+        public TestCase(Action action)
+        {
+        }
+
+        /// <summary>
+        /// Converts the instance to an xUnit.net ITestCommand instance.
+        /// </summary>
+        /// <param name="method">
+        /// The method adorned by a <see cref="DefaultFirstClassTheoremAttribute" />.
+        /// </param>
+        /// <param name="fixtureFactory">
+        /// A test fixture factory to provide auto data.
+        /// </param>
+        /// <returns>
+        /// An xUnit.net ITestCommand that represents the executable test case.
+        /// </returns>
+        public ITestCommand ConvertToTestCommand(IMethodInfo method, ITestFixtureFactory fixtureFactory)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/ExperimentUnitTest/TestCaseTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Jwc.Experiment
+{
+    public class TestCaseTest
+    {
+        [Fact]
+        public void SutIsTestCase()
+        {
+            var sut = new TestCase(() => { });
+            Assert.IsAssignableFrom<ITestCase>(sut);
+        }
+    }
+}
