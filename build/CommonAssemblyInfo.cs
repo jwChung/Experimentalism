0f43c9acd793e46bdce28ad64de9d2e62a613e87
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-03 19:48:42 +0900

    build(DEPLOYMENT): increment patch version,
    to indicate that including experiment.dll to Experiment.AutoFixture is
    a fix rather than a breaking-change.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -11,10 +11,13 @@ using System.Runtime.InteropServices;
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyVersion("0.8.17")]
-[assembly: AssemblyInformationalVersion("0.8.17-pre01")]
+[assembly: AssemblyInformationalVersion("0.8.17")]
 
 /*
- * Version 0.8.17-pre01
+ * Version 0.8.17
  * 
- * To test publishing
+ * Experiment.AutoFixture nuget package includes Excperiment.dll
+ * but does not depend on the Experiment nuget package.
+ * 
+ * This version does not publish the Experiment nuget package.
  */
