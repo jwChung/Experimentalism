744e64ca8a11364e236f5f9f2bc1694c4810cc36
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-03 19:28:54 +0900

    build(DEPLOYMENT): increment pre-release version,
    to test publishing nuget packages.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,41 +10,11 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.8.16")]
-[assembly: AssemblyInformationalVersion("0.8.16")]
+[assembly: AssemblyVersion("0.8.17")]
+[assembly: AssemblyInformationalVersion("0.8.17-pre01")]
 
 /*
- * Version 0.8.16
+ * Version 0.8.17-pre01
  * 
- * Change TestCase to accept delegate for instance method rather than
- * delegate for only static method.
- * 
- * BREAKING CHANGE
- *   TestCase
- *     before:
- *     TestCase.New(...);
- *     after:
- *     new TestCase(...);
- *     
- *     before:
- *     TestCase.New("anonymous", x => { x.ToString() });
- *     after: disable specifying explicit arguments.
- *     var value = "anonymous";
- *     new TestCase(() => { value.ToString() });
- *     
- *   FirstClassCommand
- *     before:
- *     FirstClassCommand(IMethodInfo, MethodInfo, object[])
- *     after:
- *     FirstClassCommand(IMethodInfo, Delegate, object[])
- *     
- *     before:
- *     DeclaredMethod
- *     after:
- *     Method
- *     
- *     before:
- *     TestMethod
- *     after:
- *     Delegate
+ * To test publishing
  */
