d093cbaf6ccd73ca8f1ff83f8c3c2d3e6d242c64
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-07 13:05:33 +0900

    dev: introduce FirstClassTheoremAttribute.

---
+++ src/Experiment.AutoFixture.NuGetFiles/FirstClassTheoremAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+namespace Jwc.Experiment
+{
+    /// <summary>
+    /// A test attribute used to adorn methods that creates first-class 
+    /// executable test cases. This attribute supports to generate auto data
+    /// using the AutoFixture library.
+    /// </summary>
+    public class FirstClassTheoremAttribute : BaseFirstClassTheoremAttribute
+    {
+        /// <summary>
+        /// Creates an instance of <see cref="ITestFixture" />.
+        /// </summary>
+        /// <param name="testMethod">The test method</param>
+        /// <returns>
+        /// The created fixture.
+        /// </returns>
+        public override ITestFixture CreateTestFixture(MethodInfo testMethod)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/Experiment.AutoFixture.NuGetFilesUnitTest/FirstClassTheoremAttributeTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Jwc.Experiment
+{
+    public class FirstClassTheoremAttributeTest
+    {
+        [Fact]
+        public void SutIsDefaultFirstClassTheoremAttribute()
+        {
+            var sut = new FirstClassTheoremAttribute();
+            Assert.IsAssignableFrom<BaseFirstClassTheoremAttribute>(sut);
+        } 
+    }
+}
