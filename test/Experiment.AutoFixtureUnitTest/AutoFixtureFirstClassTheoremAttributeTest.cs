3a42ff02e1f53833d5b86516651d9ead3b0a77f0
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-09 14:15:04 +0900

    dev: implement AutoFixtureFirstClassTheoremAttribute.

---
+++ test/Experiment.AutoFixtureUnitTest/AutoFixtureFirstClassTheoremAttributeTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Kernel;
+using Xunit;
+
+namespace Jwc.Experiment
+{
+    public class AutoFixtureFirstClassTheoremAttributeTest
+    {
+        [Fact]
+        public void SutIsDefaultFirstClassTheoremAttribute()
+        {
+            var sut = new FakeFirstClassTheoremAttribute();
+            Assert.IsAssignableFrom<BaseFirstClassTheoremAttribute>(sut);
+        }
+
+        [Fact]
+        public void CreatTestFixtureWithNullTestMethodThrows()
+        {
+            var sut = new FakeFirstClassTheoremAttribute();
+            Assert.Throws<ArgumentNullException>(() => sut.CreateTestFixture(null));
+        }
+
+        [Fact]
+        public void CreatTestFixtureReturnsCorrectFixture()
+        {
+            var sut = new FakeFirstClassTheoremAttribute();
+            var dummyMethod = typeof(object).GetMethod("ToString");
+
+            var actual = sut.CreateTestFixture(dummyMethod);
+
+            var adapter = Assert.IsType<AutoFixtureAdapter>(actual);
+            var context = Assert.IsType<SpecimenContext>(adapter.SpecimenContext);
+            Assert.IsType<Fixture>(context.Builder);
+        }
+
+        public class FakeFirstClassTheoremAttribute : AutoFixtureFirstClassTheoremAttribute
+        {
+            protected override IFixture CreateFixture()
+            {
+                return new Fixture();
+            }
+        }
+    }
+}
