b4232b9764789e652f4ffd8a6f514bb45954fa9e
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-09 11:59:36 +0900

    build: rectify assembly titles.

--- test/Experiment.AutoFixture.NuGetFilesUnitTest/Properties/AssemblyInfo.cs
+++ test/Experiment.AutoFixture.NuGetFilesUnitTest/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Experiment.AutoFixtureUnitTest")]
+[assembly: AssemblyTitle("Experiment.AutoFixture.NuGetFilesUnitTest")]
 [assembly: AssemblyDescription("")]
 [assembly: Guid("89df3fbd-fab1-4c91-8f5d-48b382678d9f")]
