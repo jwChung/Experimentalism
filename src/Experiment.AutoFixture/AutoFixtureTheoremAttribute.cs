d1ab94f0c449583fb080a3971bb86169dca32f13
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-09 12:45:00 +0900

    dev: introduced AutoFixtureTheoremAttribute.

---
+++ src/Experiment.AutoFixture/AutoFixtureTheoremAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+
+namespace Jwc.Experiment
+{
+    /// <summary>
+    /// A test attribute declared on a test method to indicate a test case.
+    /// This attribute can be used for non-parameterized test as well as
+    /// parameterized test, and supports to generate auto data using
+    /// the AutoFixture library.
+    /// </summary>
+    public abstract class AutoFixtureTheoremAttribute : BaseTheoremAttribute
+    {
+        /// <summary>
+        /// Creates an instance of <see cref="ITestFixture"/>.
+        /// </summary>
+        /// <param name="testMethod">
+        /// The test method
+        /// </param>
+        /// <returns>
+        /// The created fixture.
+        /// </returns>
+        public override ITestFixture CreateTestFixture(MethodInfo testMethod)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/Experiment.AutoFixtureUnitTest/AutoFixtureTheoremAttributeTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Jwc.Experiment
+{
+    public class AutoFixtureTheoremAttributeTest
+    {
+        [Fact]
+        public void SutIsBaseTheoremAttribute()
+        {
+            var sut = new DelegatingAutoFixtureTheoremAttribute();
+            Assert.IsAssignableFrom<BaseTheoremAttribute>(sut);
+        }
+
+        private class DelegatingAutoFixtureTheoremAttribute : AutoFixtureTheoremAttribute
+        {
+        }
+    }
+}
