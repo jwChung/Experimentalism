d3a366abbe011668653611ff735d0857ee205c11
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-09 12:29:39 +0900

    dev: add assembly-reference test.

---
+++ test/Experiment.AutoFixtureUnitTest/AssemblyLevelTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Jwc.Experiment
+{
+    public class AssemblyLevelTest
+    {
+        [Fact]
+        public void SutReferencesOnlySpecifiedAssemblies()
+        {
+            var sut = typeof(AutoFixtureAdapter).Assembly;
+            var specifiedAssemblies = new []
+            {
+                "mscorlib",
+                "Jwc.Experiment",
+                "Ploeh.AutoFixture"
+            };
+
+            var actual = sut.GetReferencedAssemblies().Select(an => an.Name).Distinct().ToArray();
+
+            Assert.Equal(specifiedAssemblies.Length, actual.Length);
+            Assert.False(specifiedAssemblies.Except(actual).Any(), "Empty");
+        }
+    }
+}
