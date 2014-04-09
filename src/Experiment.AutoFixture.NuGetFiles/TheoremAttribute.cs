6165125b524871a7fc988c86de5a2846d0f3a095
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-09 16:31:18 +0900

    docs: improve XML document.

--- src/Experiment.AutoFixture.NuGetFiles/TheoremAttribute.cs
+++ src/Experiment.AutoFixture.NuGetFiles/TheoremAttribute.cs
@@ -5,11 +5,10 @@ using Ploeh.AutoFixture;
 namespace Jwc.NuGetFiles
 {
     /// <summary>
-    /// 이 attribute는 method위에 선언되어 해당 method가 test case라는 것을
-    /// 지칭하게 되며, non-parameterized test 뿐 아니라 parameterized test에도
-    /// 사용될 수 있다.
-    /// Parameterized test에 대해 이 attribute는 AutoFixture library를 이용하여
-    /// auto data기능을 제공한다.
+    /// A test attribute declared on a test method to indicate a test case.
+    /// This attribute can be used for non-parameterized test as well as
+    /// parameterized test, and supports to generate auto data using
+    /// the AutoFixture library.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
     public sealed class TheoremAttribute : AutoFixtureTheoremAttribute
