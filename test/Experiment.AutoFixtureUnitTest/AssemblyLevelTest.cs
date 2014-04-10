49adfc18f15b61c752d7889e41fc17ca0d5ae6b3
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-10 23:44:19 +0900

    dev: add tests for generating transform files.

--- test/Experiment.AutoFixtureUnitTest/AssemblyLevelTest.cs
+++ test/Experiment.AutoFixtureUnitTest/AssemblyLevelTest.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Text;
 using Xunit;
 using Xunit.Extensions;
 
@@ -7,6 +10,9 @@ namespace Jwc.Experiment
 {
     public class AssemblyLevelTest
     {
+        const string _productDirectory = @"..\..\..\..\src\Experiment.AutoFixture\";
+        const string _testDirectory = @"..\..\..\..\test\Experiment.AutoFixtureUnitTest\";
+
         [Fact]
         public void SutReferencesOnlySpecifiedAssemblies()
         {
@@ -42,6 +48,27 @@ namespace Jwc.Experiment
 
             // Exercise system and Verify outcome
             sut.VerifyDoesNotExpose(types);
+        }
+
+        [Theory]
+        [InlineData(_productDirectory, "TheoremAttribute")]
+        [InlineData(_productDirectory, "FirstClassTheoremAttribute")]
+        [InlineData(_testDirectory, "Scenario")]
+        public void SutCorrectlyGeneratesNugetTransformFiles(string directory, string originName)
+        {
+            var origin = directory + originName + ".cs";
+            var destination = directory + originName + ".cs.pp";
+            Assert.True(File.Exists(origin), "exists.");
+            VerifyGeneratingFile(origin, destination);
+        }
+
+        [Conditional("CI")]
+        private static void VerifyGeneratingFile(string origin, string destination)
+        {
+            var content = File.ReadAllText(origin, Encoding.UTF8)
+                .Replace("namespace Jwc.NuGetFiles", "namespace $rootnamespace$");
+            File.WriteAllText(destination, content, Encoding.UTF8);
+            Assert.True(File.Exists(destination), "exists.");
         }
     }
 }
