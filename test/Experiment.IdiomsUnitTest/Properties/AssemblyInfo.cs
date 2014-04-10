dfff8eea3f92365a6fb5e230bda5686851aac6da
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-10 13:51:20 +0900

    build: create a new test project - Experiment.IdiomsUnitTest.

---
+++ test/Experiment.IdiomsUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Experiment.IdiomsUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("cd59a677-0570-443d-b519-15fad55c9069")]
