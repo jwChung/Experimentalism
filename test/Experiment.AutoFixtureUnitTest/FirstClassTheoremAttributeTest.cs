8a0feedb2d0b21ead596efea022bba86bd655e92
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-11 14:05:01 +0900

    refactor: introduce the MethodInfo parameter to CreateFixture
    of FirstClassTheoremAttribute.

--- test/Experiment.AutoFixtureUnitTest/FirstClassTheoremAttributeTest.cs
+++ test/Experiment.AutoFixtureUnitTest/FirstClassTheoremAttributeTest.cs
@@ -30,20 +30,44 @@ namespace NuGet.Jwc.Experiment
         public void CreateFixtureReturnsCorrectFixture()
         {
             var sut = new TestSpecificFirstClassTheoremAttribute();
-            var actual = sut.CallCreateFixture();
+            var actual = sut.CallCreateFixture(null);
             Assert.IsType<Fixture>(actual);
+        }
+
+        [Fact]
+        public void CreateTestFixturePassesCorrecTestMethodToCreateFixture()
+        {
+            var sut = new TestSpecificFirstClassTheoremAttribute();
+            var testMethod = typeof(object).GetMethod("ToString");
+
+            sut.CallCreateTestFixture(testMethod);
+
+            var actual = sut.TestMethod;
+            Assert.Equal(testMethod, actual);
         }
 
         public class TestSpecificFirstClassTheoremAttribute : FirstClassTheoremAttribute
         {
-            public ITestFixture CallCreateTestFixture(MethodInfo testMethod)
+            public MethodInfo TestMethod
             {
-                return CreateTestFixture(testMethod);
+                get;
+                set;
             }
 
-            public IFixture CallCreateFixture()
+            public ITestFixture CallCreateTestFixture(MethodInfo testMethod)
             {
-                return CreateFixture();
+                return base.CreateTestFixture(testMethod);
+            }
+
+            public IFixture CallCreateFixture(MethodInfo testMethod)
+            {
+                return base.CreateFixture(testMethod);
+            }
+
+            protected override IFixture CreateFixture(MethodInfo testMethod)
+            {
+                TestMethod = testMethod;
+                return base.CreateFixture(testMethod);
             }
         }
     }
