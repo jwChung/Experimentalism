93ca46e2efbb552ea2be816134dd1c4d81b572fc
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-12 08:24:44 +0900

    dev: introduce the IdiomaticTestCommand class.

---
+++ src/Experiment.Idioms/IdiomaticTestCommand.cs
@@ -0,0 +1,36 @@
+﻿using Ploeh.Albedo;
+using Xunit.Sdk;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents a test command for idiom.
+    /// </summary>
+    public class IdiomaticTestCommand : TestCommand
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdiomaticTestCommand" /> class.
+        /// </summary>
+        /// <param name="method">The method under test.</param>
+        /// <param name="element">The reflection element to be asserted.</param>
+        /// <param name="assertion">The assertion.</param>
+        public IdiomaticTestCommand(
+            IMethodInfo method,
+            IReflectionElement element,
+            IReflectionVisitor<object> assertion)
+            : base(method, "anonymous", 100)
+        {
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="testClass"></param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        /// <inheritdoc />
+        public override MethodResult Execute(object testClass)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/IdiomaticTestCommandTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class IdiomaticTestCommandTest
+    {
+        [Fact]
+        public void SutIsTestCommand()
+        {
+            var dummyMethod = Reflector.Wrap((MethodInfo)MethodBase.GetCurrentMethod());
+            var sut = new IdiomaticTestCommand(dummyMethod, null, null);
+            Assert.IsAssignableFrom<TestCommand>(sut);
+        } 
+    }
+}
