f998400563db27039e9844cacee76f8222fa62f7
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-12 17:03:28 +0900

    dev: add guard clauses to ReflectionElementCollection.

---
+++ src/Experiment.Idioms/ReflectionElementCollection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Ploeh.Albedo;
+using Ploeh.Albedo.Refraction;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents collection of reflection elements.
+    /// </summary>
+    public class ReflectionElementCollection : IEnumerable<IReflectionElement>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReflectionElementCollection"/> class.
+        /// </summary>
+        /// <param name="sources">The sources to be refracted.</param>
+        /// <param name="refractions">The reflection element refractions.</param>
+        public ReflectionElementCollection(
+            IEnumerable<object> sources, params IReflectionElementRefraction<object>[] refractions)
+        {
+            if (sources == null)
+            {
+                throw new ArgumentNullException("sources");
+            }
+
+            if (refractions == null)
+            {
+                throw new ArgumentNullException("refractions");
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="IEnumerator{T}" /> that can be used to iterate through the collection.
+        /// </returns>
+        public IEnumerator<IReflectionElement> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/ReflectionElementCollectionTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Ploeh.Albedo;
+using Ploeh.Albedo.Refraction;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class ReflectionElementCollectionTest
+    {
+        [Fact]
+        public void SutIsEnumerableOfReflectionElement()
+        {
+            var sut = new ReflectionElementCollection(new object[0]);
+            Assert.IsAssignableFrom<IEnumerable<IReflectionElement>>(sut);
+        }
+
+        [Fact]
+        public void InitializeWithNullSourcesThrows()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ReflectionElementCollection(null));
+        }
+
+        [Fact]
+        public void InitializeWithNullRefractionsThrows()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new ReflectionElementCollection(new object[0], null));
+        }
+    }
+}
