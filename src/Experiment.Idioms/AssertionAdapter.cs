58ab77e5470f3b6d06f7c3d21df56e25edb4f1cc
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-13 00:33:56 +0900

    dev: implement AssertionAdapter.

---
+++ src/Experiment.Idioms/AssertionAdapter.cs
@@ -0,0 +1,31 @@
+﻿using Ploeh.Albedo;
+using Ploeh.AutoFixture.Idioms;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents an assertion to adapt <see cref="IIdiomaticAssertion"/>
+    /// to <see cref="IReflectionVisitor{T}"/>.
+    /// </summary>
+    public class AssertionAdapter : ReflectionVisitor<object>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AssertionAdapter"/> class.
+        /// </summary>
+        /// <param name="assertion">The assertion.</param>
+        public AssertionAdapter(IIdiomaticAssertion assertion)
+        {
+        }
+
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        public override object Value
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/AssertionAdapterTest.cs
@@ -0,0 +1,17 @@
+﻿using Ploeh.Albedo;
+using Ploeh.AutoFixture.Idioms;
+using Ploeh.AutoFixture.Kernel;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class AssertionAdapterTest
+    {
+        [Fact]
+        public void SutIsReflectionVisitor()
+        {
+            var sut = new AssertionAdapter(new GuardClauseAssertion(new ArrayRelay()));
+            Assert.IsAssignableFrom<IReflectionVisitor<object>>(sut);
+        }
+    }
+}
