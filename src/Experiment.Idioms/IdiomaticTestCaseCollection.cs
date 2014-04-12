834fbf8df1887b9fdb1ec7e866867bb9b1e77727
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-12 15:56:15 +0900

    dev: introduce IdiomaticTestCaseCollection.

---
+++ src/Experiment.Idioms/IdiomaticTestCaseCollection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents a collection of <see cref="IdiomaticTestCase"/>
+    /// </summary>
+    public class IdiomaticTestCaseCollection : IEnumerable<ITestCase>
+    {
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="IEnumerator{T}" /> that can be used to iterate through the collection.
+        /// </returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public IEnumerator<ITestCase> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/IdiomaticTestCaseCollectionTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class IdiomaticTestCaseCollectionTest
+    {
+        [Fact]
+        public void SutIsEnumerableOfTestCase()
+        {
+            var sut = new IdiomaticTestCaseCollection();
+            Assert.IsAssignableFrom<IEnumerable<ITestCase>>(sut);
+        }
+    }
+}
