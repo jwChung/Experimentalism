3f103309111c7695400435c233074871cd6bc89f
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-12 14:30:16 +0900

    dev: introduce IdiomaticTestCase.

---
+++ src/Experiment.Idioms/IdiomaticTestCase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents a idiomatic test case that can be turned into an
+    /// xUnit.net ITestCommand when returned from a test method adorned with
+    /// the <see cref="BaseFirstClassTheoremAttribute" />.
+    /// </summary>
+    public class IdiomaticTestCase : ITestCase
+    {
+        /// <summary>
+        /// Converts the instance to an xUnit.net ITestCommand instance.
+        /// </summary>
+        /// <param name="method">
+        /// The method adorned by a <see cref="BaseFirstClassTheoremAttribute" />.
+        /// </param>
+        /// <param name="fixtureFactory">
+        /// A test fixture factory to provide auto data.
+        /// </param>
+        /// <returns>
+        /// An xUnit.net ITestCommand that represents the executable test case.
+        /// </returns>
+        public ITestCommand ConvertToTestCommand(
+            IMethodInfo method, Func<MethodInfo, ITestFixture> fixtureFactory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/IdiomaticTestCaseTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class IdiomaticTestCaseTest
+    {
+        [Fact]
+        public void SutIsTestCase()
+        {
+            var sut = new IdiomaticTestCase();
+            Assert.IsAssignableFrom<ITestCase>(sut);
+        }
+    }
+}
