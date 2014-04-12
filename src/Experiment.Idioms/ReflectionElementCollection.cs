a565f5860e7b6270941489ec05d86b5e55ce4541
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-12 17:13:40 +0900

    dev: implement some properties of ReflectionElementCollection.

--- src/Experiment.Idioms/ReflectionElementCollection.cs
+++ src/Experiment.Idioms/ReflectionElementCollection.cs
@@ -11,11 +11,14 @@ namespace Jwc.Experiment.Idioms
     /// </summary>
     public class ReflectionElementCollection : IEnumerable<IReflectionElement>
     {
+        private readonly IEnumerable<object> _sources;
+        private readonly IReflectionElementRefraction<object>[] _refractions;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="ReflectionElementCollection"/> class.
         /// </summary>
         /// <param name="sources">The sources to be refracted.</param>
-        /// <param name="refractions">The reflection element refractions.</param>
+        /// <param name="refractions">The reflection-element refractions.</param>
         public ReflectionElementCollection(
             IEnumerable<object> sources, params IReflectionElementRefraction<object>[] refractions)
         {
@@ -27,6 +30,34 @@ namespace Jwc.Experiment.Idioms
             if (refractions == null)
             {
                 throw new ArgumentNullException("refractions");
+            }
+
+            _sources = sources;
+            _refractions = refractions;
+        }
+
+        /// <summary>
+        /// Gets the sources.
+        /// </summary>
+        public IEnumerable<object> Sources
+        {
+            get
+            {
+                return _sources;
+            }
+        }
+
+        /// <summary>
+        /// Gets the reflection-element refractions.
+        /// </summary>
+        /// <value>
+        /// The refractions.
+        /// </value>
+        public IEnumerable<IReflectionElementRefraction<object>> Refractions
+        {
+            get
+            {
+                return _refractions;
             }
         }
 
--- test/Experiment.IdiomsUnitTest/ReflectionElementCollectionTest.cs
+++ test/Experiment.IdiomsUnitTest/ReflectionElementCollectionTest.cs
@@ -27,5 +27,30 @@ namespace Jwc.Experiment.Idioms
             Assert.Throws<ArgumentNullException>(
                 () => new ReflectionElementCollection(new object[0], null));
         }
+
+        [Fact]
+        public void SourcesIsCorrect()
+        {
+            var sources = new object[0];
+            var sut = new ReflectionElementCollection(sources, new TypeElementRefraction<object>());
+
+            var actual = sut.Sources;
+
+            Assert.Equal(sources, actual);
+        }
+
+        [Fact]
+        public void RefractionsIsCorrect()
+        {
+            var refraction1 = new TypeElementRefraction<object>();
+            var refraction2 = new AssemblyElementRefraction<object>();
+            var sut = new ReflectionElementCollection(new object[0], refraction1, refraction2);
+
+            var actual = sut.Refractions;
+
+            Assert.Equal(
+                new IReflectionElementRefraction<object>[] { refraction1, refraction2 },
+                actual);
+        }
     }
 }
