eca6c77027c7c7fdb3e60100846e5d1eff64bbca
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-13 03:59:05 +0900

    dev: introduce GuardClauseAssertionTestCases.

---
+++ src/Experiment.Idioms/GuardClauseAssertionTestCases.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using Ploeh.Albedo;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents test cases for guard clause assertion.
+    /// </summary>
+    public class GuardClauseAssertionTestCases : IdiomaticTestCases
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GuardClauseAssertionTestCases"/> class.
+        /// </summary>
+        /// <param name="type">The target type whose members are verified.</param>
+        /// <param name="exceptedMembers">The excepted members.</param>
+        public GuardClauseAssertionTestCases(Type type, params MemberInfo[] exceptedMembers)
+            : base(new IReflectionElement[0], new GuardClauseAssertionFactory())
+        {
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/GuardClauseAssertionTestCasesTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class GuardClauseAssertionTestCasesTest
+    {
+        [Fact]
+        public void SutIsIdiomaticTestCases()
+        {
+            var sut = new GuardClauseAssertionTestCases(GetType());
+            Assert.IsAssignableFrom<IdiomaticTestCases>(sut);
+        } 
+    }
+}
