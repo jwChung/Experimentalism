706aeaaff48553b9389eeb9a017516a4bb7db838
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-13 02:00:09 +0900

    dev: introduce GuardClauseAssertionFactory.

---
+++ src/Experiment.Idioms/GuardClauseAssertionFactory.cs
@@ -0,0 +1,23 @@
+﻿using Ploeh.Albedo;
+using Ploeh.AutoFixture.Idioms;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents a factory to create <see cref="GuardClauseAssertion"/>
+    /// </summary>
+    public class GuardClauseAssertionFactory : IAssertionFactory
+    {
+        /// <summary>
+        /// Creates an assertion.
+        /// </summary>
+        /// <param name="testFixture">The test fixture.</param>
+        /// <returns>
+        /// The created assertion.
+        /// </returns>
+        public IReflectionVisitor<object> Create(ITestFixture testFixture)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/GuardClauseAssertionFactoryTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class GuardClauseAssertionFactoryTest
+    {
+        [Fact]
+        public void SutIsAssertionFactory()
+        {
+            var sut = new GuardClauseAssertionFactory();
+            Assert.IsAssignableFrom<IAssertionFactory>(sut);
+        } 
+    }
+}
