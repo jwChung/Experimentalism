f36cc22a912dbc2699a3d72fabe8f7495ae3ff1b
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-12 22:48:36 +0900

    dev: introduce the DefaultMembers class.

---
+++ src/Experiment.Idioms/DefaultMembers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents default members of a certain <see cref="Type"/> which need
+    /// to verify idiomatic assertions.
+    /// </summary>
+    public class DefaultMembers : IEnumerable<MemberInfo>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DefaultMembers"/> class.
+        /// </summary>
+        /// <param name="type">The target type.</param>
+        public DefaultMembers(Type type)
+        {
+            
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="T:System.Collections.Generic.IEnumerator`1" /> that
+        /// can be used to iterate through the collection.
+        /// </returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public IEnumerator<MemberInfo> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/DefaultMembersTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class DefaultMembersTest
+    {
+        [Fact]
+        public void SutIsEnumerableOfMemberInfo()
+        {
+            var sut = new DefaultMembers(typeof(object));
+            Assert.IsAssignableFrom<IEnumerable<MemberInfo>>(sut);
+        }
+    }
+}
