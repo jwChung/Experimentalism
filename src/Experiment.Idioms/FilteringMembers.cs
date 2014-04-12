07711c71faa7e0b74219f4734c140fdf74d15df7
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-13 03:40:03 +0900

    dev: introduce FilteringMembers.

---
+++ src/Experiment.Idioms/FilteringMembers.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents filter to except certain members from given members.
+    /// </summary>
+    public class FilteringMembers : IEnumerable<MemberInfo>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FilteringMembers"/> class.
+        /// </summary>
+        /// <param name="targetMembers">The target members to be excepted.</param>
+        /// <param name="exceptedMembers">The excepted members.</param>
+        public FilteringMembers(
+            IEnumerable<MemberInfo> targetMembers,
+            params MemberInfo[] exceptedMembers)
+        {
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="IEnumerator{T}" /> that can be used to iterate through the collection.
+        /// </returns>
+        public IEnumerator<MemberInfo> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/FilteringMembersTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class FilteringMembersTest
+    {
+        [Fact]
+        public void SutIsEnumerableMemberInfo()
+        {
+            var sut = new FilteringMembers(new MemberInfo[0]);
+            Assert.IsAssignableFrom<IEnumerable<MemberInfo>>(sut);
+        }
+    }
+}
