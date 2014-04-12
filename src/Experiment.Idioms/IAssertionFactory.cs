2813123196c816c46ff5957742ce47d0a2ecfafd
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-13 01:34:59 +0900

    dev: introduce IAssertionFactory.

---
+++ src/Experiment.Idioms/IAssertionFactory.cs
@@ -0,0 +1,17 @@
+﻿using Ploeh.Albedo;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents an assertion factory.
+    /// </summary>
+    public interface IAssertionFactory
+    {
+        /// <summary>
+        /// Creates an assertion.
+        /// </summary>
+        /// <param name="testFixture">The test fixture.</param>
+        /// <returns>The created assertion.</returns>
+        IReflectionVisitor<object> Create(ITestFixture testFixture);
+    }
+}
