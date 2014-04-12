721e894231d61c9d83ec09f9962365b183656189
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-12 16:34:40 +0900

    dev: introduce CompositeTestCaseCollection.

---
+++ src/Experiment.Idioms/CompositeTestCaseCollection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents composite collection of test cases.
+    /// </summary>
+    public class CompositeTestCaseCollection : IEnumerable<ITestCase>
+    {
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="IEnumerator{T}" /> that can be used to iterate through the collection.
+        /// </returns>
+        public IEnumerator<ITestCase> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/CompositeTestCaseCollectionTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class CompositeTestCaseCollectionTest
+    {
+        [Fact]
+        public void SutIsEnumerableOfTestCase()
+        {
+            var sut = new CompositeTestCaseCollection();
+            Assert.IsAssignableFrom<ITestCase>(sut);
+        }
+    }
+}
