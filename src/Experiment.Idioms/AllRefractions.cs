065e49375f5d9436e9147ae1796b566958a2a391
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-12 23:51:34 +0900

    dev: implement AllRefractions.

---
+++ src/Experiment.Idioms/AllRefractions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Ploeh.Albedo.Refraction;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents all kinds of <see cref="ReflectionElementRefraction{T}"/>
+    /// </summary>
+    public class AllRefractions : IEnumerable<IReflectionElementRefraction<object>>
+    {
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="IEnumerator{T}" /> that can be used to iterate through the collection.
+        /// </returns>
+        public IEnumerator<IReflectionElementRefraction<object>> GetEnumerator()
+        {
+            yield return new AssemblyElementRefraction<object>();
+            yield return new ConstructorInfoElementRefraction<object>();
+            yield return new EventInfoElementRefraction<object>();
+            yield return new FieldInfoElementRefraction<object>();
+            yield return new LocalVariableInfoElementRefraction<object>();
+            yield return new MethodInfoElementRefraction<object>();
+            yield return new ParameterInfoElementRefraction<object>();
+            yield return new PropertyInfoElementRefraction<object>();
+            yield return new TypeElementRefraction<object>();
+            yield return new ReflectionElementRefraction<object>();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/AllRefractionsTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Ploeh.Albedo.Refraction;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class AllRefractionsTest
+    {
+        [Fact]
+        public void SutIsEnumerableOfRefraction()
+        {
+            var sut = new AllRefractions();
+            Assert.IsAssignableFrom<IEnumerable<IReflectionElementRefraction<object>>>(sut);
+        }
+
+        [Fact]
+        public void SutEnumeratesAllKindsOfRefraction()
+        {
+            var refractions = new[]
+            {
+                typeof(AssemblyElementRefraction<object>),
+                typeof(ConstructorInfoElementRefraction<object>),
+                typeof(EventInfoElementRefraction<object>),
+                typeof(FieldInfoElementRefraction<object>),
+                typeof(LocalVariableInfoElementRefraction<object>),
+                typeof(MethodInfoElementRefraction<object>),
+                typeof(ParameterInfoElementRefraction<object>),
+                typeof(PropertyInfoElementRefraction<object>),
+                typeof(TypeElementRefraction<object>),
+                typeof(ReflectionElementRefraction<object>)
+            };
+            var sut = new AllRefractions();
+
+            var actual = sut.Select(r => r.GetType());
+
+            Assert.Equal(
+                refractions.OrderBy(t => t.FullName),
+                actual.OrderBy(t => t.FullName));
+        }
+    }
+}
