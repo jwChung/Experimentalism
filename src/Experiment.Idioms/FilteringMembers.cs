9302a9dd33cb5ba440409d5c8d06a66a1c14a91a
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-13 04:16:25 +0900

    dev: address some code-analysis warnings.

--- src/Experiment.Idioms/FilteringMembers.cs
+++ src/Experiment.Idioms/FilteringMembers.cs
@@ -9,6 +9,7 @@ namespace Jwc.Experiment.Idioms
     /// <summary>
     /// Represents filter to except certain members from given members.
     /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix", Justification = "The main responsibility of this class isn't to be a 'collection' (which, by the way, it isn't - it's just an Iterator).")]
     public class FilteringMembers : IEnumerable<MemberInfo>
     {
         private readonly IEnumerable<MemberInfo> _targetMembers;
--- src/Experiment.Idioms/GuardClauseAssertionTestCases.cs
+++ src/Experiment.Idioms/GuardClauseAssertionTestCases.cs
@@ -9,6 +9,7 @@ namespace Jwc.Experiment.Idioms
     /// <summary>
     /// Represents test cases for guard clause assertion.
     /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix", Justification = "The main responsibility of this class isn't to be a 'collection' (which, by the way, it isn't - it's just an Iterator).")]
     public class GuardClauseAssertionTestCases : IdiomaticTestCases
     {
         /// <summary>
