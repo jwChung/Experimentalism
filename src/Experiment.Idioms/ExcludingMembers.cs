1a62dbe83de69a56721dbc121ae325d2f84ac676
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-14 00:21:35 +0900

    build: address some code-analysis warnings.

--- src/Experiment.Idioms/ExcludingMembers.cs
+++ src/Experiment.Idioms/ExcludingMembers.cs
@@ -9,6 +9,7 @@ namespace Jwc.Experiment.Idioms
     /// <summary>
     /// Represents filter to exclude specified members.
     /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix", Justification = "The main responsibility of this class isn't to be a 'collection' (which, by the way, it isn't - it's just an Iterator).")]
     public class ExcludingMembers : IEnumerable<MemberInfo>
     {
         private readonly IEnumerable<MemberInfo> _targetMembers;
