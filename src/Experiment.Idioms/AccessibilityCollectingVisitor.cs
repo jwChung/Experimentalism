4e365313529bf2f1adcb82689c4550a7ecf51380
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-13 12:05:27 +0900

    dev: introduce AccessibilityCollectingVisitor.

---
+++ src/Experiment.Idioms/AccessibilityCollectingVisitor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Ploeh.Albedo;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents a class to collect <see cref="Accessibilities"/>.
+    /// </summary>
+    public class AccessibilityCollectingVisitor : ReflectionVisitor<IEnumerable<Accessibilities>>
+    {
+        /// <summary>
+        /// Gets the observation or value produced by this instance.
+        /// </summary>
+        public override IEnumerable<Accessibilities> Value
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/AccessibilityCollectingVisitorTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Ploeh.Albedo;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class AccessibilityCollectingVisitorTest
+    {
+        [Fact]
+        public void SutIsReflectionVisitor()
+        {
+            var sut = new AccessibilityCollectingVisitor();
+            Assert.IsAssignableFrom<ReflectionVisitor<IEnumerable<Accessibilities>>>(sut);
+        } 
+    }
+}
