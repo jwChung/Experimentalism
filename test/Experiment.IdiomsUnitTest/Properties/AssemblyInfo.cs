67260453c156e21cdcd83a2af100ffc1f8c78c34
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-14 11:03:02 +0900

    docs: add the assembly description
    and clarify the nuspec description.

--- test/Experiment.IdiomsUnitTest/Properties/AssemblyInfo.cs
+++ test/Experiment.IdiomsUnitTest/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Experiment.IdiomsUnitTest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Experiment.Idioms is to facilitate running idiomatic, regular unit-tests.")]
 [assembly: Guid("cd59a677-0570-443d-b519-15fad55c9069")]
