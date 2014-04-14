ec16f9d43dc6b9c5ae5b3c58fd4b14d1a267b784
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-14 21:44:41 +0900

    dev: introduce ConstructingMemberException.

---
+++ src/Experiment.Idioms/ConstructingMemberException.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents a verification error when testing whether members (property or field)
+    /// are correctly intialized by a constructor.
+    /// </summary>
+    [Serializable]
+    public class ConstructingMemberException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConstructingMemberException"/> class.
+        /// </summary>
+        public ConstructingMemberException()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConstructingMemberException"/> class.
+        /// </summary>
+        /// <param name="message">
+        /// The message that describes the error.
+        /// </param>
+        public ConstructingMemberException(string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConstructingMemberException"/> class.
+        /// </summary>
+        /// <param name="message">
+        /// The exception message.
+        /// </param>
+        /// <param name="inner">
+        /// The inner exception.
+        /// </param>
+        public ConstructingMemberException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConstructingMemberException" /> class.
+        /// </summary>
+        /// <param name="info">
+        /// The <see cref="SerializationInfo" /> that holds the serialized object
+        /// data about the exception being thrown.
+        /// </param>
+        /// <param name="context">
+        /// The <see cref="StreamingContext" /> that contains contextual information
+        /// about the source or destination.
+        /// </param>
+        protected ConstructingMemberException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/ConstructingMemberExceptionTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class ConstructingMemberExceptionTest
+    {
+        [Fact]
+        public void SutIsException()
+        {
+            var sut = new ConstructingMemberException();
+            Assert.IsAssignableFrom<Exception>(sut);
+        }
+    }
+}
