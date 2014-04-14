42bb77013c1569038b984e55b79a7522d8841d3e
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-14 15:05:58 +0900

    dev: introduce ConstructingMemberAssertion.

---
+++ src/Experiment.Idioms/ConstructingMemberAssertion.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Ploeh.Albedo;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Encapsulates a unit test that verifies that members (property or field)
+    /// are correctly intialized by a constructor.
+    /// </summary>
+    public class ConstructingMemberAssertion : ReflectionVisitor<object>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConstructingMemberAssertion" /> class.
+        /// </summary>
+        /// <param name="parameterToMemberComparer">
+        /// The comparer to determine if a given parameter value of
+        /// a constructor equals to a member(property or field) value.
+        /// </param>
+        /// <param name="memberToParameterComparer">
+        /// The comparer to determine if a given member(property or field) value
+        /// equals to a parameter value of a constructor.
+        /// </param>
+        public ConstructingMemberAssertion(
+            IEqualityComparer<IReflectionElement> parameterToMemberComparer,
+            IEqualityComparer<IReflectionElement> memberToParameterComparer)
+        {
+        }
+
+        /// <summary>
+        /// Gets the observation or value produced by this instance.
+        /// </summary>
+        public override object Value
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/ConstructingMemberAssertionTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Ploeh.Albedo;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class ConstructingMemberAssertionTest
+    {
+        [Fact]
+        public void SutIsReflectionVisitor()
+        {
+            var sut = new ConstructingMemberAssertion(
+                EqualityComparer<IReflectionElement>.Default,
+                EqualityComparer<IReflectionElement>.Default);
+            Assert.IsAssignableFrom<IReflectionVisitor<object>>(sut);
+        } 
+    }
+}
