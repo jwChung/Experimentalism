3c6fe32b36fe15b16bf8e529f71d53ae4edd1619
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-14 18:40:34 +0900

    dev: introduce PropertyToParameterComparer.

---
+++ src/Experiment.Idioms/PropertyToParameterComparer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Ploeh.Albedo;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represent comaprer to determine that a property value equals to
+    /// a parameter value.
+    /// </summary>
+    public class PropertyToParameterComparer : InverseEqualityComparer<IReflectionElement>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PropertyToParameterComparer" /> class.
+        /// </summary>
+        /// <param name="testFixture">The test fixture.</param>
+        public PropertyToParameterComparer(ITestFixture testFixture)
+            : base(EqualityComparer<IReflectionElement>.Default)
+        {
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/PropertyToParameterComparerTest.cs
@@ -0,0 +1,15 @@
+﻿using Ploeh.Albedo;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class PropertyToParameterComparerTest
+    {
+        [Fact]
+        public void SutIsInverseEqualityComparer()
+        {
+            var sut = new PropertyToParameterComparer(new DelegatingTestFixture());
+            Assert.IsAssignableFrom<InverseEqualityComparer<IReflectionElement>>(sut);
+        }
+    }
+}
