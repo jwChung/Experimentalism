e9221e405982a69320e299e592b0e0d52c66dfff
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-14 18:48:15 +0900

    dev: implement EqualityComparer property.

--- test/Experiment.IdiomsUnitTest/PropertyToParameterComparerTest.cs
+++ test/Experiment.IdiomsUnitTest/PropertyToParameterComparerTest.cs
@@ -11,5 +11,17 @@ namespace Jwc.Experiment.Idioms
             var sut = new PropertyToParameterComparer(new DelegatingTestFixture());
             Assert.IsAssignableFrom<InverseEqualityComparer<IReflectionElement>>(sut);
         }
+
+        [Fact]
+        public void EqualityComaprerIsCorrect()
+        {
+            var testFixture = new DelegatingTestFixture();
+            var sut = new PropertyToParameterComparer(testFixture);
+
+            var actual = sut.EqualityComparer;
+
+            var comparer = Assert.IsAssignableFrom<ParameterToPropertyComparer>(actual);
+            Assert.Equal(testFixture, comparer.TestFixture);
+        }
     }
 }
