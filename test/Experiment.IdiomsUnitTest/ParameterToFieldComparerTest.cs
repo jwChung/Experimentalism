5053640f77d01e63f369bfae6b9045a33e2a4da6
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-15 10:25:35 +0900

    dev: introduce ParameterToFieldComparer.

---
+++ test/Experiment.IdiomsUnitTest/ParameterToFieldComparerTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Ploeh.Albedo;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class ParameterToFieldComparerTest
+    {
+        [Fact]
+        public void SutIsEqualityComparer()
+        {
+            var sut = new ParameterToFieldComparer(new DelegatingTestFixture());
+            Assert.IsAssignableFrom<IEqualityComparer<IReflectionElement>>(sut);
+        }
+    }
+}
