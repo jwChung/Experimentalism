c25eac59d4613fcbcd56c979fe92de10ffbb1aad
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-15 10:53:16 +0900

    dev: introduce FieldToParameterComparer.

---
+++ test/Experiment.IdiomsUnitTest/FieldToParameterComparerTest.cs
@@ -0,0 +1,15 @@
+﻿using Ploeh.Albedo;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class FieldToParameterComparerTest
+    {
+        [Fact]
+        public void SutIsInverseEqualityComparer()
+        {
+            var sut = new FieldToParameterComparer(new DelegatingTestFixture());
+            Assert.IsAssignableFrom<InverseEqualityComparer<IReflectionElement>>(sut);
+        } 
+    }
+}
