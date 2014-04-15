8d0178b73a225533b786e3c0a3e48681bade8929
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-16 00:48:52 +0900

    build: increment patch version,
    to indicate that making CompositeTestCases generic is a fix.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,22 +10,15 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.10.0")]
-[assembly: AssemblyInformationalVersion("0.10.0")]
+[assembly: AssemblyVersion("0.10.1")]
+[assembly: AssemblyInformationalVersion("0.10.1")]
 
 /*
- * Version 0.10.0
+ * Version 0.10.1
  * 
- * Publishes a new nuget package called 'Experiment.Idioms', which facilitates
- * running idiomatic, regular unit-tests.
+ * Renames CompositeTestCases to CompositeEnumerables and makes it generic type.
  * 
- * This release includes the first idiomatic-test as
- * `GuardClauseAssertionTestCases`. This assertion verifies guard-clause
- * assertions for all public API.
- * 
- * [FirstClassTheorem]
- * public IEnumerable<ITestCase> Demo()
- * {
- *     return new GuardClauseAssertionTestCases(typeof(Foo));
- * }
+ * BREAKING-CHANGE
+ * - Rename: CompositeTestCases -> CompositeEnumerables
+ * - Make generic type: CompositeEnumerables<T>
  */
