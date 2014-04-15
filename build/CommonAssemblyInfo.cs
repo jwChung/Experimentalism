96e79bfa202e09c1695a6569eaea5fd6b19a7322
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-16 01:09:22 +0900

    build: increment patch version,
    to indicate that this fixes the typo 's' of CompositeEnumerables.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,15 +10,11 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.10.1")]
-[assembly: AssemblyInformationalVersion("0.10.1")]
+[assembly: AssemblyVersion("0.10.2")]
+[assembly: AssemblyInformationalVersion("0.10.2")]
 
 /*
- * Version 0.10.1
+ * Version 0.10.2
  * 
- * Renames CompositeTestCases to CompositeEnumerables and makes it generic type.
- * 
- * BREAKING-CHANGE
- * - Rename: CompositeTestCases -> CompositeEnumerables
- * - Make generic type: CompositeEnumerables<T>
+ * Renames CompositeEnumerables to CompositeEnumerable, which fixes the typo.
  */
