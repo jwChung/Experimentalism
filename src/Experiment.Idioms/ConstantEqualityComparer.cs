e3db89afbd22ac215090c22fd2fe5af3e1c7412a
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-16 12:58:10 +0900

    dev: introduce ConstantEqualityComparer.

---
+++ src/Experiment.Idioms/ConstantEqualityComparer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represent <see cref="IEqualityComparer{T}" /> always returning a constant
+    /// boolean.
+    /// </summary>
+    /// <typeparam name="T">The compared type.</typeparam>
+    public class ConstantEqualityComparer<T> : IEqualityComparer<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConstantEqualityComparer{T}"/> class.
+        /// </summary>
+        /// <param name="value">The constant value.</param>
+        public ConstantEqualityComparer(bool value)
+        {
+        }
+
+        /// <summary>
+        /// Determines whether the specified objects are equal.
+        /// </summary>
+        /// <returns>
+        /// true if the specified objects are equal; otherwise, false.
+        /// </returns>
+        /// <param name="x">
+        /// The first object of type <paramref name="x" /> to compare.
+        /// </param>
+        /// <param name="y">
+        /// The second object of type <paramref name="y" /> to compare.
+        /// </param>
+        public bool Equals(T x, T y)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns a hash code for the specified object.
+        /// </summary>
+        /// <returns>
+        /// A hash code for the specified object.
+        /// </returns>
+        /// <param name="obj">
+        /// The <see cref="object" /> for which a hash code is to be returned.
+        /// </param>
+        public int GetHashCode(T obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/ConstantEqualityComparerTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class ConstantEqualityComparerTest
+    {
+        [Fact]
+        public void SutIsEqualityComparer()
+        {
+            var sut = new ConstantEqualityComparer<object>(false);
+            Assert.IsAssignableFrom<IEqualityComparer<object>>(sut);
+        } 
+    }
+}
