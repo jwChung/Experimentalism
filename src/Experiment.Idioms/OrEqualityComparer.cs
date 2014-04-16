f54ac44a1c087d778d23d67630f7135dacff19e5
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-16 13:10:25 +0900

    dev: introduce OrEqualityComparer.

---
+++ src/Experiment.Idioms/OrEqualityComparer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents logical or operator for <see cref="IEqualityComparer{T}" />
+    /// </summary>
+    /// <typeparam name="T">The compared type.</typeparam>
+    public class OrEqualityComparer<T> : IEqualityComparer<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OrEqualityComparer{T}"/> class.
+        /// </summary>
+        /// <param name="equalityComparers">
+        /// The equality comparers.
+        /// </param>
+        public OrEqualityComparer(params IEqualityComparer<T>[] equalityComparers)
+        {
+        }
+
+        /// <summary>
+        /// Determines whether the specified objects are equal.
+        /// </summary>
+        /// <returns>
+        /// true if the specified objects are equal; otherwise, false.
+        /// </returns>
+        /// <param name="x">
+        /// The first object of type <paramref name="x" /> to compare.
+        /// </param>
+        /// <param name="y">
+        /// The second object of type <paramref name="y" /> to compare.
+        /// </param>
+        public bool Equals(T x, T y)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns a hash code for the specified object.
+        /// </summary>
+        /// <returns>
+        /// A hash code for the specified object.
+        /// </returns>
+        /// <param name="obj">
+        /// The <see cref="object" /> for which a hash code is to be returned.
+        /// </param>
+        public int GetHashCode(T obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/OrEqualityComparer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class OrEqualityComparerTest
+    {
+        [Fact]
+        public void SutIsEqualityComparer()
+        {
+            var sut = new OrEqualityComparer<object>();
+            Assert.IsAssignableFrom<IEqualityComparer<object>>(sut);
+        }
+    }
+}
