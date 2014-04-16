9b866f32a3d94280e16311fe48aa2d44c27f682c
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-16 10:33:56 +0900

    build: increment patch version,
    to indicate that this renames TargetMembers to TypeMembers, which is a
    fix rather than a breaking-change.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,11 +10,11 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.10.2")]
-[assembly: AssemblyInformationalVersion("0.10.2")]
+[assembly: AssemblyVersion("0.10.3")]
+[assembly: AssemblyInformationalVersion("0.10.3")]
 
 /*
- * Version 0.10.2
+ * Version 0.10.3
  * 
- * Renames CompositeEnumerables to CompositeEnumerable, which fixes the typo.
+ * Renames TargetMembers to TypeMembers to clarify.
  */
