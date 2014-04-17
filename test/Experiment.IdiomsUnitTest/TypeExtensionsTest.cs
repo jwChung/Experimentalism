4e272e4fa135139ff492d0113b0b19cd1122e0b6
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-18 00:21:15 +0900

    dev: implements GetReferencedAssemblies(type).

---
+++ test/Experiment.IdiomsUnitTest/TypeExtensionsTest.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Ploeh.Albedo;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Kernel;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class TypeExtensionsTest
+    {
+        [Theory]
+        [SuccessVisitTypeElementData]
+        public void GetReferencedAssembliesReturnsCorrectAssemblies(
+            Type type, Assembly[] expected)
+        {
+            var actual = TypeExtensions.GetReferencedAssemblies(type).ToArray();
+            Assert.Equal(expected.Length, actual.Length);
+            Assert.Empty(actual.Except(expected));
+        }
+
+        [Fact]
+        public void GetReferencedAssembliesWithNullTypeThrows()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => TypeExtensions.GetReferencedAssemblies(null));
+        }
+
+        public class SuccessVisitTypeElementDataAttribute : DataAttribute
+        {
+            public override IEnumerable<object[]> GetData(MethodInfo methodUnderTest, Type[] parameterTypes)
+            {
+                yield return new object[]
+                {
+                    typeof(object), new[] { typeof(object).Assembly }
+                };
+                yield return new object[]
+                {
+                    typeof(TypeExtensionsTest),
+                    new[]
+                    {
+                        typeof(object).Assembly,
+                        typeof(TypeExtensionsTest).Assembly
+                    }
+                };
+                yield return new object[]
+                {
+                    typeof(TheoryAttribute),
+                    new[]
+                    {
+                        typeof(FactAttribute).Assembly,
+                        typeof(object).Assembly,
+                        typeof(TheoryAttribute).Assembly
+                    }
+                };
+                yield return new object[]
+                {
+                    typeof(SpecimenBuilderAdapter),
+                    new[]
+                    {
+                        typeof(SpecimenBuilderAdapter).Assembly,
+                        typeof(object).Assembly,
+                        typeof(Fixture).Assembly
+                    }
+                };
+                yield return new object[]
+                {
+                    typeof(TypeImplementingMultiple),
+                    new[]
+                    {
+                        typeof(TypeImplementingMultiple).Assembly,
+                        typeof(IDisposable).Assembly,
+                        typeof(ISpecimenContext).Assembly
+                    }
+                };
+                yield return new object[]
+                {
+                    typeof(TypeImplementingHierarchical),
+                    new[]
+                    {
+                        typeof(TypeImplementingMultiple).Assembly,
+                        typeof(IDisposable).Assembly,
+                        typeof(IReflectionElement).Assembly,
+                        typeof(Fixture).Assembly
+                    }
+                };
+            }
+        }
+
+        public class TypeImplementingMultiple : IDisposable, ISpecimenContext
+        {
+            public void Dispose()
+            {
+                throw new NotSupportedException();
+            }
+
+            public object Resolve(object request)
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        public class TypeImplementingHierarchical : IDisposable, IHierarchical
+        {
+            public void Dispose()
+            {
+                throw new NotSupportedException();
+            }
+
+            public object Resolve(object request)
+            {
+                throw new NotSupportedException();
+            }
+
+            public IReflectionVisitor<T> Accept<T>(IReflectionVisitor<T> visitor)
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        public interface IHierarchical : ISpecimenContext, IReflectionElement
+        {
+        }
+    }
+}
