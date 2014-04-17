303a1a7f861f2c4b5f5baae76f6c23d01b9babb9
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-17 13:31:24 +0900

    dev: introduces RestrictingReferenceException class.

---
+++ src/Experiment.Idioms/RestrictingReferenceException.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents a verification error thrown when testing whether an assembly
+    /// references only given assemblies.
+    /// </summary>
+    [Serializable]
+    public class RestrictingReferenceException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RestrictingReferenceException"/> class.
+        /// </summary>
+        public RestrictingReferenceException()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RestrictingReferenceException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public RestrictingReferenceException(string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RestrictingReferenceException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="inner">The inner.</param>
+        public RestrictingReferenceException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RestrictingReferenceException"/> class.
+        /// </summary>
+        /// <param name="info">
+        /// The <see cref="SerializationInfo" /> that holds the serialized object data about the
+        /// exception being thrown.
+        /// </param>
+        /// <param name="context">
+        /// The <see cref="StreamingContext" /> that contains contextual information about the
+        ///  source or destination.
+        /// </param>
+        protected RestrictingReferenceException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/RestrictingReferenceExceptionTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class RestrictingReferenceExceptionTest
+    {
+        [Fact]
+        public void SutIsException()
+        {
+            var sut = new RestrictingReferenceException();
+            Assert.IsAssignableFrom<Exception>(sut);
+        }
+    }
+}
