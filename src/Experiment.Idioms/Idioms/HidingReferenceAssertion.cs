f1cf1f98d362d205dde4823a0a6df62256766d1f
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-18 14:19:49 +0900

    dev: introduce HidingReferenceAssertion.

---
+++ src/Experiment.Idioms/Idioms/HidingReferenceAssertion.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using Ploeh.Albedo;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents assertions to verify that specified assemblies are not
+    /// directly referenced.
+    /// </summary>
+    public class HidingReferenceAssertion : ReflectionVisitor<object>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HidingReferenceAssertion"/> class.
+        /// </summary>
+        /// <param name="assemblies">
+        /// The assemblies which are not referenced from elements.
+        /// </param>
+        public HidingReferenceAssertion(params Assembly[] assemblies)
+        {
+        }
+
+        /// <summary>
+        /// Gets the observation or value produced by this instance.
+        /// </summary>
+        public override object Value
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/Idioms/HidingReferenceAssertionTest.cs
@@ -0,0 +1,15 @@
+﻿using Ploeh.Albedo;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class HidingReferenceAssertionTest
+    {
+        [Fact]
+        public void SutIsReflectionVisitor()
+        {
+            var sut = new HidingReferenceAssertion();
+            Assert.IsAssignableFrom<IReflectionVisitor<object>>(sut);
+        }
+    }
+}
