1d465c7e611ded9217a7b0ca858766384d1a9096
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-18 20:06:34 +0900

    dev: implements Visit(TypeElement).

--- src/Experiment.Idioms/DirectReferenceCollectingVisitor.cs
+++ src/Experiment.Idioms/DirectReferenceCollectingVisitor.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Ploeh.Albedo;
 
@@ -9,6 +11,27 @@ namespace Jwc.Experiment
     /// </summary>
     public class DirectReferenceCollectingVisitor : ReflectionVisitor<IEnumerable<Assembly>>
     {
+        private readonly IEnumerable<Assembly> _assemblies;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DirectReferenceCollectingVisitor"/> class.
+        /// </summary>
+        public DirectReferenceCollectingVisitor()
+            : this(new Assembly[0])
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DirectReferenceCollectingVisitor"/> class.
+        /// </summary>
+        /// <param name="assemblies">
+        /// The assemblies.
+        /// </param>
+        protected DirectReferenceCollectingVisitor(IEnumerable<Assembly> assemblies)
+        {
+            _assemblies = assemblies;
+        }
+
         /// <summary>
         /// Gets the observation or value produced by this instance.
         /// </summary>
@@ -16,8 +39,59 @@ namespace Jwc.Experiment
         {
             get
             {
-                return new Assembly[0];
+                return _assemblies;
             }
+        }
+
+        /// <summary>
+        /// Allows an <see cref="TypeElement" /> to be visited. This method is
+        /// called when the element accepts this visitor instance.
+        /// </summary>
+        /// <param name="typeElement">The <see cref="TypeElement" /> being visited.
+        /// </param>
+        /// <returns>
+        /// A <see cref="IReflectionVisitor{T}" /> instance which can be used
+        /// to continue the visiting process with potentially updated
+        /// observations.
+        /// </returns>
+        public override IReflectionVisitor<IEnumerable<Assembly>> Visit(TypeElement typeElement)
+        {
+            if (typeElement == null)
+            {
+                throw new ArgumentNullException("typeElement");
+            }
+
+            var type = typeElement.Type;
+            var assemblies = GetReferencedAssemblies(type);
+
+            return new DirectReferenceCollectingVisitor(
+                assemblies.Concat(base.Visit(typeElement).Value).Distinct());
+        }
+
+        private static IEnumerable<Assembly> GetReferencedAssemblies(Type type)
+        {
+            return new[] { type }
+                .Concat(type.GetGenericArguments())
+                .SelectMany(GetReferenceAssembliesFromAncestors);
+        }
+
+        private static IEnumerable<Assembly> GetReferenceAssembliesFromAncestors(Type type)
+        {
+            return ReferencedAssembliesFromBaseTypes(type)
+                .Concat(GetReferencedAssembliesFromInterfaces(type));
+        }
+
+        private static IEnumerable<Assembly> ReferencedAssembliesFromBaseTypes(Type type)
+        {
+            var assemblies = new[] { type.Assembly };
+            return type.BaseType == null
+                ? assemblies
+                : assemblies.Concat(ReferencedAssembliesFromBaseTypes(type.BaseType));
+        }
+
+        private static IEnumerable<Assembly> GetReferencedAssembliesFromInterfaces(Type type)
+        {
+            return type.GetInterfaces().Select(i => i.Assembly);
         }
     }
 }
--- test/Experiment.IdiomsUnitTest/DirectReferenceCollectingVisitorTest.cs
+++ test/Experiment.IdiomsUnitTest/DirectReferenceCollectingVisitorTest.cs
@@ -1,7 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Ploeh.Albedo;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Kernel;
 using Xunit;
+using Xunit.Extensions;
 
 namespace Jwc.Experiment
 {
@@ -20,6 +25,181 @@ namespace Jwc.Experiment
             var sut = new DirectReferenceCollectingVisitor();
             var actual = sut.Value;
             Assert.Empty(actual);
+        }
+        
+        [Theory]
+        [ReferenceCollectingData]
+        public void VisitTypeEelementCollectsCorrectAssemblies(
+            Type type, Assembly[] expected)
+        {
+            var sut = new DirectReferenceCollectingVisitor();
+
+            var actual = sut.Visit(type.ToElement());
+
+            var result = actual.Value.ToArray();
+            Assert.Equal(expected.Length, result.Length);
+            Assert.Empty(result.Except(expected));
+        }
+
+        [Fact]
+        public void VisitTypeElementCallsBaseMethod()
+        {
+            ConstructorInfo constructor = null;
+            var type = typeof(object);
+            var visitor = new TestSpecificDirectReferenceCollectingVisitor(type.Assembly);
+            var sut = new TestSpecificDirectReferenceCollectingVisitor
+            {
+                OnVisitConstructorInfoElement = c =>
+                {
+                    constructor = c.ConstructorInfo;
+                    return visitor;
+                }
+            };
+            
+            var actual = sut.Visit(type.ToElement());
+
+            Assert.Equal(new[] { type.Assembly }, actual.Value);
+            Assert.Equal(constructor, type.GetConstructors().Single());
+        }
+
+        [Fact]
+        public void VisitNullTypeElementThrows()
+        {
+            var sut = new DirectReferenceCollectingVisitor();
+            Assert.Throws<ArgumentNullException>(() => sut.Visit((TypeElement)null));
+        }
+
+        private class TestSpecificDirectReferenceCollectingVisitor : DirectReferenceCollectingVisitor
+        {
+            public TestSpecificDirectReferenceCollectingVisitor()
+            {
+            }
+
+            public TestSpecificDirectReferenceCollectingVisitor(params Assembly[] assemblies)
+                : base(assemblies)
+            {
+            }
+
+            public Func<ConstructorInfoElement, IReflectionVisitor<IEnumerable<Assembly>>>
+                OnVisitConstructorInfoElement
+            {
+                get;
+                set;
+            }
+
+            public override IReflectionVisitor<IEnumerable<Assembly>> Visit(
+                ConstructorInfoElement constructorInfoElement)
+            {
+                return OnVisitConstructorInfoElement(constructorInfoElement);
+            }
+        }
+
+        private class ReferenceCollectingDataAttribute : DataAttribute
+        {
+            public override IEnumerable<object[]> GetData(MethodInfo methodUnderTest, Type[] parameterTypes)
+            {
+                yield return new object[]
+                {
+                    typeof(object), new[] { typeof(object).Assembly }
+                };
+                yield return new object[]
+                {
+                    typeof(TypeExtensionsTest),
+                    new[]
+                    {
+                        typeof(object).Assembly,
+                        typeof(TypeExtensionsTest).Assembly
+                    }
+                };
+                yield return new object[]
+                {
+                    typeof(TheoryAttribute),
+                    new[]
+                    {
+                        typeof(FactAttribute).Assembly,
+                        typeof(object).Assembly,
+                        typeof(TheoryAttribute).Assembly
+                    }
+                };
+                yield return new object[]
+                {
+                    typeof(SpecimenBuilderAdapter),
+                    new[]
+                    {
+                        typeof(SpecimenBuilderAdapter).Assembly,
+                        typeof(object).Assembly,
+                        typeof(Fixture).Assembly
+                    }
+                };
+                yield return new object[]
+                {
+                    typeof(TypeImplementingMultiple),
+                    new[]
+                    {
+                        typeof(TypeImplementingMultiple).Assembly,
+                        typeof(IDisposable).Assembly,
+                        typeof(ISpecimenContext).Assembly
+                    }
+                };
+                yield return new object[]
+                {
+                    typeof(TypeImplementingHierarchical),
+                    new[]
+                    {
+                        typeof(TypeImplementingMultiple).Assembly,
+                        typeof(IDisposable).Assembly,
+                        typeof(IReflectionElement).Assembly,
+                        typeof(Fixture).Assembly
+                    }
+                };
+
+                yield return new object[]
+                {
+                    typeof(IEnumerable<TypeImplementingHierarchical>),
+                    new[]
+                    {
+                        typeof(TypeImplementingMultiple).Assembly,
+                        typeof(IDisposable).Assembly,
+                        typeof(IReflectionElement).Assembly,
+                        typeof(Fixture).Assembly
+                    }
+                };
+            }
+        }
+
+        public class TypeImplementingMultiple : IDisposable, ISpecimenContext
+        {
+            public void Dispose()
+            {
+                throw new NotSupportedException();
+            }
+
+            public object Resolve(object request)
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        public class TypeImplementingHierarchical : IDisposable, IHierarchical
+        {
+            public void Dispose()
+            {
+                throw new NotSupportedException();
+            }
+
+            public object Resolve(object request)
+            {
+                throw new NotSupportedException();
+            }
+
+            public IReflectionVisitor<T> Accept<T>(IReflectionVisitor<T> visitor)
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        public interface IHierarchical : ISpecimenContext, IReflectionElement
+        {
         }
     }
 }
