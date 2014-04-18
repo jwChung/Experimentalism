699e9e53294ac47687fb2deac6cdfb1fb2c4e79d
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-18 13:24:39 +0900

    build: adds release note for RestrictingReferenceAssertion.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyInformationalVersion("0.12.0")]
 
 /*
- * Version 0.12.1
+ * Version 0.13.0
  * 
  * - [FIX] Changes constructor accessiblity of AccessibilityCollectingVisitor
  *   from private to protected.
@@ -22,4 +22,13 @@ using System.Runtime.InteropServices;
  * - [FIX] AccessibilityCollectingVisitor returns visitor itself instead of
  *   throwing NotSupprotedException, when Visit method is called with
  *   ReflectionElements which does not have any accessiblities.
+ *   
+ * - [NEW] Adds RestrictingReferenceAssertion to verify referenced assemblies.
+ * 
+ *   [Fact]
+ *   public void Demo()
+ *   {
+ *       var assertion = new RestrictingReferenceAssertion();
+ *       assertion.Visit(typeof(IEnumerable<object>).Assembly.ToElement());
+ *   }  
  */
