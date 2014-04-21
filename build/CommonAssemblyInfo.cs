7dcf2c46a61ae461a86c33427fae9c45659cbca5
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-21 15:29:08 +0900

    build: increment patch version,
    to indicate that this fixes missing the Mono.Reflection.dll reference.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,28 +10,11 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.13.0")]
-[assembly: AssemblyInformationalVersion("0.13.0")]
+[assembly: AssemblyVersion("0.13.1")]
+[assembly: AssemblyInformationalVersion("0.13.1")]
 
 /*
- * Version 0.13.0
+ * Version 0.13.1
  * 
- * - [FIX] Changes constructor accessiblity of AccessibilityCollectingVisitor
- *   from private to protected.
- *
- * - [FIX] AccessibilityCollectingVisitor returns visitor itself instead of
- *   throwing NotSupprotedException, when Visit method is called with
- *   ReflectionElements which does not have any accessiblities.
- *   
- * - [NEW] Adds RestrictingReferenceAssertion to verify referenced assemblies.
- * 
- *   [Fact]
- *   public void Demo()
- *   {
- *       var assertion = new RestrictingReferenceAssertion();
- *       assertion.Visit(typeof(IEnumerable<object>).Assembly.ToElement());
- *   }  
- *   
- * - [FIX] Splits the Jwc.Experiment.Idioms namespace to Jwc.Experiment and
- *   Jwc.Experiment.Idioms. (BREAKING-CHANGE)
+ * - [FIX] Fixes missing the Mono.Reflection.dll reference.
  */
