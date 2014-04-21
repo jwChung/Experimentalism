d4bea792e4bdad27d44c8713bc1604dfc8e553ac
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-21 14:49:46 +0900

    build: increment minor version,
    to indicate that RestrictingReferenceAssertion is a new feature and
    several fixes are addressed.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.12.0")]
-[assembly: AssemblyInformationalVersion("0.12.0")]
+[assembly: AssemblyVersion("0.13.0")]
+[assembly: AssemblyInformationalVersion("0.13.0")]
 
 /*
  * Version 0.13.0
