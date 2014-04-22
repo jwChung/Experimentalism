1abd4dc160c8b22e746b667440fb7906308ce850
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-22 11:31:53 +0900

    docs: adds release note for Verify(Assembly).

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -14,8 +14,20 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyInformationalVersion("0.13.1")]
 
 /*
- * Version 0.13.2
+ * Version 0.14.0
  * 
  * - [FIX] Makes ReferenceCollectingVisitor tread-safe and improves its
  *   performance.
+ *   
+ * - [NEW] Introduces the new RestrictingReferenceAssertion.Verify(Assembly)
+ *   method to be used to verify that all references of a given assembly are
+ *   correctly specified, instead of using
+ *   RestrictingReferenceAssertion.Visit(AssemblyElement).
+ *   
+ *   [Fact]
+ *   public void Demo()
+ *   {
+ *       var assertion = new RestrictingReferenceAssertion();
+ *       assertion.Verify(typeof(IEnumerable&lt;object&gt;).Assembly);
+ *   }
  */
