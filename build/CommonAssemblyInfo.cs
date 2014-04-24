bddda9854a0c8f29f3b55562efd3a4be97f8ff84
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-24 16:51:08 +0900

    build: increment minor version,
    to indicate that the GuardClauseAssertion class is a new feature.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.14.0")]
-[assembly: AssemblyInformationalVersion("0.14.0")]
+[assembly: AssemblyVersion("0.15.0")]
+[assembly: AssemblyInformationalVersion("0.15.0")]
 
 /*
  * Version 0.15.0
