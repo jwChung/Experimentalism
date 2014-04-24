9acd4518f298aa020f396e6aa9c9287804e20de7
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-25 01:13:24 +0900

    dev: implements the Visit(AssemblyElement) method.

--- src/Experiment.Idioms/DisplayNameVisitor.cs
+++ src/Experiment.Idioms/DisplayNameVisitor.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using Ploeh.Albedo;
 
 namespace Jwc.Experiment
@@ -8,15 +11,54 @@ namespace Jwc.Experiment
     /// </summary>
     public class DisplayNameVisitor : ReflectionVisitor<IEnumerable<string>>
     {
+        private readonly IEnumerable<string> _displayNames;
+
         /// <summary>
-        /// Gets the observation or value produced by this instance.
+        /// Initializes a new instance of the <see cref="DisplayNameVisitor"/> class.
+        /// </summary>
+        public DisplayNameVisitor() : this(new string[0])
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DisplayNameVisitor"/> class.
+        /// </summary>
+        /// <param name="displayNames">
+        /// The collected display names.
+        /// </param>
+        protected DisplayNameVisitor(IEnumerable<string> displayNames)
+        {
+            _displayNames = displayNames;
+        }
+
+        /// <summary>
+        /// Gets a value indicating the collected dispaly names.
         /// </summary>
         public override IEnumerable<string> Value
         {
             get
             {
-                throw new System.NotImplementedException();
+                return _displayNames;
             }
+        }
+
+        /// <summary>
+        /// Visits the specified assembly element to collect a dispaly name of
+        /// an assembly element.
+        /// </summary>
+        /// <param name="assemblyElement">
+        /// The assembly element whose display name is collected.
+        /// </param>
+        /// <returns>
+        /// The visitor which collected a display name.
+        /// </returns>
+        public override IReflectionVisitor<IEnumerable<string>> Visit(
+            AssemblyElement assemblyElement)
+        {
+            if (assemblyElement == null)
+                throw new ArgumentNullException("assemblyElement");
+
+            return new DisplayNameVisitor(Value.Concat(new[] { assemblyElement.ToString() }));
         }
     }
 }
--- test/Experiment.IdiomsUnitTest/DisplayNameVisitorTest.cs
+++ test/Experiment.IdiomsUnitTest/DisplayNameVisitorTest.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Ploeh.Albedo;
 using Xunit;
 
@@ -11,6 +13,45 @@ namespace Jwc.Experiment
         {
             var sut = new DisplayNameVisitor();
             Assert.IsAssignableFrom<IReflectionVisitor<IEnumerable<string>>>(sut);
+        }
+
+        [Fact]
+        public void ValueIsCorrect()
+        {
+            var sut = new DisplayNameVisitor();
+            var actual = sut.Value;
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void VisitAssemblyElementCollectsCorrectName()
+        {
+            var sut = new DisplayNameVisitor();
+            var assembly = typeof(object).Assembly;
+            var expected = assembly.ToElement().ToString();
+
+            var actual = sut.Visit(assembly.ToElement());
+
+            Assert.Equal(expected, actual.Value.Single());
+        }
+
+        [Fact]
+        public void VisitNullAssemblyElementThrows()
+        {
+            var sut = new DisplayNameVisitor();
+            Assert.Throws<ArgumentNullException>(() => sut.Visit((AssemblyElement)null));
+        }
+
+        [Fact]
+        public void VisitManyAssemblyElementsCollectsCorrectNames()
+        {
+            var sut = new DisplayNameVisitor();
+            var assembly = typeof(object).Assembly;
+
+            var actual1 = sut.Visit(assembly.ToElement());
+            var actual2 = actual1.Visit(assembly.ToElement());
+
+            Assert.Equal(2, actual2.Value.Count());
         }
     }
 }
