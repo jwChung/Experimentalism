f7e32c1c064b437c8fd95066675e788f32a93f4d
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-24 15:46:23 +0900

    dev: lets GuardClauseAssertion accept only relevant elements.

--- src/Experiment.Idioms/Idioms/GuardClauseAssertion.cs
+++ src/Experiment.Idioms/Idioms/GuardClauseAssertion.cs
@@ -1,4 +1,8 @@
-﻿namespace Jwc.Experiment.Idioms
+﻿using System.Collections.Generic;
+using System.Linq;
+using Ploeh.Albedo;
+
+namespace Jwc.Experiment.Idioms
 {
     /// <summary>
     /// Encapsulates a unit test that verifies that a method or constructor has
@@ -6,6 +10,8 @@
     /// </summary>
     public class GuardClauseAssertion : AssertionAdapter
     {
+        private readonly AccessibilityCollectingVisitor _accessibilityCollectingVisitor
+            = new AccessibilityCollectingVisitor();
         private readonly ITestFixture _testFixture;
 
         /// <summary>
@@ -29,6 +35,87 @@
             {
                 return _testFixture;
             }
+        }
+
+        /// <summary>
+        /// Allows <see cref="TypeElement" /> instances to be 'visited'.
+        /// This method is called when the elements 'accepts' this visitor instance.
+        /// </summary>
+        /// <param name="typeElements">
+        /// The <see cref="TypeElement" /> instances being visited.
+        /// </param>
+        /// <returns>
+        /// A (potentially) new <see cref="IReflectionVisitor{T}" /> instance which can be
+        /// used to continue the visiting process with potentially updated observations.
+        /// </returns>
+        public override IReflectionVisitor<object> Visit(
+            params TypeElement[] typeElements)
+        {
+            return base.Visit(GetPublicReflectionElements(typeElements));
+        }
+
+        /// <summary>
+        /// Allows <see cref="ConstructorInfoElement" /> instances to be 'visited'.
+        /// This method is called when the elements 'accepts' this visitor instance.
+        /// </summary>
+        /// <param name="constructorInfoElements">
+        /// The <see cref="ConstructorInfoElement" /> instances being visited.
+        /// </param>
+        /// <returns>
+        /// A (potentially) new <see cref="IReflectionVisitor{T}" /> instance which can be
+        /// used to continue the visiting process with potentially updated observations.
+        /// </returns>
+        public override IReflectionVisitor<object> Visit(
+            params ConstructorInfoElement[] constructorInfoElements)
+        {
+            return base.Visit(GetPublicReflectionElements(constructorInfoElements));
+        }
+
+        /// <summary>
+        /// Allows <see cref="PropertyInfoElement"/> instances to be 'visited'.
+        /// This method is called when the elements 'accepts' this visitor instance.
+        /// </summary>
+        /// <param name="propertyInfoElements">
+        /// The <see cref="PropertyInfoElement"/> instances being visited.
+        /// </param>
+        /// <returns>
+        /// A (potentially) new <see cref="IReflectionVisitor{T}"/> instance which can be
+        /// used to continue the visiting process with potentially updated observations.
+        /// </returns>
+        public override IReflectionVisitor<object> Visit(
+            params PropertyInfoElement[] propertyInfoElements)
+        {
+            var elements = propertyInfoElements.Where(e => e.PropertyInfo.GetSetMethod() != null).ToArray();
+            return base.Visit(elements);
+        }
+
+        /// <summary>
+        /// Allows <see cref="MethodInfoElement"/> instances to be 'visited'.
+        /// This method is called when the elements 'accepts' this visitor instance.
+        /// </summary>
+        /// <param name="methodInfoElements">
+        /// The <see cref="MethodInfoElement"/> instances being visited.
+        /// </param>
+        /// <returns>
+        /// A (potentially) new <see cref="IReflectionVisitor{T}"/> instance which can be
+        /// used to continue the visiting process with potentially updated observations.
+        /// </returns>
+        public override IReflectionVisitor<object> Visit(
+            params MethodInfoElement[] methodInfoElements)
+        {
+            return base.Visit(GetPublicReflectionElements(methodInfoElements));
+        }
+
+        private T[] GetPublicReflectionElements<T>(IEnumerable<T> reflectionElements)
+            where T : IReflectionElement
+        {
+            return reflectionElements.Where(IsPublic).ToArray();
+        }
+
+        private bool IsPublic<T>(T e) where T : IReflectionElement
+        {
+            var accessibilities = e.Accept(_accessibilityCollectingVisitor).Value.Single();
+            return (accessibilities & Accessibilities.Public) == Accessibilities.Public;
         }
     }
 }
--- test/Experiment.IdiomsUnitTest/Idioms/GuardClauseAssertionTest.cs
+++ test/Experiment.IdiomsUnitTest/Idioms/GuardClauseAssertionTest.cs
@@ -1,9 +1,18 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Moq;
+using Ploeh.Albedo;
+using Xunit;
 
 namespace Jwc.Experiment.Idioms
 {
     public class GuardClauseAssertionTest
     {
+        private const BindingFlags _bindingFlags =
+           BindingFlags.Static | BindingFlags.Instance |
+           BindingFlags.Public | BindingFlags.NonPublic;
+
         [Fact]
         public void SutIsAssertionAdapter()
         {
@@ -33,6 +42,112 @@ namespace Jwc.Experiment.Idioms
             var actual = sut.TestFixture;
 
             Assert.Equal(testFixture, actual);
+        }
+
+        [Fact]
+        public void VisitTypeElementsPassesPublicElementsToBaseMethod()
+        {
+            // Fixture setup
+            var sut = new Mock<GuardClauseAssertion>(new DelegatingTestFixture()) { CallBase = true }.Object;
+            var visitor = new Mock<GuardClauseAssertion>(new DelegatingTestFixture()) { CallBase = true }.Object;
+
+            var expectedElements = new List<TypeElement>();
+            sut.ToMock().Setup(x => x.Visit(It.IsAny<TypeElement>())).Returns(visitor)
+                .Callback<TypeElement>(expectedElements.Add);
+            visitor.ToMock().Setup(x => x.Visit(It.IsAny<TypeElement>())).Returns(visitor)
+                .Callback<TypeElement>(expectedElements.Add);
+            var typeElements = typeof(object).Assembly.GetTypes().Select(t => t.ToElement()).ToArray();
+
+            // Exercise system
+            var actual = sut.Visit(typeElements);
+
+            // Verify outcome
+            Assert.Equal(visitor, actual);
+            AssertArePublicElements(expectedElements);
+        }
+
+        [Fact]
+        public void VisitConstructorInfoElementsPassesPublicElementsToBaseMethod()
+        {
+            // Fixture setup
+            var sut = new Mock<GuardClauseAssertion>(new DelegatingTestFixture()) { CallBase = true }.Object;
+            var visitor = new Mock<GuardClauseAssertion>(new DelegatingTestFixture()) { CallBase = true }.Object;
+
+            var expectedElements = new List<ConstructorInfoElement>();
+            sut.ToMock().Setup(x => x.Visit(It.IsAny<ConstructorInfoElement>())).Returns(visitor)
+                .Callback<ConstructorInfoElement>(expectedElements.Add);
+            visitor.ToMock().Setup(x => x.Visit(It.IsAny<ConstructorInfoElement>())).Returns(visitor)
+                .Callback<ConstructorInfoElement>(expectedElements.Add);
+            var constructorInfoElements = typeof(TypeWithMembers).GetConstructors(_bindingFlags)
+                .Select(c => c.ToElement()).ToArray();
+
+            // Exercise system
+            var actual = sut.Visit(constructorInfoElements);
+
+            // Verify outcome
+            Assert.Equal(visitor, actual);
+            AssertArePublicElements(expectedElements);
+        }
+
+        [Fact]
+        public void VisitPropertyInfoElementsPassesPublicWritableElementsToBaseMethod()
+        {
+            // Fixture setup
+            var sut = new Mock<GuardClauseAssertion>(new DelegatingTestFixture()) { CallBase = true }.Object;
+            var visitor = new Mock<GuardClauseAssertion>(new DelegatingTestFixture()) { CallBase = true }.Object;
+
+            var expectedElements = new List<PropertyInfoElement>();
+            sut.ToMock().Setup(x => x.Visit(It.IsAny<PropertyInfoElement>())).Returns(visitor)
+                .Callback<PropertyInfoElement>(expectedElements.Add);
+            visitor.ToMock().Setup(x => x.Visit(It.IsAny<PropertyInfoElement>())).Returns(visitor)
+                .Callback<PropertyInfoElement>(expectedElements.Add);
+            var propertyInfoElements = typeof(TypeWithMembers).GetProperties(_bindingFlags)
+                .Select(c => c.ToElement()).ToArray();
+
+            // Exercise system
+            var actual = sut.Visit(propertyInfoElements);
+
+            // Verify outcome
+            Assert.Equal(visitor, actual);
+            AssertArePublicElements(expectedElements);
+            Assert.True(expectedElements.All(e => e.PropertyInfo.GetSetMethod() != null));
+        }
+
+        [Fact]
+        public void VisitMethodInfoElementsPassesPublicElementsToBaseMethod()
+        {
+            // Fixture setup
+            var sut = new Mock<GuardClauseAssertion>(new DelegatingTestFixture()) { CallBase = true }.Object;
+            var visitor = new Mock<GuardClauseAssertion>(new DelegatingTestFixture()) { CallBase = true }.Object;
+
+            var expectedElements = new List<MethodInfoElement>();
+            sut.ToMock().Setup(x => x.Visit(It.IsAny<MethodInfoElement>())).Returns(visitor)
+                .Callback<MethodInfoElement>(expectedElements.Add);
+            visitor.ToMock().Setup(x => x.Visit(It.IsAny<MethodInfoElement>())).Returns(visitor)
+                .Callback<MethodInfoElement>(expectedElements.Add);
+            var methodInfoElements = typeof(TypeWithMembers).GetMethods(_bindingFlags)
+                .Select(c => c.ToElement()).ToArray();
+
+            // Exercise system
+            var actual = sut.Visit(methodInfoElements);
+
+            // Verify outcome
+            Assert.Equal(visitor, actual);
+            AssertArePublicElements(expectedElements);
+        }
+
+        private static void AssertArePublicElements(IEnumerable<IReflectionElement> reflectionElements)
+        {
+            var result = reflectionElements.ToArray();
+
+            Assert.True(
+                result.ToArray().All(e => (GetAccessibilities(e) & Accessibilities.Public) == Accessibilities.Public),
+                "Public");
+        }
+
+        private static Accessibilities GetAccessibilities(IReflectionElement e)
+        {
+            return e.Accept(new AccessibilityCollectingVisitor()).Value.Single();
         }
     }
 }
