ee6cb8e125881dccafeb6458af86691e64e99325
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-25 00:34:01 +0900

    dev: introduces DisplayNameVisitor.

---
+++ src/Experiment.Idioms/DisplayNameVisitor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Ploeh.Albedo;
+
+namespace Jwc.Experiment
+{
+    /// <summary>
+    /// Represetns a display name of a reflection member.
+    /// </summary>
+    public class DisplayNameVisitor : ReflectionVisitor<IEnumerable<string>>
+    {
+        /// <summary>
+        /// Gets the observation or value produced by this instance.
+        /// </summary>
+        public override IEnumerable<string> Value
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/DisplayNameVisitorTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Ploeh.Albedo;
+using Xunit;
+
+namespace Jwc.Experiment
+{
+    public class DisplayNameVisitorTest
+    {
+        [Fact]
+        public void SutIsReflectionVisitor()
+        {
+            var sut = new DisplayNameVisitor();
+            Assert.IsAssignableFrom<IReflectionVisitor<IEnumerable<string>>>(sut);
+        }
+    }
+}
