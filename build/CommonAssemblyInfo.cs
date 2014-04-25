e3dcbbbe4c342964fb0247c3c924e0726c2b0cfa
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-25 10:38:57 +0900

    build: increment patch version,
    to indicate that this improves a display name of a member verified by
    idiomatic test.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.15.1")]
-[assembly: AssemblyInformationalVersion("0.15.1")]
+[assembly: AssemblyVersion("0.15.2")]
+[assembly: AssemblyInformationalVersion("0.15.2")]
 
 /*
  * Version 0.15.2
