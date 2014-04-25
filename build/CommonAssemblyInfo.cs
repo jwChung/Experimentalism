5eb248b8b26822a88cfd69b4ed471d2072543562
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-25 16:28:07 +0900

    build: increment patch version,
    to indicate that this fixes excluding interfaces from verifying
    `ConstructingMemberAssertion` and `GuardClauseAssertion`.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.15.3")]
-[assembly: AssemblyInformationalVersion("0.15.3")]
+[assembly: AssemblyVersion("0.15.4")]
+[assembly: AssemblyInformationalVersion("0.15.4")]
 
 /*
  * Version 0.15.4
