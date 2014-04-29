68fc683739d895e4c5a0378b5a7d94d7ca0cbd97
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-29 21:58:13 +0900

    dev: introduces the IIdiomaticMemberAssertion interface.

---
+++ src/Experiment.Idioms/IIdiomaticMemberAssertion.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace Jwc.Experiment
+{
+    /// <summary>
+    /// Represents an encapsulation of an idiomatic unit test assertion based on
+    /// <see cref="MemberInfo"/>.
+    /// </summary>
+    public interface IIdiomaticMemberAssertion
+    {
+        /// <summary>
+        /// Verifies that the idiomatic assertion can be verified for the
+        /// specified member.
+        /// </summary>
+        /// <param name="member">The member.</param>
+        void Verify(MemberInfo member);
+    }
+}
