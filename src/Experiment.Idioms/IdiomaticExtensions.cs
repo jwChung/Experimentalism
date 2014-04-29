5e680ed22add5e111082f1e9c1d4c129d0ba8264
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-29 23:20:27 +0900

    dev: implements GetIdiomaticMembers method.

---
+++ src/Experiment.Idioms/IdiomaticExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Jwc.Experiment
+{
+    /// <summary>
+    /// Defines idiomatic extension methods.
+    /// </summary>
+    public static class IdiomaticExtensions
+    {
+        /// <summary>
+        /// Gets idiomatic members.
+        /// </summary>
+        /// <param name="type">A type to enumerate the idiomatic members.</param>
+        /// <returns>The idiomatic members.</returns>
+        public static IEnumerable<MemberInfo> GetIdiomaticMembers(this Type type)
+        {
+            return new IdiomaticMembers(type);
+        }
+
+        /// <summary>
+        /// Gets idiomatic members corresponding to member kinds.
+        /// </summary>
+        /// <param name="type">A type to enumerate the idiomatic members.</param>
+        /// <param name="memberKinds">The member kinds.</param>
+        /// <returns>The idiomatic members.</returns>
+        public static IEnumerable<MemberInfo> GetIdiomaticMembers(this Type type, MemberKinds memberKinds)
+        {
+            return new IdiomaticMembers(type, memberKinds);
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/IdiomaticExtensionsTest.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace Jwc.Experiment
+{
+    public class IdiomaticExtensionsTest
+    {
+        [Fact]
+        public void GetIdiomaticMembersReturnsCorrectMembers()
+        {
+            var type = typeof(TypeWithMembers);
+
+            var actual = type.GetIdiomaticMembers();
+
+            var members = Assert.IsAssignableFrom<IdiomaticMembers>(actual);
+            Assert.Equal(type, members.Type);
+            Assert.Equal(MemberKinds.All, members.MemberKinds);
+        }
+
+        [Fact]
+        public void GetIdiomaticMembersWithMemberKindsReturnsCorrectMembers()
+        {
+            var type = typeof(TypeWithMembers);
+            var memberKinds = MemberKinds.Event;
+
+            var actual = type.GetIdiomaticMembers(memberKinds);
+
+            var members = Assert.IsAssignableFrom<IdiomaticMembers>(actual);
+            Assert.Equal(type, members.Type);
+            Assert.Equal(memberKinds, members.MemberKinds);
+        }
+    }
+}
