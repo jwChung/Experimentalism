99809e0b7f92e3f8e4b0c629d2d6bb610bfa0773
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-30 09:58:48 +0900

    dev: implements NullGuardClauseAssertion.

---
+++ src/Experiment.Idioms/Idioms2/NullGuardClauseAssertion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+using Ploeh.AutoFixture.Idioms;
+
+namespace Jwc.Experiment.Idioms2
+{
+    /// <summary>
+    /// Encapsulates a unit test that verifies that a method or constructor has
+    /// appropriate Null Guard Clauses in place.
+    /// </summary>
+    public class NullGuardClauseAssertion : IIdiomaticMemberAssertion
+    {
+        private readonly ITestFixture _testFixture;
+        private readonly IIdiomaticAssertion _assertion;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NullGuardClauseAssertion"/> class.
+        /// </summary>
+        /// <param name="testFixture">
+        /// A test fixture to create auto-data.
+        /// </param>
+        public NullGuardClauseAssertion(ITestFixture testFixture)
+        {
+            if (testFixture == null)
+                throw new ArgumentNullException("testFixture");
+
+            _testFixture = testFixture;
+            _assertion = new GuardClauseAssertion(new SpecimenBuilder(testFixture));
+        }
+
+        /// <summary>
+        /// Gets a value indicating the test fixture.
+        /// </summary>
+        public ITestFixture TestFixture
+        {
+            get
+            {
+                return _testFixture;
+            }
+        }
+
+        /// <summary>
+        /// Verifies that a member has appropriate Null Guard Clause in place.
+        /// </summary>
+        /// <param name="member">
+        /// The member.
+        /// </param>
+        public void Verify(MemberInfo member)
+        {
+            _assertion.Verify(member);
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/Idioms2/NullGuardClauseAssertionTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Ploeh.Albedo;
+using Ploeh.AutoFixture.Idioms;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms2
+{
+    public class NullGuardClauseAssertionTest
+    {
+        [Fact]
+        public void SutIsIdiomaticMemberAssertion()
+        {
+            var sut = new NullGuardClauseAssertion(new DelegatingTestFixture());
+            Assert.IsAssignableFrom<IIdiomaticMemberAssertion>(sut);
+        }
+
+        [Fact]
+        public void InitializeWithNullTestFixtureThrows()
+        {
+            Assert.Throws<ArgumentNullException>(() => new NullGuardClauseAssertion(null));
+        }
+
+        [Fact]
+        public void TestFixtureIsCorrect()
+        {
+            var testFixture = new DelegatingTestFixture();
+            var sut = new NullGuardClauseAssertion(testFixture);
+
+            var actual = sut.TestFixture;
+
+            Assert.Equal(testFixture, actual);
+        }
+
+        [Fact]
+        public void VerityGuardedMethodDoesNotThrow()
+        {
+            var sut = new NullGuardClauseAssertion(new FakeTestFixture());
+            var guardedMethod = new Methods<ClassForGuardClause>().Select(x => x.GuardedMethod(null, null));
+            Assert.DoesNotThrow(() => sut.Verify(guardedMethod));
+        }
+
+        [Fact]
+        public void VerityUnguardedMethodThrows()
+        {
+            var sut = new NullGuardClauseAssertion(new FakeTestFixture());
+            var unguardedMethod = new Methods<ClassForGuardClause>().Select(x => x.UnguardedMethod(null, null));
+            Assert.Throws<GuardClauseException>(() => sut.Verify(unguardedMethod));
+        }
+
+        private class ClassForGuardClause
+        {
+            public void GuardedMethod(string arg1, object arg2)
+            {
+                if (arg1 == null)
+                    throw new ArgumentNullException("arg1");
+
+                if (arg2 == null)
+                    throw new ArgumentNullException("arg2");
+            }
+
+            public void UnguardedMethod(string arg1, object arg2)
+            {
+                if (arg1 == null)
+                    throw new ArgumentNullException("arg1");
+            }
+        }
+    }
+}
