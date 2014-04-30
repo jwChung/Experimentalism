47bf74094e89e8af338465361516239e7bb42972
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-30 14:03:36 +0900

    dev: implements MemberInitializationAssertion.

---
+++ src/Experiment.Idioms/Idioms2/MemberInitializationAssertion.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using Ploeh.Albedo;
+
+namespace Jwc.Experiment.Idioms2
+{
+    /// <summary>
+    /// Encapsulates a unit test that verifies that members (property or field)
+    /// are correctly intialized by a constructor.
+    /// </summary>
+    public class MemberInitializationAssertion : IdiomaticMemberAssertion
+    {
+        private readonly ITestFixture _testFixture;
+        private readonly IEqualityComparer<IReflectionElement> _parameterToMemberComparer;
+        private readonly IEqualityComparer<IReflectionElement> _memberToParameterComparer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MemberInitializationAssertion" /> class.
+        /// </summary>
+        /// <param name="testFixture">
+        /// A test fixture to crete auto-data.
+        /// </param>
+        public MemberInitializationAssertion(ITestFixture testFixture) : this(
+            new OrEqualityComparer<IReflectionElement>(
+                new ParameterToPropertyComparer(testFixture),
+                new ParameterToFieldComparer(testFixture)),
+            new OrEqualityComparer<IReflectionElement>(
+                new PropertyToParameterComparer(testFixture),
+                new FieldToParameterComparer(testFixture)))
+        {
+            _testFixture = testFixture;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MemberInitializationAssertion" /> class.
+        /// </summary>
+        /// <param name="parameterToMemberComparer">
+        /// A comparer to determine if a given parameter value of
+        /// a constructor equals to a member(property or field) value.
+        /// </param>
+        /// <param name="memberToParameterComparer">
+        /// A comparer to determine if a given member(property or field) value
+        /// equals to a parameter value of a constructor.
+        /// </param>
+        public MemberInitializationAssertion(
+            IEqualityComparer<IReflectionElement> parameterToMemberComparer,
+            IEqualityComparer<IReflectionElement> memberToParameterComparer)
+        {
+            if (parameterToMemberComparer == null)
+                throw new ArgumentNullException("parameterToMemberComparer");
+
+            if (memberToParameterComparer == null)
+                throw new ArgumentNullException("memberToParameterComparer");
+
+            _parameterToMemberComparer = parameterToMemberComparer;
+            _memberToParameterComparer = memberToParameterComparer;
+        }
+
+        /// <summary>
+        /// Gets a value indicating the test fixture.
+        /// </summary>
+        public ITestFixture TestFixture
+        {
+            get
+            {
+                return _testFixture;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value to compare a parameter with a member(field or propety).
+        /// </summary>
+        public IEqualityComparer<IReflectionElement> ParameterToMemberComparer
+        {
+            get
+            {
+                return _parameterToMemberComparer;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value to compare a member(field or propety) with a parameter.
+        /// </summary>
+        public IEqualityComparer<IReflectionElement> MemberToParameterComparer
+        {
+            get
+            {
+                return _memberToParameterComparer;
+            }
+        }
+
+        /// <summary>
+        /// Verifies that a field correctly exposes a certain parameter of a
+        /// constructor.
+        /// </summary>
+        /// <param name="field">The field.</param>
+        public override void Verify(FieldInfo field)
+        {
+            var fieldInfoElement = field.ToElement();
+            var parameterInfoElements = GetParameterInfoElements(field.ReflectedType);
+
+            if (parameterInfoElements.Any(p => MemberToParameterComparer.Equals(fieldInfoElement, p)))
+                return;
+
+            throw new MemberInitializationException(
+                string.Format(
+                    CultureInfo.CurrentCulture,
+                    "No constructors with an argument that matches the field were found.{0}" +
+                    "Declaring type: {1}{0}" +
+                    "Reflected type: {2}{0}" +
+                    "Field: {3}",
+                    Environment.NewLine,
+                    field.DeclaringType,
+                    field.ReflectedType,
+                    fieldInfoElement));
+        }
+
+        /// <summary>
+        /// Verifies that all parameters of a constructor are correctly exposed
+        /// through fields or properties.
+        /// </summary>
+        /// <param name="constructor">
+        /// The constructor.
+        /// </param>
+        public override void Verify(ConstructorInfo constructor)
+        {
+            if (constructor == null)
+                throw new ArgumentNullException("constructor");
+
+            var parameterInfoElements = GetParameterInfoElements(constructor);
+            var memberElements = GetMemberElements(constructor.ReflectedType).ToArray();
+
+            foreach (var parameterInfoElement in parameterInfoElements)
+            {
+                if (memberElements.Any(m => ParameterToMemberComparer.Equals(parameterInfoElement, m)))
+                    continue;
+
+                throw new MemberInitializationException(
+                    string.Format(
+                        CultureInfo.CurrentCulture,
+                        "The constructor parameter was not exposed through any fields or properties.{0}" +
+                        "Type: {1}{0}" +
+                        "Constructor: {2}{0}" +
+                        "Parameter: {3}",
+                        Environment.NewLine,
+                        constructor.DeclaringType,
+                        constructor,
+                        parameterInfoElement.ParameterInfo));
+            }
+        }
+
+        /// <summary>
+        /// Verifies that a field correctly exposes a certain parameter of a
+        /// constructor.
+        /// </summary>
+        /// <param name="property">The property.</param>
+        public override void Verify(PropertyInfo property)
+        {
+            var propertyInfoElement = property.ToElement();
+            var parameterInfoElements = GetParameterInfoElements(property.ReflectedType);
+
+            if (parameterInfoElements.Any(p => MemberToParameterComparer.Equals(propertyInfoElement, p)))
+                return;
+
+            throw new MemberInitializationException(
+                string.Format(
+                    CultureInfo.CurrentCulture,
+                    "No constructors with an argument that matches the property were found.{0}" +
+                    "Declaring type: {1}{0}" +
+                    "Reflected type: {2}{0}" +
+                    "Property: {3}",
+                    Environment.NewLine,
+                    property.DeclaringType,
+                    property.ReflectedType,
+                    propertyInfoElement));
+        }
+
+        private static IEnumerable<ParameterInfoElement> GetParameterInfoElements(Type reflectedType)
+        {
+            return reflectedType.GetConstructors().SelectMany(GetParameterInfoElements);
+        }
+
+        private static IEnumerable<ParameterInfoElement> GetParameterInfoElements(ConstructorInfo constructor)
+        {
+            return constructor.GetParameters().Select(x => x.ToElement());
+        }
+
+        private static IEnumerable<IReflectionElement> GetMemberElements(Type reflectedType)
+        {
+            return GetPropertyInfoElements(reflectedType)
+                .Concat(GetFieldInfoElements(reflectedType));
+        }
+
+        private static IEnumerable<IReflectionElement> GetPropertyInfoElements(Type reflectedType)
+        {
+            return reflectedType.GetFields().Select(x => x.ToElement());
+        }
+
+        private static IEnumerable<IReflectionElement> GetFieldInfoElements(Type reflectedType)
+        {
+            return reflectedType.GetProperties().Select(x => x.ToElement());
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/Idioms2/MemberInitializationAssertionTest.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Ploeh.Albedo;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Jwc.Experiment.Idioms2
+{
+    public class MemberInitializationAssertionTest
+    {
+        [Fact]
+        public void SutIsReflectionVisitor()
+        {
+            var sut = new MemberInitializationAssertion(
+                EqualityComparer<IReflectionElement>.Default,
+                EqualityComparer<IReflectionElement>.Default);
+            Assert.IsAssignableFrom<IIdiomaticMemberAssertion>(sut);
+        }
+
+        [Fact]
+        public void ParameterToMemberComparerIsCorrect()
+        {
+            var constructorComparer = new DelegatingReflectionElementComparer();
+            var sut = new MemberInitializationAssertion(
+                   constructorComparer,
+                   EqualityComparer<IReflectionElement>.Default);
+
+            var actual = sut.ParameterToMemberComparer;
+
+            Assert.Equal(constructorComparer, actual);
+        }
+
+        [Fact]
+        public void MemberToParameterComparerIsCorrect()
+        {
+            var memberComparer = EqualityComparer<IReflectionElement>.Default;
+            var sut = new MemberInitializationAssertion(
+                   EqualityComparer<IReflectionElement>.Default,
+                   memberComparer);
+
+            var actual = sut.MemberToParameterComparer;
+
+            Assert.Equal(memberComparer, actual);
+        }
+
+        [Fact]
+        public void InitializeWithNullParameterToMemberComparerThrows()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new MemberInitializationAssertion(
+                    null,
+                    EqualityComparer<IReflectionElement>.Default));
+        }
+
+        [Fact]
+        public void InitializeWithNullMemberToParameterComparerThrows()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new MemberInitializationAssertion(
+                    EqualityComparer<IReflectionElement>.Default,
+                    null));
+        }
+
+        [Fact]
+        public void ParameterToMemberComparerIsCorrectWhenInitializedWithTestFixture()
+        {
+            var testFixture = new DelegatingTestFixture();
+            var sut = new MemberInitializationAssertion(testFixture);
+
+            var actual = sut.ParameterToMemberComparer;
+
+            var comparers = Assert.IsAssignableFrom<OrEqualityComparer<IReflectionElement>>(actual)
+                .EqualityComparers.ToArray();
+            Assert.Equal(2, comparers.Length);
+            var comparers1 = Assert.IsAssignableFrom<ParameterToPropertyComparer>(comparers[0]);
+            Assert.Equal(testFixture, comparers1.TestFixture);
+            var comparers2 = Assert.IsAssignableFrom<ParameterToFieldComparer>(comparers[1]);
+            Assert.Equal(testFixture, comparers2.TestFixture);
+        }
+
+        [Fact]
+        public void MemberToParameterComparerIsCorrectWhenInitializedWithTestFixture()
+        {
+            var testFixture = new DelegatingTestFixture();
+            var sut = new MemberInitializationAssertion(testFixture);
+
+            var actual = sut.MemberToParameterComparer;
+
+            var comparers = Assert.IsAssignableFrom<OrEqualityComparer<IReflectionElement>>(actual)
+                .EqualityComparers.ToArray();
+            Assert.Equal(2, comparers.Length);
+            var comparers1 = Assert.IsAssignableFrom<PropertyToParameterComparer>(comparers[0]);
+            Assert.Equal(testFixture, comparers1.TestFixture);
+            var comparers2 = Assert.IsAssignableFrom<FieldToParameterComparer>(comparers[1]);
+            Assert.Equal(testFixture, comparers2.TestFixture);
+        }
+
+        [Fact]
+        public void TestFixtureIsCorrect()
+        {
+            var testFixture = new DelegatingTestFixture();
+            var sut = new MemberInitializationAssertion(testFixture);
+
+            var actual = sut.TestFixture;
+
+            Assert.Equal(testFixture, actual);
+        }
+
+        [Theory]
+        [SatisfiedConstructorData]
+        public void VerifySatisfiedConstructorDoesNotThrow(ConstructorInfo constructor)
+        {
+            var sut = new MemberInitializationAssertion(new FakeTestFixture());
+            Assert.DoesNotThrow(() => sut.Verify(constructor));
+        }
+
+        [Theory]
+        [UnsatisfiedConstructorData]
+        public void VerifyUnsatisfiedConstructorThrows(ConstructorInfo constructor)
+        {
+            var sut = new MemberInitializationAssertion(new FakeTestFixture());
+            Assert.Throws<MemberInitializationException>(() => sut.Verify(constructor));
+        }
+
+        [Theory]
+        [SatisfiedFieldData]
+        public void VerifySatisfiedFieldDoesNotThrow(FieldInfo field)
+        {
+            var sut = new MemberInitializationAssertion(new FakeTestFixture());
+            Assert.DoesNotThrow(() => sut.Verify(field));
+        }
+
+        [Theory]
+        [UnsatisfiedFieldData]
+        public void VerifyUnsatisfiedFieldThrows(FieldInfo field)
+        {
+            var sut = new MemberInitializationAssertion(new FakeTestFixture());
+            Assert.Throws<MemberInitializationException>(() => sut.Verify(field));
+        }
+
+        [Theory]
+        [SatisfiedPropertyData]
+        public void VerifySatisfiedPropertyDoesNotThrow(PropertyInfo property)
+        {
+            var sut = new MemberInitializationAssertion(new FakeTestFixture());
+            Assert.DoesNotThrow(() => sut.Verify(property));
+        }
+
+        [Theory]
+        [UnsatisfiedPropertyData]
+        public void VerifyUnsatisfiedPropertyThrows(PropertyInfo property)
+        {
+            var sut = new MemberInitializationAssertion(new FakeTestFixture());
+            Assert.Throws<MemberInitializationException>(() => sut.Verify(property));
+        }
+
+        [Fact]
+        public void VerifyNullConstructorThrows()
+        {
+            var sut = new MemberInitializationAssertion(new DelegatingTestFixture());
+            Assert.Throws<ArgumentNullException>(() => sut.Verify((ConstructorInfo)null));
+        }
+
+        private class SatisfiedConstructorDataAttribute : DataAttribute
+        {
+            public override IEnumerable<object[]> GetData(MethodInfo methodUnderTest, Type[] parameterTypes)
+            {
+                return typeof(ClassForSatisfiedConstructors).GetConstructors().Select(c => new object[] { c });
+            }
+
+            private class ClassForSatisfiedConstructors
+            {
+                public readonly string StringValue;
+                public readonly int IntValue;
+
+                public ClassForSatisfiedConstructors()
+                {
+                }
+
+                public ClassForSatisfiedConstructors(string stringValue)
+                {
+                    StringValue = stringValue;
+                }
+
+                public ClassForSatisfiedConstructors(string stringValue, int intValue)
+                {
+                    StringValue = stringValue;
+                    IntValue = intValue;
+                }
+
+                public ClassForSatisfiedConstructors(object objectValue, int intValue)
+                {
+                    ObjectValue = objectValue;
+                    IntValue = intValue;
+                }
+
+                public ClassForSatisfiedConstructors(object objectValue, byte byteValue)
+                {
+                    ObjectValue = objectValue;
+                    ByteValue = byteValue;
+                }
+
+                public object ObjectValue
+                {
+                    get;
+                    set;
+                }
+
+                public byte ByteValue
+                {
+                    get;
+                    set;
+                }
+            }
+        }
+
+        private class UnsatisfiedConstructorDataAttribute : DataAttribute
+        {
+            public override IEnumerable<object[]> GetData(MethodInfo methodUnderTest, Type[] parameterTypes)
+            {
+                return typeof(ClassForUnsatisfiedConstructors).GetConstructors().Select(c => new object[] { c });
+            }
+
+            private class ClassForUnsatisfiedConstructors
+            {
+                public readonly string StringValue;
+
+                public ClassForUnsatisfiedConstructors(string stringValue)
+                {
+                }
+
+                public ClassForUnsatisfiedConstructors(string stringValue, int intValue)
+                {
+                    StringValue = stringValue;
+                }
+
+                public ClassForUnsatisfiedConstructors(int intValue, object objectValue)
+                {
+                    ObjectValue = objectValue;
+                }
+
+                public object ObjectValue
+                {
+                    get;
+                    set;
+                }
+            }
+        }
+
+        private class SatisfiedFieldDataAttribute : DataAttribute
+        {
+            public override IEnumerable<object[]> GetData(MethodInfo methodUnderTest, Type[] parameterTypes)
+            {
+                return typeof(ClassForSatisfiedFields).GetFields().Select(c => new object[] { c });
+            }
+
+            private class ClassForSatisfiedFields
+            {
+                public readonly object ObjectValue;
+                public readonly string StringValue;
+                public readonly int IntValue;
+
+                public ClassForSatisfiedFields(object objectValue, string stringValue)
+                {
+                    ObjectValue = objectValue;
+                }
+
+                public ClassForSatisfiedFields(string stringValue, int intValue)
+                {
+                    StringValue = stringValue;
+                    IntValue = intValue;
+                }
+            }
+        }
+
+        private class UnsatisfiedFieldDataAttribute : DataAttribute
+        {
+            public override IEnumerable<object[]> GetData(MethodInfo methodUnderTest, Type[] parameterTypes)
+            {
+                return typeof(ClassForUnsatisfiedFields).GetFields().Select(c => new object[] { c });
+            }
+
+            private class ClassForUnsatisfiedFields
+            {
+                public readonly object ObjectValue;
+                public readonly string StringValue;
+                public readonly int IntValue;
+
+                public ClassForUnsatisfiedFields(object objectValue, string stringValue)
+                {
+                }
+
+                public ClassForUnsatisfiedFields(string stringValue, int intValue)
+                {
+                }
+            }
+        }
+
+        private class SatisfiedPropertyDataAttribute : DataAttribute
+        {
+            public override IEnumerable<object[]> GetData(MethodInfo methodUnderTest, Type[] parameterTypes)
+            {
+                return typeof(ClassForSatisfiedProperties).GetProperties().Select(c => new object[] { c });
+            }
+
+            private class ClassForSatisfiedProperties
+            {
+                private readonly int _intValue;
+
+                public ClassForSatisfiedProperties(object objectValue, string stringValue)
+                {
+                    ObjectValue = objectValue;
+                }
+
+                public ClassForSatisfiedProperties(string stringValue, int intValue)
+                {
+                    StringValue = stringValue;
+                    _intValue = intValue;
+                }
+
+                public object ObjectValue
+                {
+                    get;
+                    set;
+                }
+
+                public string StringValue
+                {
+                    get;
+                    private set;
+                }
+
+                public int IntValue
+                {
+                    get
+                    {
+                        return _intValue;
+                    }
+                }
+            }
+        }
+
+        private class UnsatisfiedPropertyDataAttribute : DataAttribute
+        {
+            public override IEnumerable<object[]> GetData(MethodInfo methodUnderTest, Type[] parameterTypes)
+            {
+                return typeof(ClassForUnsatisfiedProperties).GetProperties().Select(c => new object[] { c });
+            }
+
+            private class ClassForUnsatisfiedProperties
+            {
+                public ClassForUnsatisfiedProperties(object objectValue, string stringValue)
+                {
+                }
+
+                public ClassForUnsatisfiedProperties(string stringValue, int intValue)
+                {
+                }
+
+                public object ObjectValue
+                {
+                    get;
+                    set;
+                }
+
+                public string StringValue
+                {
+                    get;
+                    private set;
+                }
+
+                public int IntValue
+                {
+                    get
+                    {
+                        return 10;
+                    }
+                }
+            }
+        }
+    }
+}
