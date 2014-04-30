b94bf4e4838eabf7da92d34d3cc9a4fddb734ebd
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-30 10:55:38 +0900

    dev: implements IdiomaticMemberAssertion.

---
+++ src/Experiment.Idioms/Idioms2/IdiomaticMemberAssertion.cs
@@ -0,0 +1,83 @@
+﻿using System.Reflection;
+
+namespace Jwc.Experiment.Idioms2
+{
+    /// <summary>
+    /// Base implementation of <see cref="IIdiomaticMemberAssertion"/>.
+    /// </summary>
+    public abstract class IdiomaticMemberAssertion : IIdiomaticMemberAssertion
+    {
+        /// <summary>
+        /// Verifies that the idiomatic assertion can be verified for the
+        /// specified member.
+        /// </summary>
+        /// <param name="member">The member.</param>
+        public void Verify(MemberInfo member)
+        {
+            var field = member as FieldInfo;
+            if (field != null)
+                Verify(field);
+
+            var constructor = member as ConstructorInfo;
+            if (constructor != null)
+                Verify(constructor);
+
+            var property = member as PropertyInfo;
+            if (property != null)
+                Verify(property);
+
+            var method = member as MethodInfo;
+            if (method != null)
+                Verify(method);
+
+            var @event = member as EventInfo;
+            if (@event != null)
+                Verify(@event);
+        }
+
+        /// <summary>
+        /// Verifies that the idiomatic assertion can be verified for the
+        /// specified field.
+        /// </summary>
+        /// <param name="field">The field.</param>
+        public virtual void Verify(FieldInfo field)
+        {
+        }
+
+        /// <summary>
+        /// Verifies that the idiomatic assertion can be verified for the
+        /// specified constructor.
+        /// </summary>
+        /// <param name="constructor">The constructor.</param>
+        public virtual void Verify(ConstructorInfo constructor)
+        {
+        }
+
+        /// <summary>
+        /// Verifies that the idiomatic assertion can be verified for the
+        /// specified property.
+        /// </summary>
+        /// <param name="property">The property.</param>
+        public virtual void Verify(PropertyInfo property)
+        {
+        }
+
+        /// <summary>
+        /// Verifies that the idiomatic assertion can be verified for the
+        /// specified method.
+        /// </summary>
+        /// <param name="method">The method.</param>
+        public virtual void Verify(MethodInfo method)
+        {
+        }
+
+        /// <summary>
+        /// Verifies that the idiomatic assertion can be verified for the
+        /// specified event.
+        /// </summary>
+        /// <param name="event">The event.</param>
+        public virtual void Verify(EventInfo @event)
+        {
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/Idioms2/IdiomaticMemberAssertionTest.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using System.Reflection;
+using Moq;
+using Ploeh.Albedo;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms2
+{
+    public class IdiomaticMemberAssertionTest
+    {
+        [Fact]
+        public void SutIsIdiomaticMemberAssertion()
+        {
+            var sut = new Mock<IdiomaticMemberAssertion> { CallBase = true }.Object;
+            Assert.IsAssignableFrom<IIdiomaticMemberAssertion>(sut);
+        }
+
+        [Fact]
+        public void VerifyFieldMemberCallsVerifyField()
+        {
+            var sut = new Mock<IdiomaticMemberAssertion> { CallBase = true }.Object;
+            var field = new Fields<TypeWithMembers>().Select(x => x.PublicField);
+
+            sut.Verify((MemberInfo)field);
+
+            sut.ToMock().Verify(x => x.Verify(field));
+        }
+
+        [Fact]
+        public void VerifyConstructorMemberCallsVerifyConstructor()
+        {
+            var sut = new Mock<IdiomaticMemberAssertion> { CallBase = true }.Object;
+            var constructor = Constructors.Select(() => new TypeWithMembers());
+
+            sut.Verify((MemberInfo)constructor);
+
+            sut.ToMock().Verify(x => x.Verify(constructor));
+        }
+
+        [Fact]
+        public void VerifyPropertyMemberCallsVerifyProperty()
+        {
+            var sut = new Mock<IdiomaticMemberAssertion> { CallBase = true }.Object;
+            var property = new Properties<TypeWithMembers>().Select(x => x.PublicProperty);
+
+            sut.Verify((MemberInfo)property);
+
+            sut.ToMock().Verify(x => x.Verify(property));
+        }
+
+        [Fact]
+        public void VerifyMethodMemberCallsVerifyMethod()
+        {
+            var sut = new Mock<IdiomaticMemberAssertion> { CallBase = true }.Object;
+            var method = new Methods<TypeWithMembers>().Select(x => x.PublicMethod());
+
+            sut.Verify((MemberInfo)method);
+
+            sut.ToMock().Verify(x => x.Verify(method));
+        }
+
+        [Fact]
+        public void VerifyMethodMemberCallsVerifyEvent()
+        {
+            var sut = new Mock<IdiomaticMemberAssertion> { CallBase = true }.Object;
+            var @event = typeof(TypeWithMembers).GetEvents().First();
+
+            sut.Verify((MemberInfo)@event);
+
+            sut.ToMock().Verify(x => x.Verify(@event));
+        }
+    }
+}
