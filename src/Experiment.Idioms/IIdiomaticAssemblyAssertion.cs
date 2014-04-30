49ae90454359c4c5140820335a2a507634e52559
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-30 20:33:18 +0900

    dev: introduces IIdiomaticAssemblyAssertion.

---
+++ src/Experiment.Idioms/IIdiomaticAssemblyAssertion.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents an encapsulation of an idiomatic unit test assertion based on
+    /// <see cref="Assembly"/>.
+    /// </summary>
+    public interface IIdiomaticAssemblyAssertion
+    {
+        /// <summary>
+        /// Verifies that the idiomatic assertion can be verified for the
+        /// specified assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        void Verify(Assembly assembly);
+    }
+}
