d11d29fd6052856127ad2fd63f5bae0f778afc8e
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-30 16:13:42 +0900

    dev: introduces IIdiomaticTypeAssertion.

---
+++ src/Experiment.Idioms/IIdiomaticTypeAssertion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Represents an encapsulation of an idiomatic unit test assertion based on
+    /// <see cref="Type"/>.
+    /// </summary>
+    public interface IIdiomaticTypeAssertion
+    {
+        /// <summary>
+        /// Verifies that the idiomatic assertion can be verified for the
+        /// specified type.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        void Verify(Type type);
+    }
+}
