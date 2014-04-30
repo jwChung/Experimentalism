7650964b6758ade7ef3bd188651f2889c84fbb43
Jin-Wook Chung <jwChung@hotmail.com>
2014-04-30 14:59:32 +0900

    refactor: adds scenario tests.

--- test/Experiment.IdiomsUnitTest/Scenario.cs
+++ test/Experiment.IdiomsUnitTest/Scenario.cs
@@ -2,35 +2,107 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Ploeh.AutoFixture;
-using Ploeh.AutoFixture.Kernel;
+using Jwc.Experiment.Idioms2;
+using Ploeh.Albedo;
+using Xunit;
 
 namespace Jwc.Experiment
 {
     public class Scenario
     {
-        private class TypeWithGuardTestMembers
+        [Fact]
+        public void VerifyNullGuardClasuseAssertionCorrectlyVerifies()
         {
-            public void NonGuardedMethod(object arg)
+            var assertion = new NullGuardClauseAssertion(new FakeTestFixture());
+
+            typeof(ClassWithGuardedMembers)
+                .GetIdiomaticMembers()
+                .Except(
+                    new MemberInfo[]
+                    {
+                        Constructors.Select(() => new ClassWithGuardedMembers("anonymous")),
+                        new Properties<ClassWithGuardedMembers>().Select(x => x.UnguradedProperty)
+                    })
+                .Verify(assertion);
+        }
+
+        [FirstClassTheorem]
+        public IEnumerable<ITestCase> MembersToTestCasesWithNullGuardClasuseAssertionCorrectlyCreatesTestCases()
+        {
+            var assertion = new NullGuardClauseAssertion(new FakeTestFixture());
+
+            return typeof(ClassWithGuardedMembers)
+                .GetIdiomaticMembers()
+                .Except(
+                    new MemberInfo[]
+                    {
+                        Constructors.Select(() => new ClassWithGuardedMembers("anonymous")),
+                        new Properties<ClassWithGuardedMembers>().Select(x => x.UnguradedProperty)
+                    })
+                .ToTestCases(assertion);
+        }
+
+        [Fact]
+        public void VerifyMemberInitializationAssertionCorrectlyVerifies()
+        {
+            var assertion = new MemberInitializationAssertion(new FakeTestFixture());
+
+            typeof(ClassWithMembersInitializedByConstructor)
+                .GetIdiomaticMembers()
+                .Verify(assertion);
+        }
+
+        [FirstClassTheorem]
+        public IEnumerable<ITestCase> MembersToTestCasesWithMemberInitializationAssertionCorrectlyCreatesTestCases()
+        {
+            var assertion = new MemberInitializationAssertion(new FakeTestFixture());
+
+            return typeof(ClassWithMembersInitializedByConstructor)
+                .GetIdiomaticMembers()
+                .ToTestCases(assertion);
+        }
+
+        private class ClassWithGuardedMembers
+        {
+            public ClassWithGuardedMembers(object arg)
+            {
+                if (arg == null)
+                    throw new ArgumentNullException("arg");
+            }
+
+            public ClassWithGuardedMembers(string arg)
             {
             }
 
-            public void GuardedMethod(object arg)
+            public object Property
+            {
+                set
+                {
+                    if(value == null)
+                        throw new ArgumentNullException("value");
+                }
+            }
+
+            public object UnguradedProperty
+            {
+                get;
+                set;
+            }
+
+            public void Method(object arg)
             {
                 if (arg == null)
-                {
                     throw new ArgumentNullException("arg");
-                }
             }
         }
 
-        private class TypeWithMembersInitializedByConstructor
+        private class ClassWithMembersInitializedByConstructor
         {
             private readonly int _x;
             private readonly IList<object> _y;
             private readonly string _z;
 
-            public TypeWithMembersInitializedByConstructor(int x, object[] y, string z)
+            public ClassWithMembersInitializedByConstructor(int x, object[] y, string z)
             {
                 _x = x;
                 _y = y.ToList();
@@ -66,17 +138,7 @@ namespace Jwc.Experiment
         {
             protected override ITestFixture CreateTestFixture(MethodInfo testMethod)
             {
-                return new CustomTestFixture();
-            }
-        }
-
-        private class CustomTestFixture : ITestFixture
-        {
-            private readonly ISpecimenContext _context = new SpecimenContext(new Fixture());
-
-            public object Create(object request)
-            {
-                return _context.Resolve(request);
+                return new FakeTestFixture();
             }
         }
     }
