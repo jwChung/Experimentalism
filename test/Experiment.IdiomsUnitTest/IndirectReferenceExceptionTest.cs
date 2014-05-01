b1ee7de1eb6cf93f0265184b8b92d3236bcd94f6
Jin-Wook Chung <jwChung@hotmail.com>
2014-05-01 10:41:54 +0900

    dev: indroduces IndirectReferenceException.

---
+++ test/Experiment.IdiomsUnitTest/IndirectReferenceExceptionTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class IndirectReferenceExceptionTest
+    {
+        [Fact]
+        public void SutIsException()
+        {
+            var sut = new IndirectReferenceException();
+            Assert.IsAssignableFrom<Exception>(sut);
+        }
+    }
+}
