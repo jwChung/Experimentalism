9a2d7bc0b52fadff3f22974544517c5c324da6ed
Jin-Wook Chung <jwChung@hotmail.com>
2014-05-01 15:01:18 +0900

    dev: implements IndirectReferenceAssertion.

---
+++ src/Experiment.Idioms/IndirectReferenceAssertion.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Ploeh.Albedo;
+
+namespace Jwc.Experiment.Idioms
+{
+    /// <summary>
+    /// Encapsulates a unit test that verifies that certain assemblies are not
+    /// exposed through API.
+    /// </summary>
+    public class IndirectReferenceAssertion
+        : IdiomaticMemberAssertion, IIdiomaticTypeAssertion, IIdiomaticAssemblyAssertion
+    {
+        private readonly AccessibilityCollector _accessibilityCollector = new AccessibilityCollector();
+        private readonly Assembly[] _indirectReferences;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IndirectReferenceAssertion"/> class.
+        /// </summary>
+        /// <param name="indirectReferences">
+        /// The indirect references which should not be exposed though API.
+        /// </param>
+        public IndirectReferenceAssertion(params Assembly[] indirectReferences)
+        {
+            _indirectReferences = indirectReferences;
+        }
+
+        /// <summary>
+        /// Gets a value indicating the indirect references.
+        /// </summary>
+        public IEnumerable<Assembly> IndirectReferences
+        {
+            get
+            {
+                return _indirectReferences;
+            }
+        }
+
+        /// <summary>
+        /// Verifies that an assembly does not expose the indirect references
+        /// through API.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        public void Verify(Assembly assembly)
+        {
+            var types = assembly.GetTypes().Where(IsExposed);
+
+            foreach (var type in types)
+                Verify(type);
+        }
+
+        /// <summary>
+        /// Verifies that a type does not expose the indirect references
+        /// through API.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        public virtual void Verify(Type type)
+        {
+            var members = new TypeMembers(type, MemberKinds.All, Accessibilities.Public | Accessibilities.Protected);
+
+            foreach (var member in members)
+                Verify(member);
+        }
+
+        /// <summary>
+        /// Verifies that a field does not expose the indirect references
+        /// through API.
+        /// </summary>
+        /// <param name="field">The field.</param>
+        public override void Verify(FieldInfo field)
+        {
+            EnsureNotExpose(field.ToElement());
+        }
+
+        /// <summary>
+        /// Verifies that a constructor does not expose the indirect references
+        /// through API.
+        /// </summary>
+        /// <param name="constructor">The constructor.</param>
+        public override void Verify(ConstructorInfo constructor)
+        {
+            EnsureNotExpose(constructor.ToElement());
+        }
+
+        /// <summary>
+        /// Verifies that a property does not expose the indirect references
+        /// through API.
+        /// </summary>
+        /// <param name="property">The field.</param>
+        public override void Verify(PropertyInfo property)
+        {
+            EnsureNotExpose(property.ToElement());
+        }
+
+        /// <summary>
+        /// Verifies that a method does not expose the indirect references
+        /// through API.
+        /// </summary>
+        /// <param name="method">The method.</param>
+        public override void Verify(MethodInfo method)
+        {
+            EnsureNotExpose(method.ToElement());
+        }
+
+        /// <summary>
+        /// Verifies that an event does not expose the indirect references
+        /// through API.
+        /// </summary>
+        /// <param name="event">The event.</param>
+        public override void Verify(EventInfo @event)
+        {
+            EnsureNotExpose(@event.ToElement());
+        }
+
+        private void EnsureNotExpose(IReflectionElement reflectionElement)
+        {
+            var reference = GetReferences(reflectionElement).FirstOrDefault(r => IndirectReferences.Contains(r));
+
+            if (reference == null)
+                return;
+
+            throw new IndirectReferenceException(
+                string.Format(
+                "The indirect reference should not be exposed through the API.{0}" +
+                "Indirect reference: {1}{0}" + 
+                "API(exposing)     : {2}{0}",
+                Environment.NewLine,
+                reference,
+                reflectionElement.Accept(new DisplayNameVisitor()).Value.Single()));
+        }
+
+        private bool IsExposed(Type type)
+        {
+            return (GetAccessibilities(type) & (Accessibilities.Public | Accessibilities.Protected))
+                != Accessibilities.None;
+        }
+
+        private static IEnumerable<Assembly> GetReferences(IReflectionElement reflectionElement)
+        {
+            return reflectionElement.Accept(new ElementReferenceCollector()).Value;
+        }
+
+        private Accessibilities GetAccessibilities(Type type)
+        {
+            return type.ToElement().Accept(_accessibilityCollector).Value.Single();
+        }
+
+        private class ElementReferenceCollector : ReferenceCollector
+        {
+            protected override void VisitMethodBody(MethodBase methodBase)
+            {
+            }
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/IndirectReferenceAssertionTest.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Moq;
+using Ploeh.Albedo;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class IndirectReferenceAssertionTest
+    {
+        [Fact]
+        public void SutIsIdiomaticMemberAssertion()
+        {
+            var sut = new IndirectReferenceAssertion();
+            Assert.IsAssignableFrom<IIdiomaticMemberAssertion>(sut);
+        }
+
+        [Fact]
+        public void SutIsIdiomaticTypeAssertion()
+        {
+            var sut = new IndirectReferenceAssertion();
+            Assert.IsAssignableFrom<IIdiomaticTypeAssertion>(sut);
+        }
+
+        [Fact]
+        public void SutIsIdiomaticAssemblyAssertion()
+        {
+            var sut = new IndirectReferenceAssertion();
+            Assert.IsAssignableFrom<IIdiomaticAssemblyAssertion>(sut);
+        }
+        
+        [Fact]
+        public void IndirectReferencesIsCorrect()
+        {
+            var indirectReferences = new[]
+            {
+                typeof(object).Assembly,
+                Assembly.Load("Ploeh.AutoFixture")
+            };
+            var sut = new IndirectReferenceAssertion(indirectReferences);
+
+            var actual = sut.IndirectReferences;
+
+            Assert.Equal(indirectReferences, actual);
+        }
+
+        [Fact]
+        public void VerifyFieldDoesNotThrowWhenFieldDoesNotExposeAnyIndirectReference()
+        {
+            // Fixture setup
+            var indirectReferences = new[]
+            {
+                GetType().Assembly
+            };
+            var sut = new IndirectReferenceAssertion(indirectReferences);
+            var field = new Fields<ClassForIndirectReference>().Select(x => x.Field);
+
+            // Exercise system and Verify outcome
+            Assert.DoesNotThrow(() => sut.Verify(field));
+        }
+
+        [Fact]
+        public void VerifyFieldThrowsWhenFieldExposesAnyIndirectReference()
+        {
+            // Fixture setup
+            var indirectReferences = new[]
+            {
+                GetType().Assembly,
+                Assembly.Load("Ploeh.AutoFixture"),
+                typeof(object).Assembly
+            };
+            var sut = new IndirectReferenceAssertion(indirectReferences);
+            var field = new Fields<ClassForIndirectReference>().Select(x => x.Field);
+
+            // Exercise system and Verify outcome
+            var exception = Assert.Throws<IndirectReferenceException>(() => sut.Verify(field));
+            Assert.Contains("Ploeh.AutoFixture", exception.Message);
+        }
+
+        [Fact]
+        public void VerifyConstructorDoesNotThrowWhenConstructorDoesNotExposeAnyIndirectReference()
+        {
+            // Fixture setup
+            var indirectReferences = new[]
+            {
+                GetType().Assembly
+            };
+            var sut = new IndirectReferenceAssertion(indirectReferences);
+            var constructor = Constructors.Select(() => new ClassForIndirectReference(null, null));
+
+            // Exercise system and Verify outcome
+            Assert.DoesNotThrow(() => sut.Verify(constructor));
+        }
+
+        [Fact]
+        public void VerifyConstructorThrowsWhenConstructorExposesAnyIndirectReference()
+        {
+            // Fixture setup
+            var indirectReferences = new[]
+            {
+                GetType().Assembly,
+                Assembly.Load("Jwc.Experiment.Idioms")
+            };
+            var sut = new IndirectReferenceAssertion(indirectReferences);
+            var constructor = Constructors.Select(() => new ClassForIndirectReference(null, null));
+
+            // Exercise system and Verify outcome
+            var exception = Assert.Throws<IndirectReferenceException>(() => sut.Verify(constructor));
+            Assert.Contains("Jwc.Experiment.Idioms", exception.Message);
+        }
+
+        [Fact]
+        public void VerifyFieldIndependentlyVerifies()
+        {
+            // Fixture setup
+            var indirectReferences = new[]
+            {
+                Assembly.Load("Ploeh.AutoFixture")
+            };
+            var sut = new IndirectReferenceAssertion(indirectReferences);
+            var constructor = Constructors.Select(() => new ClassForIndirectReference(null, null));
+            var field = new Fields<ClassForIndirectReference>().Select(x => x.Field);
+
+            // Exercise system and Verify outcome
+            Assert.Throws<IndirectReferenceException>(() => sut.Verify(field));
+            Assert.DoesNotThrow(() => sut.Verify(constructor));
+        }
+
+        [Fact]
+        public void VerifyConstructorDoesNotAddressMethodBody()
+        {
+            // Fixture setup
+            var indirectReferences = new[]
+            {
+                Assembly.Load("Ploeh.AutoFixture")
+            };
+            var sut = new IndirectReferenceAssertion(indirectReferences);
+            var constructor = Constructors.Select(() => new ClassForIndirectReference());
+
+            // Exercise system and Verify outcome
+            Assert.DoesNotThrow(() => sut.Verify(constructor));
+        }
+
+        [Fact]
+        public void VerifyPropertyDoesNotThrowWhenPropertyDoesNotExposeAnyIndirectReference()
+        {
+            // Fixture setup
+            var indirectReferences = new[]
+            {
+                GetType().Assembly
+            };
+            var sut = new IndirectReferenceAssertion(indirectReferences);
+            var property = new Properties<ClassForIndirectReference>().Select(x => x.Property);
+
+            // Exercise system and Verify outcome
+            Assert.DoesNotThrow(() => sut.Verify(property));
+        }
+
+        [Fact]
+        public void VerifyPropertyThrowsWhenPropertyExposesAnyIndirectReference()
+        {
+            // Fixture setup
+            var indirectReferences = new[]
+            {
+                GetType().Assembly,
+                Assembly.Load("Ploeh.AutoFixture"),
+                typeof(object).Assembly
+            };
+            var sut = new IndirectReferenceAssertion(indirectReferences);
+            var property = new Properties<ClassForIndirectReference>().Select(x => x.Property);
+
+            // Exercise system and Verify outcome
+            var exception = Assert.Throws<IndirectReferenceException>(() => sut.Verify(property));
+            Assert.Contains("Ploeh.AutoFixture", exception.Message);
+        }
+
+        [Fact]
+        public void VerifyMethodDoesNotThrowWhenMethodDoesNotExposeAnyIndirectReference()
+        {
+            // Fixture setup
+            var indirectReferences = new[]
+            {
+                GetType().Assembly
+            };
+            var sut = new IndirectReferenceAssertion(indirectReferences);
+            var method = new Methods<ClassForIndirectReference>().Select(x => x.Method(null, null));
+
+            // Exercise system and Verify outcome
+            Assert.DoesNotThrow(() => sut.Verify(method));
+        }
+
+        [Fact]
+        public void VerifyMethodThrowsWhenMethodExposesAnyIndirectReference()
+        {
+            // Fixture setup
+            var indirectReferences = new[]
+            {
+                GetType().Assembly,
+                Assembly.Load("Jwc.Experiment.Idioms")
+            };
+            var sut = new IndirectReferenceAssertion(indirectReferences);
+            var method = new Methods<ClassForIndirectReference>().Select(x => x.Method(null, null));
+
+            // Exercise system and Verify outcome
+            var exception = Assert.Throws<IndirectReferenceException>(() => sut.Verify(method));
+            Assert.Contains("Jwc.Experiment.Idioms", exception.Message);
+        }
+
+        [Fact]
+        public void VerifyEventDoesNotThrowWhenMethodDoesNotExposeAnyIndirectReference()
+        {
+            // Fixture setup
+            var indirectReferences = new[]
+            {
+                GetType().Assembly
+            };
+            var sut = new IndirectReferenceAssertion(indirectReferences);
+            var @event = typeof(ClassForIndirectReference).GetEvent("Event");
+
+            // Exercise system and Verify outcome
+            Assert.DoesNotThrow(() => sut.Verify(@event));
+        }
+
+        [Fact]
+        public void VerifyEventThrowsWhenEventExposesAnyIndirectReference()
+        {
+            // Fixture setup
+            var indirectReferences = new[]
+            {
+                GetType().Assembly,
+                Assembly.Load("Jwc.Experiment.Idioms")
+            };
+            var sut = new IndirectReferenceAssertion(indirectReferences);
+            var @event = typeof(ClassForIndirectReference).GetEvent("Event");
+
+            // Exercise system and Verify outcome
+            var exception = Assert.Throws<IndirectReferenceException>(() => sut.Verify(@event));
+            Assert.Contains("Jwc.Experiment.Idioms", exception.Message);
+        }
+
+        [Fact]
+        public void VerifyTypeCorrectlyVerifies()
+        {
+            // Fixture setup
+            var sut = new Mock<IndirectReferenceAssertion>(new object[] { new Assembly[0] })
+                { CallBase = true }.Object;
+
+            var members = new List<MemberInfo>();
+            sut.ToMock().Setup(x => x.Verify(It.IsAny<MemberInfo>())).Callback<MemberInfo>(members.Add);
+
+            var type = typeof(ClassWithMembers);
+            
+            var expected = new TypeMembers(
+                type,
+                MemberKinds.All,
+                Accessibilities.Protected | Accessibilities.Public);
+
+            // Exercise system
+            sut.Verify(type);
+
+            // Verify outcome
+            Assert.Equal(expected, members);
+        }
+
+        [Fact]
+        public void VerifyAssemblyCorrectlyVerifies()
+        {
+            // Fixture setup
+            var sut = new Mock<IndirectReferenceAssertion>(new object[] { new Assembly[0] })
+                { CallBase = true }.Object;
+
+            var types = new List<Type>();
+            sut.ToMock().Setup(x => x.Verify(It.IsAny<Type>())).Callback<Type>(types.Add);
+
+            var assembly = typeof(object).Assembly;
+            var expected = assembly.GetTypes().Where(IsExposed).ToArray();
+
+            Assert.True(expected.Any(t => t.IsNestedPublic), "IsNestedPublic");
+            Assert.True(expected.Any(t => t.IsNestedFamORAssem), "IsNestedFamORAssem");
+            Assert.True(expected.Any(t => t.IsNestedFamily), "IsNestedFamily");
+            Assert.True(expected.All(t => !t.IsNestedAssembly), "IsNestedAssembly");
+            Assert.True(expected.All(t => !t.IsNestedPrivate), "IsNestedPrivate.");
+
+            // Exercise system
+            sut.Verify(assembly);
+
+            // Verify outcome
+            Assert.Equal(expected, types);
+        }
+
+        private static bool IsExposed(Type type)
+        {
+            var accessibilityCollector = new AccessibilityCollector();
+
+            return (type.ToElement().Accept(accessibilityCollector).Value.Single()
+                & (Accessibilities.Public | Accessibilities.Protected))
+                != Accessibilities.None;
+        }
+
+        private class ClassForIndirectReference
+        {
+            public Fixture Field;
+
+            public ClassForIndirectReference()
+            {
+                var fixture = new Fixture();
+            }
+
+            public ClassForIndirectReference(IIdiomaticTypeAssertion arg1, object arg2)
+            {
+            }
+
+            public Fixture Property
+            {
+                get;
+                set;
+            }
+
+            public void Method(IIdiomaticTypeAssertion arg1, object arg2)
+            {
+            }
+
+            public event Func<IIdiomaticTypeAssertion> Event;
+        }
+    }
+}
