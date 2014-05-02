f6138e9d381a63d0e15041172f5d656ab6aff403
Jin-Wook Chung <jwChung@hotmail.com>
2014-05-02 19:12:54 +0900

    build: addresses code-analysis warnings.

--- src/Experiment.AutoFixture/FixtureFactory.cs
+++ src/Experiment.AutoFixture/FixtureFactory.cs
@@ -6,13 +6,14 @@ namespace NuGet.Jwc.Experiment
     /// <summary>
     /// Represents a factory to create <see cref="IFixture"/>
     /// </summary>
-    public class FixtureFactory
+    public static class FixtureFactory
     {
         /// <summary>
         /// Creates a fixture.
         /// </summary>
         /// <param name="testMethod">The test method.</param>
         /// <returns>The fixture.</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "testMethod", Justification = "The testMethod parameter can be used in future.")]
         public static IFixture Create(MethodInfo testMethod)
         {
             return new Fixture();
