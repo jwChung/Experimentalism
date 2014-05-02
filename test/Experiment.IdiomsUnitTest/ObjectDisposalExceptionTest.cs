a58709a0981029d9cf6ed88f73198dd8aaacf123
Jin-Wook Chung <jwChung@hotmail.com>
2014-05-02 10:21:15 +0900

    dev: introduces ObjectDisposalException.

---
+++ test/Experiment.IdiomsUnitTest/ObjectDisposalExceptionTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms
+{
+    public class ObjectDisposalExceptionTest
+    {
+        [Fact]
+        public void SutIsException()
+        {
+            var sut = new ObjectDisposalException();
+            Assert.IsAssignableFrom<Exception>(sut);
+        }
+    }
+}
