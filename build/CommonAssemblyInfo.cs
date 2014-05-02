01e59d0a9e07cc8cd0e7c108c88747ffe3ab24e9
Jin-Wook Chung <jwChung@hotmail.com>
2014-05-02 16:07:00 +0900

    build: increment minor version,
    to indicate that ObjectDisposalAssertion is a new feature.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.16.1")]
-[assembly: AssemblyInformationalVersion("0.16.1")]
+[assembly: AssemblyVersion("0.17.0")]
+[assembly: AssemblyInformationalVersion("0.17.0")]
 
 /*
  * Version 0.17.0
