0a11c3e3e255d2c246924390d3b9dccf834940e0
Jin-Wook Chung <jwChung@hotmail.com>
2014-05-14 15:55:53 +0900

    dev: implements TestFixtureFactoryAttribute.

---
+++ src/Experiment/TestFixtureFactoryAttribute.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace Jwc.Experiment
+{
+    /// <summary>
+    /// An attribute to declare a type implementing the
+    /// <see cref="ITestFixtureFactory"/> interface. An instance of type will be
+    /// only once intitalized on a certain test-assembly(dll) and will create
+    /// every instance of <see cref="ITestFixture"/> for all the tests.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false)]
+    public class TestFixtureFactoryAttribute : Attribute
+    {
+        private readonly Type _type;
+
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="TestFixtureFactoryAttribute"/> class.
+        /// </summary>
+        /// <param name="type">
+        /// The type implementing <see cref="ITestFixtureFactory"/>.
+        /// </param>
+        public TestFixtureFactoryAttribute(Type type)
+        {
+            if (type == null)
+                throw new ArgumentNullException("type");
+
+            if (!typeof(ITestFixtureFactory).IsAssignableFrom(type))
+                throw new ArgumentException(
+                    string.Format(
+                        CultureInfo.CurrentCulture,
+                        "The type '{0}' does not implement ITestFixtureFacotry.",
+                        type),
+                    "type");
+
+            _type = type;
+        }
+
+        /// <summary>
+        /// Gets a value indicating the type implementing
+        /// <see cref="ITestFixtureFactory"/>.
+        /// </summary>
+        public Type Type
+        {
+            get
+            {
+                return _type;
+            }
+        }
+    }
+}
---
+++ test/ExperimentUnitTest/TestFixtureFactoryAttributeTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Jwc.Experiment
+{
+    public class TestFixtureFactoryAttributeTest
+    {
+        [Fact]
+        public void SutIsAttribute()
+        {
+            var sut = new TestFixtureFactoryAttribute(typeof(DelegatingTestFixtureFactory));
+            Assert.IsAssignableFrom<Attribute>(sut);
+        }
+
+        [Fact]
+        public void SutHasCorrectAttributeUsage()
+        {
+            var attributeUsage = typeof(TestFixtureFactoryAttribute)
+                .GetCustomAttributes(typeof(AttributeUsageAttribute), false)
+                .Cast<AttributeUsageAttribute>().Single();
+            Assert.False(attributeUsage.AllowMultiple);
+            Assert.Equal(AttributeTargets.Assembly, attributeUsage.ValidOn);
+        }
+
+        [Fact]
+        public void InitializeWithNullTypeThrows()
+        {
+            Assert.Throws<ArgumentNullException>(() => new TestFixtureFactoryAttribute(null));
+        }
+
+        [Fact]
+        public void InitializeWithInvalidTypeThrows()
+        {
+            Assert.Throws<ArgumentException>(() => new TestFixtureFactoryAttribute(typeof(object)));
+        }
+
+        [Fact]
+        public void TypeIsCorrect()
+        {
+            var type = typeof(DelegatingTestFixtureFactory);
+            var sut = new TestFixtureFactoryAttribute(type);
+
+            var actual = sut.Type;
+
+            Assert.Equal(type, actual);
+        }
+    }
+}
