fd3452b3d5bafb5e8d3859bf0472b73c994e4b13
Jin-Wook Chung <jwChung@hotmail.com>
2014-05-15 14:01:27 +0900

    build: creates the new project 'Experiment.Xunit'.

---
+++ src/Experiment.Xunit/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Experiment.Xunit")]
+[assembly: AssemblyDescription("")]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("eb09e2ae-6278-4721-bf40-1643c910f8ad")]
