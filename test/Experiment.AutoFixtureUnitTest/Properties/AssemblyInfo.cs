9b8486b4663e96b7dced6879b00ba849c8cbbd74
Jin-Wook Chung <jwChung@hotmail.com>
2014-05-15 15:42:38 +0900

    refactor: moves the TestFixtureFactoryType declaration in AssemblyInfo
    to Scenario.

--- test/Experiment.AutoFixtureUnitTest/Properties/AssemblyInfo.cs
+++ test/Experiment.AutoFixtureUnitTest/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using Jwc.Experiment.Xunit;
-using NuGet.Jwc.Experiment;
 
 [assembly: AssemblyTitle("Experiment.AutoFixtureUnitTest")]
 [assembly: AssemblyDescription("")]
 [assembly: Guid("75d8aefb-c95f-437a-a902-9e0b121503ad")]
-[assembly: TestFixtureFactoryType(typeof(AutoFixtureFactory))]
--- test/Experiment.AutoFixtureUnitTest/Scenario.cs
+++ test/Experiment.AutoFixtureUnitTest/Scenario.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Jwc.Experiment.Xunit;
+using NuGet.Jwc.Experiment;
 using Ploeh.AutoFixture.Xunit;
 using Xunit;
 using Xunit.Extensions;
+
+[assembly: TestFixtureFactoryType(typeof(AutoFixtureFactory))]
 
 namespace NuGet.Jwc.Experiment
 {
