40268afc6b157f1d6d423e1ab2b139ff3eda7f9a
Jin-Wook Chung <jwChung@hotmail.com>
2014-05-15 14:08:44 +0900

    build: creates the new project 'Experiment.XunitUnitTest'.

---
+++ test/Experiment.XunitUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Experiment.XunitUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("332722be-c052-419e-ae8a-0d961351a25f")]
