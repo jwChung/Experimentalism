01ed7e9bef800abf3195f05a87525b829dbdd13c
Jin-Wook Chung <jwChung@hotmail.com>
2014-05-16 15:07:22 +0900

    refactor: removes the unnecessary class 'Person' in the scenario.

--- test/Experiment.AutoFixtureUnitTest/Scenario.cs
+++ test/Experiment.AutoFixtureUnitTest/Scenario.cs
@@ -100,37 +100,5 @@ namespace Jwc.Experiment.AutoFixture
                 yield return new object[] { "expected", 1234 };
             }
         }
-
-        public class Person
-        {
-            private readonly string _name;
-            private readonly int _age;
-
-            public Person()
-            {
-            }
-
-            public Person(string name, int age)
-            {
-                _name = name;
-                _age = age;
-            }
-
-            public string Name
-            {
-                get
-                {
-                    return _name;
-                }
-            }
-
-            public int Age
-            {
-                get
-                {
-                    return _age;
-                }
-            }
-        }
     }
 }
