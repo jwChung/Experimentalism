cbfcaccc1c20768a130ff0ca0efc61ec8fabba6a
Jin-Wook Chung <jwChung@hotmail.com>
2014-05-19 22:34:34 +0900

    build: adds the shell script same with run.cmd.

---
+++ Run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+function vsvers()
+{
+	if [ "$VS120COMNTOOLS" ]; then
+		echo " /property:VisualStudioVersion=12.0"
+	else
+		echo ""
+	fi
+}
+
+$WINDIR/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe `dirname $0`/build/Build.proj -v:minimal -maxcpucount -nodeReuse:false `vsvers` $*
