47ea6dcaa828a62023c3c556ace7ec3053e8be19
Jin-Wook Chung <jwChung@hotmail.com>
2014-05-21 19:00:06 +0900

    dev: implements AssemblyInitializeAttribute.

---
+++ src/Experiment.Xunit/AssemblyInitializeAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Jwc.Experiment.Xunit
+{
+    /// <summary>
+    /// Attribute to supply a type of which instance will be used to set up or
+    /// to tear down a test fixture only once on assembly level.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public sealed class AssemblyInitializeAttribute : Attribute
+    {
+        private readonly Type _initializer;
+
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="AssemblyInitializeAttribute" /> class.
+        /// </summary>
+        /// <param name="initializer">
+        /// A type to be used to set up or to tear down a test fixture only once
+        /// on assembly level.
+        /// </param>
+        public AssemblyInitializeAttribute(Type initializer)
+        {
+            if (initializer == null)
+                throw new ArgumentNullException("initializer");
+
+            _initializer = initializer;
+        }
+
+        /// <summary>
+        /// Gets a type to be used to set up or to tear down a test fixture only
+        /// once on assembly level.
+        /// </summary>
+        public Type Initializer
+        {
+            get
+            {
+                return _initializer;
+            }
+        }
+    }
+}
---
+++ test/Experiment.XunitUnitTest/AssemblyInitializeAttributeTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+
+namespace Jwc.Experiment.Xunit
+{
+    public class AssemblyInitializeAttributeTest
+    {
+        [Fact]
+        public void SutIsAttribute()
+        {
+            var sut = new AssemblyInitializeAttribute(typeof(object));
+            Assert.IsAssignableFrom<Attribute>(sut);
+        }
+
+        [Fact]
+        public void InitializeWithNullInitializerThrows()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new AssemblyInitializeAttribute(null));
+        }
+
+        [Fact]
+        public void InitializerIsCorrect()
+        {
+            var expected = GetType();
+            var sut = new AssemblyInitializeAttribute(expected);
+
+            var actual = sut.Initializer;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
