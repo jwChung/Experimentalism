00d009737878e84778c86b0760c011e9fa210981
Jin-Wook Chung <jwChung@hotmail.com>
2014-05-21 23:39:08 +0900

    dev: implements NotSupportedFixtureFactory2.

---
+++ src/Experiment.Xunit/NotSupportedFixtureFactory2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+
+namespace Jwc.Experiment.Xunit
+{
+    /// <summary>
+    /// Represents test fixture factory which throws
+    /// <see cref="NotSupportedException" /> when the <code>Create</code>
+    /// method is called.
+    /// </summary>
+    public class NotSupportedFixtureFactory2 : ITestFixtureFactory
+    {
+        /// <summary>
+        /// Throws <see cref="NotSupportedException" />.
+        /// </summary>
+        /// <param name="testMethod">
+        /// The test method in which the test fixture will be used.
+        /// </param>
+        public ITestFixture Create(MethodInfo testMethod)
+        {
+            if (testMethod == null)
+                throw new ArgumentNullException("testMethod");
+
+            throw new NotSupportedException(
+                String.Format(
+                    CultureInfo.CurrentCulture,
+                    "To create auto data, explicitly declare TestFixtureFactoryTypeAttribute on the test " +
+                    "assembly '{0}' or override the CreateTestFixture method of the test attribute " +
+                    "to create an instance of ITestFixture.",
+                    testMethod.ReflectedType.Assembly));
+        }
+    }
+}
---
+++ test/Experiment.XunitUnitTest/NotSupportedFixtureFactory2Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using Xunit;
+
+namespace Jwc.Experiment.Xunit
+{
+    public class NotSupportedFixtureFactory2Test
+    {
+        [Fact]
+        public void SutIsTestFixtureFactory()
+        {
+            var sut = new NotSupportedFixtureFactory2();
+            Assert.IsAssignableFrom<ITestFixtureFactory>(sut);
+        }
+
+        [Fact]
+        public void CreateWithNullTestMethodThrows()
+        {
+            var sut = new NotSupportedFixtureFactory2();
+            Assert.Throws<ArgumentNullException>(() => sut.Create(null));
+        }
+
+        [Fact]
+        public void CreateThrows()
+        {
+            var sut = new NotSupportedFixtureFactory2();
+            var testMethod = (MethodInfo)MethodBase.GetCurrentMethod();
+            Assert.Throws<NotSupportedException>(() => sut.Create(testMethod));
+        }
+    }
+}
