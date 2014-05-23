2633174886ffda924af3023445e6ef1dc7b8cd90
Jin-Wook Chung <jwChung@hotmail.com>
2014-05-23 23:06:09 +0900

    build: increment patch version,
    to indicate that custom display name and timeout on
    FirstClassTestAttribute is a fix.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.22.1")]
-[assembly: AssemblyInformationalVersion("0.22.1")]
+[assembly: AssemblyVersion("0.22.2")]
+[assembly: AssemblyInformationalVersion("0.22.2")]
 
 /*
  * Version 0.22.2
