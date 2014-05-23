c22d04ea26fa44cdcba15b1c34b72663cd034342
Jin-Wook Chung <jwChung@hotmail.com>
2014-05-23 13:44:13 +0900

    dev: implements IdiomaticAssertion.

---
+++ src/Experiment.Idioms/Assertions/IdiomaticAssertion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+
+namespace Jwc.Experiment.Idioms.Assertions
+{
+    /// <summary>
+    /// Represents base class implementing
+    /// <see cref="IIdiomaticAssemblyAssertion" />,
+    /// <see cref="IIdiomaticTypeAssertion" /> and
+    /// <see cref="IIdiomaticMemberAssertion" />
+    /// </summary>
+    public abstract class IdiomaticAssertion
+        : IdiomaticMemberAssertion, IIdiomaticAssemblyAssertion, IIdiomaticTypeAssertion
+    {
+        /// <summary>
+        /// Verifies that the idiomatic assertion can be verified for the
+        /// specified assembly.
+        /// </summary>
+        /// <param name="assembly">
+        /// The assembly.
+        /// </param>
+        public void Verify(Assembly assembly)
+        {
+            if (assembly == null)
+                throw new ArgumentNullException("assembly");
+
+            foreach (var type in assembly.GetExportedTypes())
+                Verify(type);
+        }
+
+        /// <summary>
+        /// Verifies that the idiomatic assertion can be verified for the
+        /// specified type.
+        /// </summary>
+        /// <param name="type">
+        /// The type.
+        /// </param>
+        public virtual void Verify(Type type)
+        {
+            foreach (var member in type.GetIdiomaticMembers())
+                Verify(member);
+        }
+    }
+}
---
+++ test/Experiment.IdiomsUnitTest/Assertions/IdiomaticAssertionTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Jwc.Experiment.Xunit;
+using Moq;
+using Xunit;
+
+namespace Jwc.Experiment.Idioms.Assertions
+{
+    public class IdiomaticAssertionTest
+    {
+        [Fact]
+        public void SutIsIdiomaticAssemblyAssertion()
+        {
+            var sut = new Mock<IdiomaticAssertion> { CallBase = true }.Object;
+            Assert.IsAssignableFrom<IIdiomaticAssemblyAssertion>(sut);
+        }
+
+        [Fact]
+        public void SutIsIdiomaticTypeAssertion()
+        {
+            var sut = new Mock<IdiomaticAssertion> { CallBase = true }.Object;
+            Assert.IsAssignableFrom<IIdiomaticTypeAssertion>(sut);
+        }
+
+        [Fact]
+        public void SutIsIdiomaticMemberAssertion()
+        {
+            var sut = new Mock<IdiomaticAssertion> { CallBase = true }.Object;
+            Assert.IsAssignableFrom<IdiomaticMemberAssertion>(sut);
+        }
+
+        [Fact]
+        public void VerifyAssemblyCorrectlyVerifies()
+        {
+            var sut = new Mock<IdiomaticAssertion> { CallBase = true }.Object;
+            var types = new List<MemberInfo>();
+            sut.ToMock().Setup(x => x.Verify(It.IsAny<Type>())).Callback<Type>(types.Add);
+            var assembly = typeof(TestAttribute).Assembly;
+            var expected = assembly.GetExportedTypes();
+
+            sut.Verify(assembly);
+
+            Assert.Equal(expected, types);
+        }
+
+        [Fact]
+        public void VerifyNullAssemblyThrows()
+        {
+            var sut = new Mock<IdiomaticAssertion> { CallBase = true }.Object;
+            Assert.Throws<ArgumentNullException>(() => sut.Verify((Assembly)null));
+        }
+
+        [Fact]
+        public void VerifyTypeVerifiesCorrectMembers()
+        {
+            var sut = new Mock<IdiomaticAssertion> { CallBase = true }.Object;
+            var members = new List<MemberInfo>();
+            sut.ToMock().Setup(x => x.Verify(It.IsAny<MemberInfo>())).Callback<MemberInfo>(members.Add);
+            var type = typeof(ClassWithMembers);
+            var expected = new IdiomaticMembers(type);
+
+            sut.Verify(type);
+
+            Assert.Equal(expected.OrderBy(m => m.Name), members.OrderBy(m => m.Name));
+        }
+
+        [Fact]
+        public void VerifyNullTypeThrows()
+        {
+            var sut = new Mock<IdiomaticAssertion> { CallBase = true }.Object;
+            Assert.Throws<ArgumentNullException>(() => sut.Verify((Type)null));
+        }
+    }
+}
