7e1894a133202ebe1fab70f8a5ab9cc5e85d9cc4
Jin-Wook Chung <jwChung@hotmail.com>
2014-06-07 18:02:50 +0900

    docs: clarified the exception message.

--- src/Experiment/NotSupportedFixtureFactory.cs
+++ src/Experiment/NotSupportedFixtureFactory.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 
 namespace Jwc.Experiment
@@ -17,19 +16,12 @@ namespace Jwc.Experiment
         /// <param name="testMethod">
         ///     The test method in which the test fixture will be used.
         /// </param>
-        [SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "CreateTestFixture")]
-        [SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "SetCurrent")]
-        [SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "TestFixtureFactory")]
-        [SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "ITestFixtureFactory")]
-        [SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "ITestFixture")]
-        [SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "AssemblyCustomizationAttribute")]
         public ITestFixture Create(MethodInfo testMethod)
         {
-            // TODO: clarify this exception message.
             throw new NotSupportedException(
-                "To create auto data, set valid 'ITestFixtureFactory' with the 'TestFixtureFactory.SetCurrent' " +
-                "method through 'AssemblyCustomizationAttribute', or override the 'CreateTestFixture' method " +
-                "of the test attribute to create an instance of 'ITestFixture'.");
+                "To create auto data, call 'TestFixtureFactory.SetCurrent' to set 'ITestFixtureFactory', or override " +
+                "'CreateTestFixture' of 'TestAttribute' or 'FirstClassTestAttribute' to create an instance of " +
+                "'ITestFixture'.");
         }
     }
 }
