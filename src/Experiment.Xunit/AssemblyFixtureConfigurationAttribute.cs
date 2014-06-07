fb17bfa3ddc12cb6461799e9823e450da79c3a5e
Jin-Wook Chung <jwChung@hotmail.com>
2014-06-07 14:27:02 +0900

    dev: introduced AssemblyFixtureConfigurationAttribute.

---
+++ src/Experiment.Xunit/AssemblyFixtureConfigurationAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Jwc.Experiment.Xunit
+{
+    /// <summary>
+    ///     Attribute to set up or tear down a test fixture on assembly level.
+    /// </summary>
+    public abstract class AssemblyFixtureConfigurationAttribute : Attribute
+    {
+    }
+}
---
+++ test/Experiment.XunitUnitTest/AssemblyFixtureConfigurationAttributeTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace Jwc.Experiment.Xunit
+{
+    public class AssemblyFixtureConfigurationAttributeTest
+    {
+        [Fact]
+        public void SutIsAttribute()
+        {
+            var sut = new TssAssemblyFixtureConfigurationAttribute();
+            Assert.IsAssignableFrom<Attribute>(sut);
+        }
+
+        private class TssAssemblyFixtureConfigurationAttribute : AssemblyFixtureConfigurationAttribute
+        {
+        }
+    }
+}
