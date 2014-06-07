2ae7d8ec092493521b96480f06179d346970382e
Jin-Wook Chung <jwChung@hotmail.com>
2014-06-07 19:39:09 +0900

    dev: implemented DefaultFixtureFactoryConfigurationAttribute.

--- src/Experiment/DefaultFixtureFactoryConfigurationAttribute.cs
+++ src/Experiment/DefaultFixtureFactoryConfigurationAttribute.cs
@@ -1,9 +1,86 @@
-﻿namespace Jwc.Experiment
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+
+namespace Jwc.Experiment
 {
     /// <summary>
     /// Attribute to configure default fixture factory.
     /// </summary>
     public class DefaultFixtureFactoryConfigurationAttribute : AssemblyFixtureConfigurationAttribute
     {
+        private readonly Type _factoryType;
+        private readonly ITestFixtureFactory _factory;
+
+        /// <summary>
+        ///     Initializes a new instance of the
+        ///     <see cref="DefaultFixtureFactoryConfigurationAttribute" /> class.
+        /// </summary>
+        /// <param name="factoryType">
+        ///     Type of the test fixture factory.
+        /// </param>
+        public DefaultFixtureFactoryConfigurationAttribute(Type factoryType)
+        {
+            if (factoryType == null)
+                throw new ArgumentNullException("factoryType");
+
+            if (!typeof(ITestFixtureFactory).IsAssignableFrom(factoryType) || factoryType.IsAbstract)
+                throw new ArgumentException(
+                    string.Format(
+                        CultureInfo.CurrentCulture,
+                        "The factory type '{0}' should be concrete class of ITestFixtureFactory.",
+                        factoryType),
+                    "factoryType");
+
+            _factoryType = factoryType;
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the
+        ///     <see cref="DefaultFixtureFactoryConfigurationAttribute" /> class.
+        /// </summary>
+        /// <param name="factory">
+        ///     The test fixture factory.
+        /// </param>
+        protected DefaultFixtureFactoryConfigurationAttribute(ITestFixtureFactory factory)
+        {
+            if (factory == null)
+                throw new ArgumentNullException("factory");
+
+            _factory = factory;
+        }
+
+        /// <summary>
+        /// Gets a value indicating the type of the test fixture factory.
+        /// </summary>
+        public Type FactoryType
+        {
+            get
+            {
+                return _factoryType ?? _factory.GetType();
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating the test fixture factory.
+        /// </summary>
+        public ITestFixtureFactory Factory
+        {
+            get
+            {
+                return _factory ?? (ITestFixtureFactory)Activator.CreateInstance(FactoryType);
+            }
+        }
+
+        /// <summary>
+        ///     Sets up the default factory of test fixture in a test assembly.
+        /// </summary>
+        /// <param name="testAssembly">
+        ///     The test assembly.
+        /// </param>
+        protected override void Setup(Assembly testAssembly)
+        {
+            DefaultFixtureFactory.SetCurrent(Factory);
+        }
     }
 }
--- test/ExperimentUnitTest/DefaultFixtureFactoryConfigurationAttributeTest.cs
+++ test/ExperimentUnitTest/DefaultFixtureFactoryConfigurationAttributeTest.cs
@@ -1,4 +1,7 @@
-﻿using Xunit;
+﻿using System;
+using System.Reflection;
+using Xunit;
+using Xunit.Extensions;
 
 namespace Jwc.Experiment
 {
@@ -7,8 +10,120 @@ namespace Jwc.Experiment
         [Fact]
         public void SutIsAssemblyFixtureConfigurationAttribute()
         {
-            var sut = new DefaultFixtureFactoryConfigurationAttribute();
+            var sut = new DefaultFixtureFactoryConfigurationAttribute(typeof(DelegatingTestFixtureFactory));
             Assert.IsAssignableFrom<AssemblyFixtureConfigurationAttribute>(sut);
+        }
+
+        [Fact]
+        public void InitializeWithNullFactoryTypeThrows()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new DefaultFixtureFactoryConfigurationAttribute(null));
+        }
+
+        [Theory]
+        [InlineData(typeof(int))]
+        [InlineData(typeof(object))]
+        [InlineData(typeof(ITestFixtureFactory))]
+        [InlineData(typeof(AbstractTestFixtureFactory))]
+        public void InitializeWithInvalidFactoryTypeThrows(Type factoryType)
+        {
+            Assert.Throws<ArgumentException>(
+                () => new DefaultFixtureFactoryConfigurationAttribute(factoryType));
+        }
+
+        [Fact]
+        public void InitializeWithNullFactoryThrows()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new TssDefaultFixtureFactoryConfigurationAttribute((ITestFixtureFactory)null));
+        }
+
+        [Fact]
+        public void FactoryTypeIsCorrectWhenInitializedWithFactoryType()
+        {
+            var expected = typeof(DelegatingTestFixtureFactory);
+            var sut = new DefaultFixtureFactoryConfigurationAttribute(expected);
+
+            var actual = sut.FactoryType;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FactoryTypeIsCorrectWhenInitializedWithFactory()
+        {
+            var factory = new DelegatingTestFixtureFactory();
+            var sut = new TssDefaultFixtureFactoryConfigurationAttribute(factory);
+
+            var actual = sut.FactoryType;
+
+            Assert.Equal(factory.GetType(), actual);
+        }
+
+        [Fact]
+        public void FactoryIsCorrectWhenInitializedWithFactoryType()
+        {
+            var expected = typeof(DelegatingTestFixtureFactory);
+            var sut = new DefaultFixtureFactoryConfigurationAttribute(expected);
+
+            var actual = sut.Factory;
+
+            Assert.IsType<DelegatingTestFixtureFactory>(actual);
+        }
+
+        [Fact]
+        public void FactoryIsCorrectWhenInitializedWithFactory()
+        {
+            var factory = new DelegatingTestFixtureFactory();
+            var sut = new TssDefaultFixtureFactoryConfigurationAttribute(factory);
+
+            var actual = sut.Factory;
+
+            Assert.Equal(factory, actual);
+        }
+
+        [StaticFact]
+        public void SetupSetsSuppliedFactoryAsCurrentOfDefaultFixtureFactory()
+        {
+            var factory = new DelegatingTestFixtureFactory();
+            var sut = new TssDefaultFixtureFactoryConfigurationAttribute(factory);
+
+            sut.CallSetup(null);
+
+            Assert.Equal(factory, DefaultFixtureFactory.Current);
+        }
+
+        [StaticFact]
+        public void SetupSetsInstanceOfSuppliedFactoryTypeAsCurrentOfDefaultFixtureFactory()
+        {
+            var factoryType = typeof(DelegatingTestFixtureFactory);
+            var sut = new TssDefaultFixtureFactoryConfigurationAttribute(factoryType);
+
+            sut.CallSetup(null);
+
+            Assert.IsType<DelegatingTestFixtureFactory>(DefaultFixtureFactory.Current);
+        }
+
+        private class TssDefaultFixtureFactoryConfigurationAttribute : DefaultFixtureFactoryConfigurationAttribute
+        {
+            public TssDefaultFixtureFactoryConfigurationAttribute(Type factoryType) : base(factoryType)
+            {
+            }
+
+            public TssDefaultFixtureFactoryConfigurationAttribute(ITestFixtureFactory factory) : base(factory)
+            {
+            }
+
+            public void CallSetup(Assembly testAssembly)
+            {
+                base.Setup(testAssembly);
+            }
+        }
+
+        private abstract class AbstractTestFixtureFactory : ITestFixtureFactory
+        {
+            public abstract ITestFixture Create(MethodInfo testMethod);
         }
     }
 }
