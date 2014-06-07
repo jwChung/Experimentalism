49fe92f745544e4f03cc12f5da4a47bb1fccddc4
Jin-Wook Chung <jwChung@hotmail.com>
2014-06-08 00:09:36 +0900

    build: increment patch version,
    to indicate that introducing TestFixtureFactoryConfiguration is a fix.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.23.1")]
-[assembly: AssemblyInformationalVersion("0.23.1")]
+[assembly: AssemblyVersion("0.23.2")]
+[assembly: AssemblyInformationalVersion("0.23.2")]
 
 /*
  * Version 0.23.2
