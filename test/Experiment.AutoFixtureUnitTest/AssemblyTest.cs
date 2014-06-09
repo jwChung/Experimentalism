f9b95b95e5f00358fb22232cb35710b97f8fe778
Jin-Wook Chung <jwChung@hotmail.com>
2014-06-09 23:05:31 +0900

    refactor: removed unused using directives.

--- test/Experiment.AutoFixtureUnitTest/AssemblyTest.cs
+++ test/Experiment.AutoFixtureUnitTest/AssemblyTest.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using System.Reflection;
+﻿using System.Reflection;
 using Jwc.Experiment.Idioms;
 using Xunit;
 
--- test/Experiment.IdiomsUnitTest/AssemblyTest.cs
+++ test/Experiment.IdiomsUnitTest/AssemblyTest.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿using System.Reflection;
 using Jwc.Experiment.Idioms;
 using Xunit;
 
