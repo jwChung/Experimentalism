63426636f7d14973640275d48070c376087df106
Jin-Wook Chung <jwChung@hotmail.com>
2014-06-10 05:28:36 +0900

    refactor: simplified the DelegatingAssembly class.

--- test/Experiment.IdiomsUnitTest/DelegatingAssembly.cs
+++ test/Experiment.IdiomsUnitTest/DelegatingAssembly.cs
@@ -5,26 +5,29 @@ namespace Jwc.Experiment
 {
     public class DelegatingAssembly : Assembly
     {
+        public DelegatingAssembly()
+        {
+            OnGetCustomAttributes = i => new object[0];
+            OnGetTypes = () => new Type[0];
+        }
+
         public Func<bool, object[]> OnGetCustomAttributes { get; set; }
 
         public Func<Type[]> OnGetTypes { get; set; }
 
         public override string FullName
         {
-            get
-            {
-                return "DelegatingAssembly";
-            }
+            get { return "DelegatingAssembly"; }
         }
 
         public override object[] GetCustomAttributes(bool inherit)
         {
-            return OnGetCustomAttributes == null ? new object[0] : OnGetCustomAttributes(inherit);
+            return OnGetCustomAttributes(inherit);
         }
 
         public override Type[] GetTypes()
         {
-            return OnGetTypes == null ? new Type[0] : OnGetTypes();
+            return OnGetTypes();
         }
     }
 }
