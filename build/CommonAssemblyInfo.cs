9969709a81b265af3f16e55e6b70af40ba9421e9
Jin-Wook Chung <jwChung@hotmail.com>
2014-06-11 14:14:31 +0900

    build: increment patch version,
    to indicate that some fixes are addressed.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.23.5")]
-[assembly: AssemblyInformationalVersion("0.23.5")]
+[assembly: AssemblyVersion("0.23.6")]
+[assembly: AssemblyInformationalVersion("0.23.6")]
 
 /*
  * Version 0.23.6
