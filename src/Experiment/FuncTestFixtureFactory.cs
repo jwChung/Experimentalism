20fd7c0e594cdbf90752490d68a7fd9ce81dbbb3
Jin-Wook Chung <jwChung@hotmail.com>
2014-06-13 10:59:33 +0900

    dev: introduced FuncTestFixtureFactory.

---
+++ src/Experiment/FuncTestFixtureFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+namespace Jwc.Experiment
+{
+    /// <summary>
+    ///     Represents a test fixture created by <see cref="Func{TResult}" />
+    /// </summary>
+    public class FuncTestFixtureFactory : ITestFixtureFactory
+    {
+        /// <summary>
+        ///     Creates a test fixture.
+        /// </summary>
+        /// <param name="testMethod">
+        ///     The test method in which the test fixture will be used.
+        /// </param>
+        /// <returns>
+        ///     The test fixture.
+        /// </returns>
+        public ITestFixture Create(MethodInfo testMethod)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/ExperimentUnitTest/FuncTestFixtureFactoryTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Jwc.Experiment
+{
+    public class FuncTestFixtureFactoryTest
+    {
+        [Fact]
+        public void SutIsTestFixtureFactory()
+        {
+            var sut = new FuncTestFixtureFactory();
+            Assert.IsAssignableFrom<ITestFixtureFactory>(sut);
+        } 
+    }
+}
