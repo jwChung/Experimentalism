60fb42b621fb3e193177b9395bf9caa6616cd8bd
Jin-Wook Chung <jwChung@hotmail.com>
2014-06-13 11:04:51 +0900

    dev: implemented the Func property.

--- src/Experiment/FuncTestFixtureFactory.cs
+++ src/Experiment/FuncTestFixtureFactory.cs
@@ -4,10 +4,31 @@ using System.Reflection;
 namespace Jwc.Experiment
 {
     /// <summary>
-    ///     Represents a test fixture created by <see cref="Func{TResult}" />
+    ///     Represents a test fixture created by a function delegate.
     /// </summary>
     public class FuncTestFixtureFactory : ITestFixtureFactory
     {
+        private readonly Func<MethodInfo, ITestFixtureFactory> _func;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="FuncTestFixtureFactory" /> class.
+        /// </summary>
+        /// <param name="func">
+        ///     The function to create the test fixture.
+        /// </param>
+        public FuncTestFixtureFactory(Func<MethodInfo, ITestFixtureFactory> func)
+        {
+            _func = func;
+        }
+
+        /// <summary>
+        /// Gets a value indicating the function supplied by constructor.
+        /// </summary>
+        public Func<MethodInfo, ITestFixtureFactory> Func
+        {
+            get { return _func; }
+        }
+
         /// <summary>
         ///     Creates a test fixture.
         /// </summary>
--- test/ExperimentUnitTest/FuncTestFixtureFactoryTest.cs
+++ test/ExperimentUnitTest/FuncTestFixtureFactoryTest.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using System;
+using System.Reflection;
+using Xunit;
 
 namespace Jwc.Experiment
 {
@@ -7,8 +9,19 @@ namespace Jwc.Experiment
         [Fact]
         public void SutIsTestFixtureFactory()
         {
-            var sut = new FuncTestFixtureFactory();
+            var sut = new FuncTestFixtureFactory(m => null);
             Assert.IsAssignableFrom<ITestFixtureFactory>(sut);
-        } 
+        }
+
+        [Fact]
+        public void FuncIsCorrect()
+        {
+            Func<MethodInfo, ITestFixtureFactory> func = m => null;
+            var sut = new FuncTestFixtureFactory(func);
+
+            var actual = sut.Func;
+
+            Assert.Equal(func, actual);
+        }
     }
 }
