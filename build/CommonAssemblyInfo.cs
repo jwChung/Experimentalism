db83fe75edecfff049be0fd952fc437277fc4dae
Jin-Wook Chung <jwChung@hotmail.com>
2014-06-18 14:57:22 +0900

    build: incremented patch version,
    to indicate that introducing FuncTestFixtureFactory and removing
    InverseEqualityComparer is a fix.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.23.8")]
-[assembly: AssemblyInformationalVersion("0.23.8")]
+[assembly: AssemblyVersion("0.23.9")]
+[assembly: AssemblyInformationalVersion("0.23.9")]
 
 /*
  * Version 0.23.9
