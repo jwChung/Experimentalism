69721ab4be0ed1b7364fde408d8823c2f8499c1f
Jin-Wook Chung <jwChung@hotmail.com>
2014-06-26 09:53:34 +0900

    build: incremented patch version,
    to indicate that renaming to
    TargetInvocationExceptionUnwrappingCommand is a fix.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("0.24.0")]
-[assembly: AssemblyInformationalVersion("0.24.0")]
+[assembly: AssemblyVersion("0.24.1")]
+[assembly: AssemblyInformationalVersion("0.24.1")]
 
 /*
  * Version 0.24.1
