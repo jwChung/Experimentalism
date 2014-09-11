857d4cf50efe7658fe5051f3915924fdec149e5a
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-11 20:04:14 +0900

    dev: Implemented the FactCommandFactory class.

---
+++ src/Experiment.Xunit/FactCommandFactory.cs
@@ -0,0 +1,33 @@
+﻿namespace Jwc.Experiment.Xunit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using global::Xunit.Sdk;
+
+    /// <summary>
+    /// Represents a factory to create <see cref="FactCommand"/>.
+    /// </summary>
+    public class FactCommandFactory : ITestCommandFactory
+    {
+        /// <summary>
+        /// Creates test commands.
+        /// </summary>
+        /// <param name="context">
+        /// The information about a test method.
+        /// </param>
+        /// <returns>
+        /// The new test commands.
+        /// </returns>
+        public IEnumerable<ITestCommand> Create(ITestMethodContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            if (context.Actual.ReturnType != typeof(void) || context.Actual.GetParameters().Any())
+                yield break;
+
+            yield return new FactCommand(Reflector.Wrap(context.Actual));
+        }
+    }
+}
---
+++ test/Experiment.Xunit.UnitTest/FactCommandFactoryTest.cs
@@ -0,0 +1,74 @@
+﻿namespace Jwc.Experiment.Xunit
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using Ploeh.Albedo;
+    using global::Xunit;
+    using global::Xunit.Sdk;
+
+    public class FactCommandFactoryTest
+    {
+        [Fact]
+        public void SutIsTestCommandFactory()
+        {
+            var sut = new FactCommandFactory();
+            Assert.IsAssignableFrom<ITestCommandFactory>(sut);
+        }
+
+        [Fact]
+        public void CreateWithNullContextThrows()
+        {
+            var sut = new FactCommandFactory();
+            Assert.Throws<ArgumentNullException>(() => sut.Create(null).ToArray());
+        }
+
+        [Fact]
+        public void CreateReturnsEmtpyCommandIfTestMethodHasReturnValue()
+        {
+            var sut = new FactCommandFactory();
+            var method = new Methods<FactCommandFactoryTest>().Select(x => x.NonVoidMethod());
+            var context = Mocked.Of<ITestMethodContext>(x => x.Actual == method);
+
+            var actual = sut.Create(context);
+
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void CreateReturnsEmtpyCommandIfTestMethodIsParameterized()
+        {
+            var sut = new FactCommandFactory();
+            var method = new Methods<FactCommandFactoryTest>().Select(x => x.ParameterizedMethod(null, 0));
+            var context = Mocked.Of<ITestMethodContext>(x => x.Actual == method);
+
+            var actual = sut.Create(context);
+
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void CreateReturnsCorrectCommandIfTestMethodIsValid()
+        {
+            var sut = new FactCommandFactory();
+            var method = new Methods<FactCommandFactoryTest>().Select(
+                x => x.CreateReturnsCorrectCommandIfTestMethodIsValid());
+            var context = Mocked.Of<ITestMethodContext>(x => x.Actual == method);
+            var expected = method.ReflectedType.FullName + "." + method.Name;
+
+            var actual = sut.Create(context).Single();
+
+            var command = Assert.IsAssignableFrom<FactCommand>(actual);
+            Assert.Equal(expected, command.DisplayName);
+        }
+
+        private object NonVoidMethod()
+        {
+            return null;
+        }
+
+        private void ParameterizedMethod(string arg1, int arg2)
+        {
+        }
+    }
+}
