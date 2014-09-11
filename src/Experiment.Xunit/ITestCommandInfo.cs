148167ddebee7f9ea998bd913ed5fa746605ccf5
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-12 00:57:20 +0900

    dev: Renamed ITestArgumentContext to ITestCommandInfo and added
    some members.

---
+++ src/Experiment.Xunit/ITestCommandInfo.cs
@@ -0,0 +1,27 @@
+﻿namespace Jwc.Experiment.Xunit
+{
+    using System;
+    using global::Xunit.Sdk;
+
+    /// <summary>
+    /// Defines information about a test command.
+    /// </summary>
+    public interface ITestCommandInfo
+    {
+        /// <summary>
+        /// Gets the test method.
+        /// </summary>
+        IMethodInfo TestMethod { get; }
+
+        /// <summary>
+        /// Gets the arguments.
+        /// </summary>
+        /// <param name="testObject">
+        /// The test object.
+        /// </param>
+        /// <returns>
+        /// The arguments.
+        /// </returns>
+        object[] GetArguments(object testObject);
+    }
+}
