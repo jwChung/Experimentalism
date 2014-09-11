b0a57224c695ff475c0aeae0da421c179acb722a
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-12 01:11:26 +0900

    dev: Introduced the ITestCase2 interface.

---
+++ src/Experiment.Xunit/ITestCase2.cs
@@ -0,0 +1,26 @@
+﻿namespace Jwc.Experiment.Xunit
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+
+    /// <summary>
+    /// Represents a test-case.
+    /// </summary>
+    public interface ITestCase2
+    {
+        /// <summary>
+        /// Gets the explicit arguments.
+        /// </summary>
+        IEnumerable<object> ExplicitArguments { get; }
+
+        /// <summary>
+        /// Gets the test method to be actually executed.
+        /// </summary>
+        MethodInfo ActualMethod { get; }
+
+        /// <summary>
+        /// Gets the test object declaring a actual test method.
+        /// </summary>
+        object ActualObject { get; }
+    }
+}
