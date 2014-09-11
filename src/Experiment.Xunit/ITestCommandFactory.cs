cf9b5af71971db7ec4977879e70b8679708fe60a
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-11 16:47:07 +0900

    dev: Introduced the ITestCommandFactory interface.

---
+++ src/Experiment.Xunit/ITestCommandFactory.cs
@@ -0,0 +1,22 @@
+﻿namespace Jwc.Experiment.Xunit
+{
+    using System.Collections.Generic;
+    using global::Xunit.Sdk;
+
+    /// <summary>
+    /// Represents a test command factory.
+    /// </summary>
+    public interface ITestCommandFactory
+    {
+        /// <summary>
+        /// Creates test commands.
+        /// </summary>
+        /// <param name="context">
+        /// The information about a test method.
+        /// </param>
+        /// <returns>
+        /// The new test commands.
+        /// </returns>
+        IEnumerable<ITestCommand> Create(ITestMethodContext context);
+    }
+}
