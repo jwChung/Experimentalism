5c85f2265311579b2ea3a823aabd18c9d9356b9c
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-12 12:36:25 +0900

    dev: Introduced the TestInfo class.

---
+++ src/Experiment.Xunit/TestInfo.cs
@@ -0,0 +1,54 @@
+﻿namespace Jwc.Experiment.Xunit
+{
+    using System;
+    using System.Reflection;
+    using global::Xunit.Sdk;
+
+    /// <summary>
+    /// Represents information of a test.
+    /// </summary>
+    public class TestInfo : ITestMethodInfo, ITestCommandInfo
+    {
+        /// <summary>
+        /// Gets the test method adorned with a test attribute.
+        /// </summary>
+        public MethodInfo TestMethod
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// Gets the test method to be actually executed.
+        /// </summary>
+        public MethodInfo ActualMethod
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// Gets the test object declaring a adorned test method.
+        /// </summary>
+        public object TestObject
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// Gets the test object declaring a actual test method.
+        /// </summary>
+        public object ActualObject
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        IMethodInfo ITestCommandInfo.TestMethod
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        object[] ITestCommandInfo.GetArguments(object testObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/Experiment.Xunit.UnitTest/TestInfoTest.cs
@@ -0,0 +1,22 @@
+﻿namespace Jwc.Experiment.Xunit
+{
+    using System;
+    using global::Xunit;
+
+    public class TestInfoTest
+    {
+        [Fact]
+        public void SutIsTestMethodInfo()
+        {
+            var sut = new TestInfo();
+            Assert.IsAssignableFrom<ITestMethodInfo>(sut);
+        }
+
+        [Fact]
+        public void SutIsTestCommandInfo()
+        {
+            var sut = new TestInfo();
+            Assert.IsAssignableFrom<ITestCommandInfo>(sut);
+        }
+    }
+}
