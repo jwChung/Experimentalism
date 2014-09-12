e6746b7f01cf863efa1dfab664cac2b83f691961
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-12 15:55:40 +0900

    dev: Introduced the TestCaseCommandFactory class.

---
+++ src/Experiment.Xunit/TestCaseCommandFactory.cs
@@ -0,0 +1,29 @@
+﻿namespace Jwc.Experiment.Xunit
+{
+    using System;
+    using System.Collections.Generic;
+    using global::Xunit.Sdk;
+
+    /// <summary>
+    /// Represents a factory to create test-case commands.
+    /// </summary>
+    public class TestCaseCommandFactory : ITestCommandFactory
+    {
+        /// <summary>
+        /// Creates test commands.
+        /// </summary>
+        /// <param name="testMethod">
+        /// Information about a test method.
+        /// </param>
+        /// <param name="fixtureFactory">
+        /// A factory of test fixture.
+        /// </param>
+        /// <returns>
+        /// The new test commands.
+        /// </returns>
+        public IEnumerable<ITestCommand> Create(IMethodInfo testMethod, ITestFixtureFactory fixtureFactory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/Experiment.Xunit.UnitTest/TestCaseCommandFactoryTEst.cs
@@ -0,0 +1,15 @@
+﻿namespace Jwc.Experiment.Xunit
+{
+    using System;
+    using global::Xunit;
+
+    public class TestCaseCommandFactoryTest
+    {
+        [Fact]
+        public void SutIsTestCommandFactory()
+        {
+            var sut = new TestCaseCommandFactory();
+            Assert.IsAssignableFrom<ITestCommandFactory>(sut);
+        }
+    }
+}
