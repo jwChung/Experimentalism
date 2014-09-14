cee243e638ed856385df501d77e5879c9d69fd40
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-15 01:45:56 +0900

    dev: Removed FrozenAttribute.

--- build/CommonAssemblyInfo.cs
+++ build/CommonAssemblyInfo.cs
@@ -38,6 +38,7 @@ using System.Runtime.InteropServices;
  *     * Experiment.AutoFixture
  *       - AutoPropertiesAttribute
  *       - FrozenAttribute
+ *       - GreedyAttribute
  *
  *     * Experiment.Xunit
  *       - FirstClassTestAttribute
