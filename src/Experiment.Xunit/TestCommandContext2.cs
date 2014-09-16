49f2675b316e44b2784b08eee2c288cf944e1c7e
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-16 10:26:38 +0900

    dev: Introduced TestCommandContext2.

---
+++ src/Experiment.Xunit/TestCommandContext2.cs
@@ -0,0 +1,20 @@
+﻿namespace Jwc.Experiment.Xunit
+{
+    using System;
+    using System.Collections.Generic;
+    using global::Xunit.Sdk;
+
+    public abstract class TestCommandContext2 : ITestCommandContext
+    {
+        public abstract IMethodInfo TestMethod { get; }
+
+        public abstract ITestMethodContext GetMethodContext(object testObject);
+
+        public abstract ITestMethodContext GetStaticMethodContext();
+
+        public IEnumerable<object> GetArguments(ITestMethodContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/Experiment.Xunit.UnitTest/TestCommandContext2Test.cs
@@ -0,0 +1,15 @@
+﻿namespace Jwc.Experiment.Xunit
+{
+    using System;
+    using global::Xunit;
+
+    public class TestCommandContext2Test
+    {
+        [Fact]
+        public void SutIsTestCommandContext()
+        {
+            var sut = Mocked.Of<TestCommandContext2>();
+            Assert.IsAssignableFrom<ITestCommandContext>(sut);
+        }
+    }
+}
