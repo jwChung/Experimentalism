74c9cdf7a7e020d9546871b477879ff85d3109d8
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-16 10:38:35 +0900

    dev: Addressed constructor of TestCommandContext2.

--- src/Experiment.Xunit/TestCommandContext2.cs
+++ src/Experiment.Xunit/TestCommandContext2.cs
@@ -6,6 +6,31 @@
 
     public abstract class TestCommandContext2 : ITestCommandContext
     {
+        private readonly ITestFixtureFactory factory;
+        private readonly IEnumerable<object> arguments;
+
+        public TestCommandContext2(ITestFixtureFactory factory, IEnumerable<object> arguments)
+        {
+            this.factory = factory;
+            this.arguments = arguments;
+        }
+
+        /// <summary>
+        /// Gets the factory to create test fixture.
+        /// </summary>
+        public ITestFixtureFactory TestFixtureFactory
+        {
+            get { return this.factory; }
+        }
+
+        /// <summary>
+        /// Gets the explicit arguments of the test.
+        /// </summary>
+        public IEnumerable<object> ExplicitArguments
+        {
+            get { return this.arguments; }
+        }
+
         public abstract IMethodInfo TestMethod { get; }
 
         public abstract ITestMethodContext GetMethodContext(object testObject);
--- test/Experiment.Xunit.UnitTest/TestCommandContext2Test.cs
+++ test/Experiment.Xunit.UnitTest/TestCommandContext2Test.cs
@@ -1,6 +1,7 @@
 ﻿namespace Jwc.Experiment.Xunit
 {
     using System;
+    using System.Collections.Generic;
     using global::Xunit;
 
     public class TestCommandContext2Test
@@ -8,8 +9,22 @@
         [Fact]
         public void SutIsTestCommandContext()
         {
-            var sut = Mocked.Of<TestCommandContext2>();
+            var sut = Mocked.Of<TestCommandContext2>(
+                Mocked.Of<ITestFixtureFactory>(),
+                Mocked.Of<IEnumerable<object>>());
             Assert.IsAssignableFrom<ITestCommandContext>(sut);
+        }
+
+        [Fact]
+        public void InitializeCorrectlyInitializes()
+        {
+            ITestFixtureFactory factory = Mocked.Of<ITestFixtureFactory>();
+            IEnumerable<object> arguments = Mocked.Of<IEnumerable<object>>();
+            
+            var sut = Mocked.Of<TestCommandContext2>(factory, arguments);
+
+            Assert.Equal(factory, sut.TestFixtureFactory);
+            Assert.Equal(arguments, sut.ExplicitArguments);
         }
     }
 }
