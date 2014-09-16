9a289750148aefe235ee0ca84baf6a2769b88ffb
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-16 13:58:06 +0900

    dev: Introduced TestCaseCommandContext.

---
+++ src/Experiment.Xunit/TestCaseCommandContext.cs
@@ -0,0 +1,34 @@
+﻿namespace Jwc.Experiment.Xunit
+{
+    using System;
+    using System.Collections.Generic;
+    using global::Xunit.Sdk;
+
+    public class TestCaseCommandContext : TestCommandContext2
+    {
+        public TestCaseCommandContext(
+            IMethodInfo testMethod,
+            IMethodInfo actualMethod,
+            object actualObject,
+            ITestFixtureFactory factory,
+            IEnumerable<object> arguments)
+            : base(factory, arguments)
+        {
+        }
+
+        public override IMethodInfo TestMethod
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override ITestMethodContext GetMethodContext(object testObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ITestMethodContext GetStaticMethodContext()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/Experiment.Xunit.UnitTest/TestCaseCommandContextTest.cs
@@ -0,0 +1,21 @@
+﻿namespace Jwc.Experiment.Xunit
+{
+    using global::Xunit;
+    using global::Xunit.Sdk;
+
+    public class TestCaseCommandContextTest
+    {
+        [Fact]
+        public void SutIsTestCommandContext()
+        {
+            var sut = new TestCaseCommandContext(
+                Mocked.Of<IMethodInfo>(),
+                Mocked.Of<IMethodInfo>(),
+                new object(),
+                Mocked.Of<ITestFixtureFactory>(),
+                new object[0]);
+            Assert.IsAssignableFrom<TestCommandContext2>(sut);
+        }
+
+    }
+}
