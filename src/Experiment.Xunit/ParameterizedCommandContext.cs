12c6b5076c77dd98131cf07a1849ad2e3da31b25
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-16 12:06:10 +0900

    dev: Introduced ParameterizedCommandContext.

---
+++ src/Experiment.Xunit/ParameterizedCommandContext.cs
@@ -0,0 +1,29 @@
+﻿namespace Jwc.Experiment.Xunit
+{
+    using System;
+    using System.Collections.Generic;
+    using global::Xunit.Sdk;
+
+    public class ParameterizedCommandContext : TestCommandContext2
+    {
+        public ParameterizedCommandContext(ITestFixtureFactory factory, IEnumerable<object> arguments)
+            : base(factory, arguments)
+        {
+        }
+
+        public override IMethodInfo TestMethod
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override ITestMethodContext GetMethodContext(object testObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ITestMethodContext GetStaticMethodContext()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/Experiment.Xunit.UnitTest/ParameterizedCommandContextTest.cs
@@ -0,0 +1,17 @@
+﻿namespace Jwc.Experiment.Xunit
+{
+    using System.Collections.Generic;
+    using global::Xunit;
+
+    public class ParameterizedCommandContextTest
+    {
+        [Fact]
+        public void SutIsTestCommandContext()
+        {
+            var sut = new ParameterizedCommandContext(
+                Mocked.Of<ITestFixtureFactory>(),
+                Mocked.Of<IEnumerable<object>>());
+            Assert.IsAssignableFrom<TestCommandContext2>(sut);
+        }
+    }
+}
