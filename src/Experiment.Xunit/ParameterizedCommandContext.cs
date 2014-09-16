c2dac00886481560208b7419b9f5556b5f5ae4f5
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-16 13:12:10 +0900

    docs: Documented ParameterizedCommandContext.

--- src/Experiment.Xunit/ParameterizedCommandContext.cs
+++ src/Experiment.Xunit/ParameterizedCommandContext.cs
@@ -4,10 +4,25 @@
     using System.Collections.Generic;
     using global::Xunit.Sdk;
 
+    /// <summary>
+    /// Represents context of a parameterized command.
+    /// </summary>
     public class ParameterizedCommandContext : TestCommandContext2
     {
         private readonly IMethodInfo testMethod;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParameterizedCommandContext"/> class.
+        /// </summary>
+        /// <param name="testMethod">
+        /// A test method.
+        /// </param>
+        /// <param name="factory">
+        /// A factory to create test fixture.
+        /// </param>
+        /// <param name="arguments">
+        /// Explicit arguments of the test method.
+        /// </param>
         public ParameterizedCommandContext(
             IMethodInfo testMethod, ITestFixtureFactory factory, IEnumerable<object> arguments)
             : base(factory, arguments)
@@ -18,11 +33,23 @@
             this.testMethod = testMethod;
         }
 
+        /// <summary>
+        /// Gets the test method.
+        /// </summary>
         public override IMethodInfo TestMethod
         {
             get { return this.testMethod; }
         }
 
+        /// <summary>
+        /// Gets information of the test method.
+        /// </summary>
+        /// <param name="testObject">
+        /// The test object.
+        /// </param>
+        /// <returns>
+        /// The information of the test method.
+        /// </returns>
         public override ITestMethodContext GetMethodContext(object testObject)
         {
             if (testObject == null)
@@ -32,6 +59,12 @@
                 this.testMethod.MethodInfo, this.testMethod.MethodInfo, testObject, testObject);
         }
 
+        /// <summary>
+        /// Gets information of the static test method.
+        /// </summary>
+        /// <returns>
+        /// The information of the static test method.
+        /// </returns>
         public override ITestMethodContext GetStaticMethodContext()
         {
             return new TestMethodContext(
