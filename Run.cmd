e8287af187e8f89498dffda9e624ff08b8eda6a0
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-21 01:43:02 +0900

    build: Deleted old scripts.

--- Run.cmd
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-IF "%VS120COMNTOOLS%"=="" (
-    SET VsVersionProperty=""
-) ELSE (
-    SET VsVersionProperty="/p:VisualStudioVersion=12.0"
-)
-
-"%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild" build\Build.proj /v:minimal /maxcpucount /nodeReuse:false %VsVersionProperty% %*
