2af63dee58799bb706cbadb9be20afe932951a3c
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-21 14:15:43 +0900

    dev: Inctroduced OmitAutoPropertiesCustomization.

---
+++ src/Experiment.AutoFixture/OmitAutoPropertiesCustomization.cs
@@ -0,0 +1,12 @@
+﻿namespace Jwc.Experiment.AutoFixture
+{
+    using Ploeh.AutoFixture;
+
+    public class OmitAutoPropertiesCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/Experiment.AutoFixture.UnitTest/OmitAutoPropertiesCustomizationTest.cs
@@ -0,0 +1,15 @@
+﻿namespace Jwc.Experiment.AutoFixture
+{
+    using global::Xunit;
+    using Ploeh.AutoFixture;
+
+    public class OmitAutoPropertiesCustomizationTest
+    {
+        [Fact]
+        public void SutIsCustomization()
+        {
+            var sut = new OmitAutoPropertiesCustomization();
+            Assert.IsAssignableFrom<ICustomization>(sut);
+        }
+    }
+}
