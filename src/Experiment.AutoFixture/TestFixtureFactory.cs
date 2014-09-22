831d23709f3f02f0c0d47ea828f65129a8ceaf90
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-22 16:53:41 +0900

    dev: Implemented TestFixtureFactory.

--- src/Experiment.AutoFixture/TestFixtureFactory.cs
+++ src/Experiment.AutoFixture/TestFixtureFactory.cs
@@ -1,10 +1,23 @@
 ﻿namespace Jwc.Experiment.AutoFixture
 {
+    using System;
+    using Ploeh.AutoFixture;
+
     public class TestFixtureFactory : ITestFixtureFactory
     {
         public ITestFixture Create(ITestMethodContext context)
         {
-            throw new System.NotImplementedException();
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            return new TestFixture(new Fixture().Customize(this.GetCustomization(context)));
+        }
+
+        protected virtual ICustomization GetCustomization(ITestMethodContext context)
+        {
+            return new CompositeCustomization(
+                new OmitAutoPropertiesCustomization(),
+                new TestParametersCustomization(context));
         }
     }
 }
--- test/Experiment.AutoFixture.UnitTest/TestFixtureFactoryTest.cs
+++ test/Experiment.AutoFixture.UnitTest/TestFixtureFactoryTest.cs
@@ -1,5 +1,10 @@
 ﻿namespace Jwc.Experiment.AutoFixture
 {
+    using System;
+    using System.Linq;
+    using Moq;
+    using Moq.Protected;
+    using Ploeh.AutoFixture;
     using global::Xunit;
 
     public class TestFixtureFactoryTest
@@ -9,6 +14,66 @@
         {
             var sut = new TestFixtureFactory();
             Assert.IsAssignableFrom<ITestFixtureFactory>(sut);
+        }
+
+        [Fact]
+        public void CreateWithNullContextThrows()
+        {
+            var sut = new TestFixtureFactory();
+            Assert.Throws<ArgumentNullException>(() => sut.Create(null));
+        }
+
+        [Fact]
+        public void CreateReturnsCorrectlyCustomizedFixture()
+        {
+            var sut  = Mocked.Of<TestFixtureFactory>();
+            var context = Mocked.Of<ITestMethodContext>();
+            var customization = Mocked.Of<ICustomization>();
+            sut.ToMock().Protected().Setup<ICustomization>(
+                "GetCustomization",
+                context)
+                .Returns(customization);
+
+            var actual = sut.Create(context);
+
+            var fixture= Assert.IsAssignableFrom<Fixture>(
+                Assert.IsAssignableFrom<TestFixture>(actual).Fixture);
+            customization.ToMock().Verify(c => c.Customize(fixture));
+        }
+
+        [Fact]
+        public void CreateReturnsFixtureCustomizedWithCorrectCustomization()
+        {
+            // Fixture setup
+            var sut = new TssTestFixtureFactory();
+
+            var context = Mocked.Of<ITestMethodContext>();
+
+            // Exercise system
+            sut.Create(context);
+
+            // Verify outcome
+            var customizations = Assert.IsAssignableFrom<CompositeCustomization>(
+                sut.Customization).Customizations;
+
+            Assert.Equal(2, customizations.Count());
+
+            customizations.OfType<OmitAutoPropertiesCustomization>().Single();
+            Assert.Equal(
+                context,
+                customizations.OfType<TestParametersCustomization>()
+                .Single().TestMethodContext);
+        }
+
+        private class TssTestFixtureFactory : TestFixtureFactory
+        {
+            public ICustomization Customization { get; set; }
+
+            protected override ICustomization GetCustomization(ITestMethodContext context)
+            {
+                this.Customization = base.GetCustomization(context);
+                return this.Customization;
+            }
         }
     }
 }
