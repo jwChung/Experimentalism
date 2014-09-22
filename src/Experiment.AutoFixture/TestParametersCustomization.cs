b9d27cc43522d3111e9ec15622e43d30bb49b193
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-22 12:20:48 +0900

    dev: Implemented TestParametersCustomization.

---
+++ src/Experiment.AutoFixture/TestParametersCustomization.cs
@@ -0,0 +1,40 @@
+﻿namespace Jwc.Experiment.AutoFixture
+{
+    using System;
+    using System.Linq;
+    using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.Xunit;
+
+    public class TestParametersCustomization : ICustomization
+    {
+        private readonly ITestMethodContext testMethodContext;
+
+        public TestParametersCustomization(ITestMethodContext testMethodContext)
+        {
+            if (testMethodContext == null)
+                throw new ArgumentNullException("testMethodContext");
+
+            this.testMethodContext = testMethodContext;
+        }
+
+        public ITestMethodContext TestMethodContext
+        {
+            get { return this.testMethodContext; }
+        }
+
+        public void Customize(IFixture fixture)
+        {
+            if (fixture == null)
+                throw new ArgumentNullException("fixture");
+
+            var customizations = this.testMethodContext.ActualMethod.GetParameters()
+                .SelectMany(p =>
+                    p.GetCustomAttributes(typeof(CustomizeAttribute), false)
+                    .Cast<CustomizeAttribute>()
+                    .Select(c => c.GetCustomization(p)))
+                .ToArray();
+
+            fixture.Customize(new CompositeCustomization(customizations));
+        }
+    }
+}
---
+++ test/Experiment.AutoFixture.UnitTest/TestParametersCustomizationTest.cs
@@ -0,0 +1,79 @@
+﻿namespace Jwc.Experiment.AutoFixture
+{
+    using System;
+    using Ploeh.Albedo;
+    using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.Xunit;
+    using global::Xunit;
+
+    public class TestParametersCustomizationTest
+    {
+        [Fact]
+        public void SutIsCustomization()
+        {
+            var sut = new TestParametersCustomization(Mocked.Of<ITestMethodContext>());
+            Assert.IsAssignableFrom<ICustomization>(sut);
+        }
+
+        [Fact]
+        public void InitializeWithNullContextThrows()
+        {
+            Assert.Throws<ArgumentNullException>(() => new TestParametersCustomization(null));
+        }
+
+        [Fact]
+        public void InitializeCorrectlyInitializesProperties()
+        {
+            var testMethodContext = Mocked.Of<ITestMethodContext>();
+            var sut = new TestParametersCustomization(testMethodContext);
+            Assert.Equal(testMethodContext, sut.TestMethodContext);
+        }
+
+        [Fact]
+        public void CustomizeWitNullFixtureThrows()
+        {
+            var sut = new TestParametersCustomization(Mocked.Of<ITestMethodContext>());
+            Assert.Throws<ArgumentNullException>(() => sut.Customize(null));
+        }
+
+        [Fact]
+        public void CustomizeCorrectlyCustomizesFixture()
+        {
+            var context = Mocked.Of<ITestMethodContext>(
+                c => c.ActualMethod
+                    == new Methods<TestParametersCustomizationTest>()
+                    .Select(x => x.TestMethod(null, null)));
+            var sut = new TestParametersCustomization(context);
+            var fixture = new Fixture();
+
+            sut.Customize(fixture);
+
+            Assert.Equal(fixture.Create<object>(), fixture.Create<object>());
+            Assert.Equal(fixture.Create<Person>(), fixture.Create<Person>());
+            Assert.NotNull(fixture.Create<Person>().Name);
+        }
+
+        private void TestMethod([Frozen] object arg1, [Frozen][Greedy] Person arg2)
+        {
+        }
+
+        private class Person
+        {
+            private readonly string name;
+
+            public Person()
+            {
+            }
+
+            public Person(string name)
+            {
+                this.name = name;
+            }
+
+            public string Name
+            {
+                get { return this.name; }
+            }
+        }
+    }
+}
