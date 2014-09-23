135287c17abe6bc3a212e60d4137acfde39743fb
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-23 11:50:52 +0900

    build: Incremented patch version,
    to indicate that this fixes a bug.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -11,10 +11,10 @@ using System.Runtime.InteropServices;
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-pre05")]
+[assembly: AssemblyInformationalVersion("2.0.0-pre06")]
 
 /*
- * Version 2.0.0-pre05
+ * Version 2.0.0-pre06
  * 
  * - [Major] Removed obsolete and discarded, types or members.
  *     * Jwc.Experiment.DefaultFixtureConfigurationAttribute
