d25cb27b1e5c13908b7ba013fdaea16368651dca
Jin-Wook Chung <jwChung@hotmail.com>
2014-09-24 20:28:28 +0900

    build: Incremented pre-release version,
    to test whether on a target project, the config. file is created
    automatically.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -11,10 +11,10 @@ using System.Runtime.InteropServices;
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-pre08")]
+[assembly: AssemblyInformationalVersion("2.0.0-pre09")]
 
 /*
- * Version 2.0.0-pre08
+ * Version 2.0.0-pre09
  * 
  * - [Major] Removed obsolete and discarded, types or members.
  *     * Jwc.Experiment.DefaultFixtureConfigurationAttribute
