900c14433938355f8ffe539cfdef76d76dc8dad6
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-15 18:29:43 +0900

    Deleted Experiment and its tests.

--- test/Experiment.Xunit.UnitTest/AssemblyTest.cs
+++ test/Experiment.Xunit.UnitTest/AssemblyTest.cs
@@ -13,7 +13,6 @@
                 Assembly.Load("mscorlib"),
                 Assembly.Load("System, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"),
                 Assembly.Load("System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"),
-                Assembly.Load("Jwc.Experiment"),
                 Assembly.Load("xunit"),
                 Assembly.Load("xunit.extensions"))
                 .Verify(Assembly.Load("Jwc.Experiment.Xunit"));
