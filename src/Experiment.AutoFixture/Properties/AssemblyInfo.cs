1bc4e1e4e4367153972b99fabb75db6f70cbe6c9
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-15 18:28:00 +0900

    Deleted Experiment.AutoFixture and its tests.

--- src/Experiment.AutoFixture/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Experiment.AutoFixture")]
-[assembly: AssemblyDescription("Experiment.AutoFixture supports parameterized tests with auto data using AutoFixture library.")]
-[assembly: CLSCompliant(true)]
