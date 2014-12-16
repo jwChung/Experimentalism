ea4e7fabb490acd7514ffc483f4f745e514edca8
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-16 22:45:53 +0900

    Refactored assembly tests.

--- test/Experiment.Idioms.UnitTest/AssemblyTest.cs
+++ test/Experiment.Idioms.UnitTest/AssemblyTest.cs
@@ -10,19 +10,23 @@
         [Fact]
         public void SutReferencesOnlySpecifiedAssemblies()
         {
-            new RestrictiveReferenceAssertion(
+            var assemblies = new[]
+            {
                 Assembly.Load("mscorlib"),
                 Assembly.Load("System, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"),
                 Assembly.Load("System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"),
                 Assembly.Load("Ploeh.Albedo"),
                 Assembly.Load("Ploeh.AutoFixture"),
-                Assembly.Load("Mono.Reflection"))
+                Assembly.Load("Mono.Reflection")
+            };
+
+            new RestrictiveReferenceAssertion(assemblies)
                 .Verify(Assembly.Load("Jwc.Experiment.Idioms"));
         }
 
         [Theory]
         [InlineData("Mono.Reflection")]
-        public void SutDoesNotExposeSpecifiedAssemblies(string assembly)
+        public void SutDoesNotExposeAnySpecifiedAssemblies(string assembly)
         {
             new NotExposedReferenceAssertion(Assembly.Load(assembly))
                 .Verify(Assembly.Load("Jwc.Experiment.Idioms"));
--- test/Experiment.Xunit.UnitTest/AssemblyTest.cs
+++ test/Experiment.Xunit.UnitTest/AssemblyTest.cs
@@ -10,20 +10,24 @@
         [Fact]
         public void SutReferencesOnlySpecifiedAssemblies()
         {
-            new RestrictiveReferenceAssertion(
+            var assemblies = new[]
+            {
                 Assembly.Load("mscorlib"),
                 Assembly.Load("System, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"),
                 Assembly.Load("System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"),
                 Assembly.Load("Ploeh.AutoFixture, Version=3.18.7.0, Culture=neutral, PublicKeyToken=b24654c590009d4f"),
                 Assembly.Load("Ploeh.AutoFixture.Xunit, Version=3.18.7.0, Culture=neutral, PublicKeyToken=b24654c590009d4f"),
                 Assembly.Load("xunit"),
-                Assembly.Load("xunit.extensions"))
+                Assembly.Load("xunit.extensions")
+            };
+
+            new RestrictiveReferenceAssertion(assemblies)
                 .Verify(Assembly.Load("Jwc.Experiment.Xunit"));
         }
 
         [Theory]
         [InlineData("xunit.extensions")] // Not il-merged
-        public void SutDoesNotExposeSpecifiedAssemblies(string assembly)
+        public void SutDoesNotExposeAnySpecifiedAssemblies(string assembly)
         {
             new NotExposedReferenceAssertion(Assembly.Load(assembly))
                 .Verify(Assembly.Load("Jwc.Experiment.Xunit"));
