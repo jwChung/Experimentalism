448b73cbab35ca17043352fbd2ac2e106fc6e4ee
Jin-Wook Chung <jwChung@hotmail.com>
2015-05-01 14:27:36 +0900

    Incremented patch version,
    in order to fix a bug.

--- build/SolutionInfo.cs
+++ build/SolutionInfo.cs
@@ -10,21 +10,11 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0")]
+[assembly: AssemblyVersion("3.0.1")]
+[assembly: AssemblyInformationalVersion("3.0.1")]
 
 /*
- * Version 3.0.0
+ * Version 3.0.1
  * 
- * - [Major] Removed the Experiment.AutoFixture project. This project is not
- *   used in the next new major release as the new version depends on the
- *   AutoFixture abstractions. (IFixture, ISpecimenBuilder)
- *   
- * - [Major] Removed the old abstractions for test fixture, which is replaced
- *   with the AutoFixture abstractions.
- *   
- * - [Major] Renamed IndirectReferenceAssertion to NotExposedReferenceAssertion.
- *
- * - [Minor] Introduced the ParameterCustomization class to support the
- *   parameter customization attributes of AutoFixture.Xunit.
+ * - Rectified an incorrect dependency of the Experiment.Idoms package.
  */
